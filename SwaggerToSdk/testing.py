557640934d8d285e3a0e29da2108cea07ff968c4
Laurent Mazuel <lmazuel@microsoft.com>
2016-04-16 09:57:27 -0700

    Swagger to sdk - new features (#240)
    * Add support for Autorest version choices
    
    * Configure the language using the 'language' meta parameter
    
    * Documentation
    
    * Log update
    
    * Add autorest_options conf options

--- SwaggerToSdk/testing.py
+++ SwaggerToSdk/testing.py
@@ -16,6 +16,7 @@ class TestSwaggerToSDK(unittest.TestCase):
             if key.startswith('TRAVIS'):
                 del os.environ[key]
 
+    @unittest.skip
     def test_get_pr_from_travis_commit_sha(self):
         os.environ['TRAVIS_REPO_SLUG'] = 'Azure/azure-sdk-for-python'
         os.environ['TRAVIS_COMMIT'] = '497955507bc152c444bd1785f34cafefc7e4e8d9'
@@ -27,5 +28,39 @@ class TestSwaggerToSDK(unittest.TestCase):
         pr_obj = get_pr_from_travis_commit_sha(GH_TOKEN)
         self.assertIsNone(pr_obj)
 
+    @unittest.skip
+    def test_download_autorest(self):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            exe_path = download_install_autorest(temp_dir)
+            self.assertTrue(exe_path.lower().endswith("autorest.exe"))
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            exe_path = download_install_autorest(temp_dir, "0.16.0-Nightly20160410")
+            self.assertTrue(exe_path.lower().endswith("autorest.exe"))
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            with self.assertRaises(ValueError):
+                exe_path = download_install_autorest(temp_dir, "0.16.0-FakePackage")
+
+    def test_build_autorest_options(self):
+        line = build_autorest_options("Python", {"A": "value"}, {"B": "value"})
+        self.assertEqual(line, "-A value -B value -CodeGenerator Azure.Python")
+
+        line = build_autorest_options("Python", {"A": "value"}, {"A": "newvalue"})
+        self.assertEqual(line, "-A newvalue -CodeGenerator Azure.Python")
+
+        line = build_autorest_options("Python", {"CodeGenerator": "NodeJS"}, {})
+        self.assertEqual(line, "-CodeGenerator NodeJS")
+
+        line = build_autorest_options("Python", {"CodeGenerator": "NodeJS"}, {"CodeGenerator": "CSharp"})
+        self.assertEqual(line, "-CodeGenerator CSharp")
+
+        line = build_autorest_options("Python", {}, {})
+        self.assertEqual(line, "-CodeGenerator Azure.Python")
+
+        line = build_autorest_options("Python", {"A": 12, "B": True})
+        self.assertEqual(line, "-A 12 -B True -CodeGenerator Azure.Python")
+
+
 if __name__ == '__main__':
-    unittest.main()      
+    unittest.main()
