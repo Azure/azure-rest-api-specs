b5289d525a6700b507f84a51a61122bb089744d5
Amar Zavery <amzavery@microsoft.com>
2017-03-07 13:09:27 -0800

    Add support for validating swaggers in yaml and a bug fix (#1008)

--- test/util/utils.js
+++ test/util/utils.js
@@ -8,6 +8,7 @@ var assert = require("assert"),
   path = require('path'),
   _ = require('lodash'),
   z = require('z-schema'),
+  YAML = require('js-yaml'),
   request = require('request'),
   util = require('util'),
   execSync = require('child_process').execSync;
@@ -52,9 +53,9 @@ exports.parseJsonFromFile = function parseJsonFromFile(filepath, callback) {
   fs.readFile(filepath, 'utf8', function (err, data) {
     if (err) return callback(err);
     try {
-      return callback(null, JSON.parse(exports.stripBOM(data)));
+      return callback(null, YAML.safeLoad(exports.stripBOM(data)));
     } catch (error) {
-      let e = new Error(`swagger "${filepath}" is an invalid JSON.\n${util.inspect(err, { depth: null })}`);
+      let e = new Error(`swagger "${filepath}" is an invalid JSON.\n${util.inspect(error, { depth: null })}`);
       return callback(e);
     }
   });
