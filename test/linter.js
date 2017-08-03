fb4144cc5306e2f668455ce499624143ae741248
Deepak Shankargouda <deshank@microsoft.com>
2017-08-03 14:57:18 -0700

    code awesomeness

--- test/linter.js
+++ test/linter.js
@@ -6,69 +6,114 @@ var
   execSync = require('child_process').execSync,
   utils = require('./util/utils'),
   fs = require('fs'),
+  literate = require('@microsoft.azure/literate'),
+  async_io_1 = require("@microsoft.azure/async-io"),
   path = require('path');
 
-function getTagsFromConfig(config){
+async function readConfigFile(file, tag) {
+  // autorest configuration type
+  const msgs = new literate.MessageEmitter();
+  msgs.Message.Subscribe((src, each) => console.log(each.Text));
+  const cfg = new literate.Configuration("\n> see https://aka.ms/autorest", new literate.DiskFileSystem("readme.md"), async_io_1.ResolveUri(async_io_1.CreateFolderUri(process.cwd()), async_io_1.CreateFileUri(path.resolve(file)) || "."), {
+    "input-file": [],
+  });
+  return await cfg.CreateView(msgs, true, { tag: tag });
+}
+
+async function getTagsMapFromConfig(args) {
+  if (!fs.existsSync(path.resolve(args.file))) {
+    console.error('config file invalid. cannot read tags from file ' + config);
+    return null;
+  }
+
+  const allTags = scanForTags(fs.readFileSync(args.file));
+  const result = {};
+  for (const each of allTags) {
+    result[each] = (await readConfigFile(args.file, each))["input-file"];
+  }
+  return result;
+}
+
+function scanForTags(content) {
+  const result = new Array();
+  const rx = /\$\(tag\)(.*)/g;
+  let match = rx.exec(content);
+  while (match) {
+    const vrx = /['"](.*?)['"]/g;
+    let v = vrx.exec(match[1]);
+    if (v && v.length && result.indexOf(v[1]) == -1) {
+      result.push(v[1]);
+    }
+    match = rx.exec(content);
+  }
+  return result;
+}
+
+async function getTagsFromConfig(config) {
   // get hold of all tags and their corresponding input files using the literate config tool
-  const result = execSync(`node node_modules/@microsoft.azure/literate/dist/app.js all --file=${path.resolve(config)}`);
+  // const result = execSync(`node node_modules/@microsoft.azure/literate/dist/app.js all --file=${path.resolve(config)}`);
   // can't get JS to parse a string with "'" 
-  const tagsMap = JSON.parse(result.toString().replace(/'/g,'"'));
+  // const tagsMap = JSON.parse(result.toString().replace(/'/g,'"'));
+  const tagsMap = await getTagsMapFromConfig({ file: config });
+  if (tagsMap === null) {
+    return null;
+  }
   const tags = Object.keys(tagsMap);
-  
+
   // filter the tags
-  if(utils.prOnly)
-  {
+  if (utils.prOnly) {
     // get path to the modified files relative to their corresponding md file, need to do this since 
     // config files have relative paths to the input files
     let allModifiedFiles = utils.getFilesChangedInPR();
-    allModifiedFiles = allModifiedFiles.map(mfile=>{
-      return path.relative(config, mfile);
+    allModifiedFiles = allModifiedFiles.map(mfile => {
+      return mfile.replace(path.dirname, '');
     });
-    
+
     // for each tag->files, find if there are any modified files and select those tags
-    return tags.filter(tag=>{
-      const tagFiles = (tagsMap[tag]+'').split(',');
+    return tags.filter(tag => {
+      const tagFiles = (tagsMap[tag] + '').split(',');
       // find intersection with the modified files
-      return tagFiles.filter(tagFile=>{
-        return allModifiedFiles.indexOf(path.normalize(tagFile))>-1;
-      }).length>0;
+      return tagFiles.filter(tagFile => {
+        return allModifiedFiles.indexOf(path.normalize(tagFile)) > -1;
+      }).length > 0;
     });
-  } 
+  }
   return tags;
 }
 
-function execLinterCommand(config, tag)
-{
-    it(config + ' should honor linter validation rules.', async function () {
-      var cmd = `autorest --validation --azure-validator ${config} --message-format=json ${tag}`.trim();
-      console.log(`Executing: ${cmd}`);
-      try {
-        let result = execSync(cmd, { encoding: 'utf8', maxBuffer: 1024 * 1024 * 64 });
-      } catch (err) {
-        throw new Error('Linter validation contains error(s)');
-      }
-    });
+function execLinterCommand(config, tag) {
+  var cmd = `autorest --validation --azure-validator ${config} --message-format=json ${tag}`.trim();
+  console.log(`Executing: ${cmd}`);
+  try {
+    let result = execSync(cmd, { encoding: 'utf8', maxBuffer: 1024 * 1024 * 64 });
+  } catch (err) {
+    throw new Error('Linter validation contains error(s)');
+  }
+
 }
 
 describe('AutoRest Linter validation:', function () {
   let configsToProcess = utils.getConfigFilesChangedInPR();
-  // Useful when debugging a test for a particular swagger. 
-  // Just update the regex. That will return an array of filtered items.
-  // configsToProcess = ['specification/sql/resource-manager/readme.md'];
   for (const config of configsToProcess) {
-    // find all tags in the config file
-    const tagsToProcess = getTagsFromConfig(config);
-    // if no tags found to process, run with the defaults    
-    if(tagsToProcess===undefined || tagsToProcess.length===0)
-    {
-      execLinterCommand(config, '');
-    } 
-    else
-    {
-      // if tags found, run linter against every single tag
-      tagsToProcess.forEach((tagToProcess) => {
-        execLinterCommand(config, `--tag=${tagToProcess}`);
-      }, this);
-    }
+    it(config + ' should honor linter validation rules.', async function () {
+
+      // Useful when debugging a test for a particular swagger. 
+      // Just update the regex. That will return an array of filtered items.
+      // configsToProcess = ['specification/sql/resource-manager/readme.md'];
+
+      // find all tags in the config file
+      const tagsToProcess = await getTagsFromConfig(config);
+      // if no tags found to process, run with the defaults    
+      if (tagsToProcess === null || tagsToProcess.length === 0) {
+        execLinterCommand(config, '');
+      }
+      else {
+        // if tags found, run linter against every single tag
+        tagsToProcess.forEach((tagToProcess) => {
+          execLinterCommand(config, `--tag=${tagToProcess}`);
+        }, this);
+      }
+    });
   }
+
 });
