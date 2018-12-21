18e41b3021a8186ccd26136181f425cc46e55839
Laurent Mazuel <lmazuel@microsoft.com>
2018-12-21 09:13:39 -0800

    Update SwaggerToSdk to new DockerHub address

--- scripts/swagger-to-sdk.sh
+++ scripts/swagger-to-sdk.sh
@@ -2,4 +2,4 @@
 
 python -c "import os; print('\n'.join(v for v in os.environ.keys() if v.startswith('TRAVIS')))" > /tmp/env_file
 docker pull lmazuel/swagger-to-sdk
-docker run --rm --env-file /tmp/env_file -e GH_TOKEN -v $PWD:/git-restapi/ lmazuel/swagger-to-sdk "$@"
+docker run --rm --env-file /tmp/env_file -e GH_TOKEN -v $PWD:/git-restapi/ azuresdk/swagger-to-sdk "$@"
