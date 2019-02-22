8cfd57b4d228e6e5306b57a0e7c462e95e6e150a
Sergey Shandar <sergey.shandar@gmail.com>
2019-02-22 10:19:43 -0800

    info

--- scripts/travis-env.ts
+++
@@ -1,8 +0,0 @@
-import * as child_process from "child_process"
-
-const systemPullRequestTargetBranch = process.env["System.PullRequest.TargetBranch"]
-if (systemPullRequestTargetBranch !== undefined) {
-  const s = systemPullRequestTargetBranch.split("/")
-  const travisBranch = s[s.length - 1]
-  child_process.spawnSync(`export TRAVIS_BRANCH=${travisBranch}`)
-}
