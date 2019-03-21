69a13dc12b6eab05e0316cad0a17bb60ff9e1a89
Sergey Shandar <sergey.shandar@gmail.com>
2019-03-20 17:17:59 -0700

    direct call

--- scripts/avocado.ts
+++ scripts/avocado.ts
@@ -1,7 +1,9 @@
 const utils = require('../test/util/utils')
-const cp = require("child_process")
-import path from "path"
+// const cp = require("child_process")
+import * as path from "path"
+import * as avocado from "@azure/avocado"
 
+/*
 const exec = (cmd: string, options: {}) => {
   const result = cp.spawnSync(
     cmd,
@@ -13,31 +15,36 @@ const exec = (cmd: string, options: {}) => {
   )
   return result.status
 }
+*/
 
 async function main() {
   const swaggersToProcess = utils.getFilesChangedInPR();
-  let result = 0
+  let errorNumbers = 0
   for (const swagger of swaggersToProcess) {
     try {
-      const avocadoPath = path.resolve("node_modules/.bin/avocado")
+      // const avocadoPath = path.resolve("node_modules/.bin/avocado")
       // await oav.validateExamples(swagger, null, {consoleLogLevel: 'error', pretty: true});
       // run OAV as a separate process to avoid memory issues.
-      console.log(path.dirname(swagger))
+      // console.log(path.dirname(swagger))
+      /*
       const r = exec(
         avocadoPath,
         {
           cwd: path.dirname(swagger)
         })
-      if (result === 0) {
-        result = r
+        */
+      const errors = avocado.avocado(path.dirname(swagger))
+      for await (const e of errors) {
+        console.error(e)
+        ++errorNumbers
       }
     } catch (e) {
-      console.error("error: ")
+      console.error("fatal error:")
       console.error(e)
-      result = 1
     }
   }
-  return result
+  console.log(`errors: ${errorNumbers}`)
+  return errorNumbers == 0 ? 0 : 1
 }
 
 main()
