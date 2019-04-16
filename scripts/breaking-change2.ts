39cad0322e29c7461965fcc939a749b0efdb2b93
Sergey Shandar <sergey.shandar@gmail.com>
2019-04-16 16:25:38 -0700

    Use scripts from @azure/rest-api-specs-scripts

--- scripts/breaking-change2.ts
+++
@@ -1,9 +0,0 @@
-import * as scripts from '@azure/rest-api-specs-scripts'
-
-scripts.breakingChange().then(() => {
-  console.log(`Thanks for using breaking change tool to review.`);
-  console.log(`If you encounter any issue(s), please open issue(s) at https://github.com/Azure/openapi-diff/issues .`);
-}).catch(err => {
-  console.log(err);
-  process.exitCode = 1;
-})
