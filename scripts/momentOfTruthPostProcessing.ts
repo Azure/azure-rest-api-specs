151ed28506e514048d120e65af958ed628598d17
Sergey Shandar <sergey.shandar@gmail.com>
2019-04-17 11:08:30 -0700

    remove 'ts-utils.ts'

--- scripts/momentOfTruthPostProcessing.ts
+++ scripts/momentOfTruthPostProcessing.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-import * as tsUtils from './ts-utils'
+import * as tsUtils from '@azure/rest-api-specs-scripts/src/ts-utils'
 import * as stringMap from '@ts-common/string-map'
 
 const fs = require('fs'),
