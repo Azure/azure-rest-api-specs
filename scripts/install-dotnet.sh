9dc028cf996e04d702c14543f1b73c4ecacf0551
Nick Schonning <nschonni@gmail.com>
2019-04-19 02:41:49 -0400

    chore: Use Xenial image for Travis
    .Net install started to fail on 14.04
    Pipelines is already using this version

--- scripts/install-dotnet.sh
+++ scripts/install-dotnet.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# From https://www.microsoft.com/net/download/linux-package-manager/ubuntu14-04/sdk-2.0.0
+# From https://www.microsoft.com/net/download/linux-package-manager/ubuntu16-04/sdk-2.0.0
 
-wget -q https://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.deb
+wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get install apt-transport-https -y
 sudo apt-get update
