781c97fd15b22843d1267d749f6e96261669cf8c
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2019-04-29 11:28:37 -0700

    [Merge is planed on 2019/04/29] Move scripts from this repo. (#5719)
    * Breaking Change2
    
    * Experimental lintdiff
    
    * No more model/semantic/syntax validations in travis-CI.
    
    * Scripts 0.1.8
    
    * Use scripts from @azure/rest-api-specs-scripts
    
    * ts-utils
    
    * fix breaking changes
    
    * momentOfTruthPostProcessing
    
    * momentOfTruthPostProcessing.ts
    
    * remove 'ts-utils.ts'
    
    * Remove `utils`
    
    * postToGitHub.ts
    
    * switch to momentOfTruthUtils
    
    * minor
    
    * Syntax.ts
    
    * semanticValidation.ts
    
    * another try
    
    * minor update
    
    * URL/fix
    
    * OAD ^0.2.13
    
    * update to new scripts.
    
    * Use scripts from `sergey/move-from-specs-repo`.
    
    * simplified model validation.
    
    * preprod
    
    * postToGitHub
    
    * momentOfTruthPostProcessing
    
    * Semantic Validation
    
    * getSwaggers()
    
    * Use `getExamples()` and `getSwaggers()`.
    
    * Update package.json
    
    * Remove travis-CI jobs except branch strategy.
    
    * remove model and semantic validations version 2.

--- test/syntax.ts
+++ test/syntax.ts
@@ -1,18 +1,24 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License in the project root for license information.
 
-'use strict';
 var assert = require("assert"),
-  fs = require('fs'),
-  path = require('path'),
   RefParser = require('json-schema-ref-parser'),
   util = require('util'),
-  utils = require('./util/utils');
+  utils = require('@azure/rest-api-specs-scripts').utils;
 
-var context;
+type Context = {
+  readonly validator: {
+    readonly validate: (parsedData: unknown, schema: unknown) => unknown
+    readonly getLastErrors: () => unknown
+  }
+  readonly extensionSwaggerSchema: unknown
+  readonly exampleSchema: unknown
+}
+
+let syntaxContext: Context
 
 
-// Useful when debugging a test for a particular swagger. 
+// Useful when debugging a test for a particular swagger.
 // Just update the regex. That will return an array of filtered items.
 // utils.swaggers = utils.swaggers.filter(function(item) {
 //   return (item.match(/.*Microsoft.Logic.*2016-06-01.*/ig) !== null);
@@ -24,19 +30,19 @@ var context;
 
 describe('Azure swagger schema validation:', function () {
   before(function (done) {
-    utils.initializeValidator().then((result) => {
-      context = result;
+    utils.initializeValidator().then((result: Context) => {
+      syntaxContext = result;
       done();
     });
-    
+
   });
 
-  for (const swagger of utils.swaggers) {
+  for (const swagger of utils.getSwaggers()) {
     it(swagger + ' should be a valid Swagger document.', function (done) {
-      utils.parseJsonFromFile(swagger).then((parsedData)=> {
-        var valid = context.validator.validate(parsedData, context.extensionSwaggerSchema);
+      utils.parseJsonFromFile(swagger).then((parsedData: unknown)=> {
+        var valid = syntaxContext.validator.validate(parsedData, syntaxContext.extensionSwaggerSchema);
       if (!valid) {
-        var error = context.validator.getLastErrors();
+        var error = syntaxContext.validator.getLastErrors();
         throw new Error("Schema validation failed: " + util.inspect(error, { depth: null }));
       }
       assert(valid === true);
@@ -46,12 +52,12 @@ describe('Azure swagger schema validation:', function () {
   }
 
   describe('Azure x-ms-example schema validation:', function () {
-    for (const example of utils.examples) {
+    for (const example of utils.getExamples()) {
       it('x-ms-examples: ' + example + ' should be a valid x-ms-example.', function (done) {
-        utils.parseJsonFromFile(example).then((parsedData) => {
-          var valid = context.validator.validate(parsedData, context.exampleSchema);
+        utils.parseJsonFromFile(example).then((parsedData: unknown) => {
+          var valid = syntaxContext.validator.validate(parsedData, syntaxContext.exampleSchema);
         if (!valid) {
-          var error = context.validator.getLastErrors();
+          var error = syntaxContext.validator.getLastErrors();
           throw new Error("Schema validation failed: " + util.inspect(error, { depth: null }));
         }
         assert(valid === true);
@@ -63,9 +69,9 @@ describe('Azure swagger schema validation:', function () {
 });
 
 describe('External file or url references ("$ref") in a swagger spec:', function () {
-  for (const swagger of utils.swaggers) {
+  for (const swagger of utils.getSwaggers()) {
     it(swagger + ' should be completely resolvable.', function (done) {
-      RefParser.bundle(swagger, function (bundleErr, bundleResult) {
+      RefParser.bundle(swagger, function (bundleErr: { readonly message: unknown }, _bundleResult: unknown) {
         if (bundleErr) {
           var msg = swagger + ' has references that cannot be resolved. They are as follows: \n' + util.inspect(bundleErr.message, { depth: null });
           console.log(msg);
