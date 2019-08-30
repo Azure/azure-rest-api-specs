603438e467baea0e3594ad2bdb4ed7544c7e8ff8
Phoenix He <tih@microsoft.com>
2019-08-30 08:15:35 +0800

    Use npm ci instead of npm install

--- scripts/switch-to-preproduction.sh
+++ scripts/switch-to-preproduction.sh
@@ -4,5 +4,5 @@
   echo "Switch to preproduction package.json"
   cp -f .azure-pipelines-preproduction/package.json ./package.json
   cp -f .azure-pipelines-preproduction/package-lock.json ./package-lock.json
-  PREPRODUCTION_PIPELINE= npm install
+  PREPRODUCTION_PIPELINE= npm ci
 }
