bc77c34a6ea85358564e528b007576092aeb8a23
Sanjai Ganesh Jeenagiri <sanjaiganesh@users.noreply.github.com>
2020-06-22 20:03:12 -0700

    git checkout master -f

--- scripts/datacontainer/run.sh
+++ scripts/datacontainer/run.sh
@@ -4,8 +4,11 @@
 timestamp=`date +%Y/%m/%d-%H:%M:%S`
 echo "[$timestamp] Copying files to /usr/data/openapispecs"
 mkdir -p /usr/data/openapispecs/.git
-cp -R /usr/data/specrepo/* /usr/data/openapispecs/
 cp -R /usr/data/specrepo/.git/* /usr/data/openapispecs/.git/
+
+timestamp=`date +%Y/%m/%d-%H:%M:%S`
+echo "[$timestamp] Running git checkout master"
+git checkout master -f
 
 timestamp=`date +%Y/%m/%d-%H:%M:%S`
 echo "[$timestamp] Deleting directory /usr/data/specrepo"
