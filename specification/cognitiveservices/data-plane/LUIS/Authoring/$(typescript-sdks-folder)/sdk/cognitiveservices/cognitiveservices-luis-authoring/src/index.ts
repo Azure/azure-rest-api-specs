d568e5b0506c7cac35441e2d297ceb208803fd9e
MIDDLEEAST\v-mariamel <v-mariamel@microsoft.com>
2021-08-05 15:50:43 +0200

    Deleting SDK files

--- specification/cognitiveservices/data-plane/LUIS/Authoring/$(typescript-sdks-folder)/sdk/cognitiveservices/cognitiveservices-luis-authoring/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./models";
-export { LuisAuthoringClient } from "./luisAuthoringClient";
-export { LuisAuthoringClientContext } from "./luisAuthoringClientContext";
-export * from "./operationsInterfaces";
