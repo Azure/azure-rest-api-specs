e0c7e7665b13f6f2ded6052e2f7d4ba855a3ae0d
ruowan <ruowan@microsoft.com>
2021-09-02 09:37:03 +0000

    delete unused powershell file and format scripts

--- specification/appplatform/resource-manager/Microsoft.AppPlatform/preview/2020-11-01-preview/scenarios/templates/uploadJar.ps1
+++ specification/appplatform/resource-manager/Microsoft.AppPlatform/preview/2020-11-01-preview/scenarios/templates/uploadJar.ps1
@@ -2,24 +2,24 @@ $uploadUrl = ${Env:uploadUrl}
 $BlobUri = ${Env:blobUrl}
 $localFilePath = './helloWorld.jar'
 function DownloadJarFromBlob([string]$blobUri, [string]$localOutputFilePath) {
-$Uri = [System.Uri]::New($blobUri.Split('?')[0])
-$SasToken = $blobUri.Split('?')[-1]
+	$Uri = [System.Uri]::New($blobUri.Split('?')[0])
+	$SasToken = $blobUri.Split('?')[-1]
 
-$StorageCredentials = [Microsoft.WindowsAzure.Storage.Auth.StorageCredentials]::New($SasToken)
-$BlobFile = [Microsoft.WindowsAzure.Storage.Blob.CloudBlob]::new($Uri, $StorageCredentials)
-$DownLoadTask = $BlobFile.DownloadToFileAsync($localOutputFilePath, 4)
-$DownLoadTask
+	$StorageCredentials = [Microsoft.WindowsAzure.Storage.Auth.StorageCredentials]::New($SasToken)
+	$BlobFile = [Microsoft.WindowsAzure.Storage.Blob.CloudBlob]::new($Uri, $StorageCredentials)
+	$DownLoadTask = $BlobFile.DownloadToFileAsync($localOutputFilePath, 4)
+	$DownLoadTask
 }
 
 function UploadToFileShare([string]$uploadUrl, [string]$localFilePath) {
-$Uri = [System.Uri]::New($uploadUrl.Split('?')[0])
+	$Uri = [System.Uri]::New($uploadUrl.Split('?')[0])
 
-$SasToken = $uploadUrl.Split('?')[-1]
-$StorageCredentials = [Microsoft.WindowsAzure.Storage.Auth.StorageCredentials]::New($SasToken)
-$CloudFile = [Microsoft.WindowsAzure.Storage.File.CloudFile]::New($Uri, $StorageCredentials)
+	$SasToken = $uploadUrl.Split('?')[-1]
+	$StorageCredentials = [Microsoft.WindowsAzure.Storage.Auth.StorageCredentials]::New($SasToken)
+	$CloudFile = [Microsoft.WindowsAzure.Storage.File.CloudFile]::New($Uri, $StorageCredentials)
 
-$UploadTask = $CloudFile.UploadFromFileAsync($localFilePath)
-$UploadTask
+	$UploadTask = $CloudFile.UploadFromFileAsync($localFilePath)
+	$UploadTask
 }
 
 Connect-AzAccount -Identity
