3918720e5fc9b733fe1709ed23af906017f47c57
ruowan <ruowan@microsoft.com>
2021-09-07 05:42:54 +0000

    update powershell scripts

--- specification/appplatform/resource-manager/Microsoft.AppPlatform/preview/2020-11-01-preview/scenarios/templates/uploadJar.ps1
+++ specification/appplatform/resource-manager/Microsoft.AppPlatform/preview/2020-11-01-preview/scenarios/templates/uploadJar.ps1
@@ -1,27 +1,18 @@
-$uploadUrl = ${Env:uploadUrl}
-$BlobUri = ${Env:blobUrl}
-$localFilePath = './helloWorld.jar'
+$uploadUri = ${Env:uploadUrl}
+$blobUri = ${Env:blobUrl}
+$localFilePath = '/tmp/temp.file'
 function DownloadJarFromBlob([string]$blobUri, [string]$localOutputFilePath) {
-	$Uri = [System.Uri]::New($blobUri.Split('?')[0])
-	$SasToken = $blobUri.Split('?')[-1]
-
-	$StorageCredentials = [Microsoft.WindowsAzure.Storage.Auth.StorageCredentials]::New($SasToken)
-	$BlobFile = [Microsoft.WindowsAzure.Storage.Blob.CloudBlob]::new($Uri, $StorageCredentials)
+	$BlobFile = [Microsoft.WindowsAzure.Storage.Blob.CloudBlob]::new($blobUri)
 	$DownLoadTask = $BlobFile.DownloadToFileAsync($localOutputFilePath, 4)
 	$DownLoadTask
 }
 
-function UploadToFileShare([string]$uploadUrl, [string]$localFilePath) {
-	$Uri = [System.Uri]::New($uploadUrl.Split('?')[0])
-
-	$SasToken = $uploadUrl.Split('?')[-1]
-	$StorageCredentials = [Microsoft.WindowsAzure.Storage.Auth.StorageCredentials]::New($SasToken)
-	$CloudFile = [Microsoft.WindowsAzure.Storage.File.CloudFile]::New($Uri, $StorageCredentials)
-
+function UploadToFileShare([string]$uploadUri, [string]$localFilePath) {
+	$CloudFile = [Microsoft.WindowsAzure.Storage.File.CloudFile]::New($uploadUri)
 	$UploadTask = $CloudFile.UploadFromFileAsync($localFilePath)
 	$UploadTask
 }
 
 Connect-AzAccount -Identity
-DownloadJarFromBlob $BlobUri $localFilePath
-UploadToFileShare $uploadUrl $localFilePath
+DownloadJarFromBlob $blobUri $localFilePath
+UploadToFileShare $uploadUri $localFilePath
