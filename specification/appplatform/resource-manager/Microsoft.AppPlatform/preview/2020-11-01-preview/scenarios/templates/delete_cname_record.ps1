e55a5cf892007bc388b0a6e16db59b3ee0f9c60f
Lei Ni <leni@microsoft.com>
2022-03-30 18:59:03 +0800

    remove scenario files

--- specification/appplatform/resource-manager/Microsoft.AppPlatform/preview/2020-11-01-preview/scenarios/templates/delete_cname_record.ps1
+++
@@ -1,9 +0,0 @@
-$resourceGroupName = ${Env:resourceGroupName}
-$dnsCNAME = ${Env:dnsCname}
-$dnsZoneName = ${Env:dnsZoneName}
-
-Connect-AzAccount -Identity
-
-$RecordSet = Get-AzDnsRecordSet -Name $dnsCname -RecordType CNAME -ResourceGroupName $resourceGroupName -ZoneName $dnsZoneName
-$Result = Remove-AzDnsRecordSet -RecordSet $RecordSet
-$Result
