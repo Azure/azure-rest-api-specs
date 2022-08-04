443a3a508f52965626a0d4f49192bd7c2d0617ff
Jianqi Zhao <archie.j.zhao@gmail.com>
2022-08-04 00:29:23 -0700

    Revert "Merge from network-2022-05-01"
    This reverts commit f0671a040b2bad684921f8f8ad4b53bb0f4b4a93.

--- specification/network/resource-manager/$(typescript-sdks-folder)/sdk/network/arm-network/src/index.ts
+++
@@ -1,4 +0,0 @@
-/// <reference lib="esnext.asynciterable" />
-export * from "./models";
-export { NetworkManagementClient } from "./networkManagementClient";
-export * from "./operationsInterfaces";
