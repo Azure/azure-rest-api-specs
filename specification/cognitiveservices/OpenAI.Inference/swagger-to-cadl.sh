905a6457b34b790b29a4829fe2c3ac8322244326
Johan Stenberg (MSFT) <johan.stenberg@microsoft.com>
2022-12-09 15:03:07 -0800

    Simplify OpenAI inferrence models and use core (#21609)
    * Simplify OpenAI inferrence models and use core
    
    * Simplified and corrected Cadl type usage for OpenAI.Inference
    
    * Fix camel case issues.
    
    Co-authored-by: tjprescott <trpresco@microsoft.com>

--- specification/cognitiveservices/OpenAI.Inference/swagger-to-cadl.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-autorest --reset
-autorest --require=README.md --cadl-init --use=https://aka.ms/autorest.cadl --output-folder="." --input-file="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/main/specification/cognitiveservices/data-plane/AzureOpenAI/inference/preview/2022-06-01-preview/inference.json"
