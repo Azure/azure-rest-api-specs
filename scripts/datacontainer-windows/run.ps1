69a5c9d3e98e016dfc08b709020c21e90059f676
Connor Roos <croos@microsoft.com>
2023-02-08 11:18:47 -0800

    Save current work

--- scripts/datacontainer-windows/run.ps1
+++ scripts/datacontainer-windows/run.ps1
@@ -1,14 +1,21 @@
 $timestamp= Get-Date -UFormat "%A %m/%d/%Y %R %Z"
-echo "[$timestamp] Copying files to /user/data/openapispecs"
-Copy-Item -Path "/usr/data/specrepo/" -Destination "/usr/data/openapispecs/.git/" -Recurse
+echo "[$timestamp] Installing MinGit"
+Expand-Archive MinGit.zip -DestinationPath c:\MinGit
+$env:PATH = $env:PATH + ';C:\MinGit\cmd\;C:\MinGit\cmd'
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment\' -Name Path -Value $env:PATH
 
 $timestamp= Get-Date -UFormat "%A %m/%d/%Y %R %Z"
-echo "[$timestamp] Running 'git checkout [$specRetrievalGitBranch] -f'"
-git checkout $specRetrievalGitBranch -f
+echo "[$timestamp] Copying files to /user/data/openapispecs"
+Copy-Item -Path "C:\\usr\\data\\specrepo\\.git" -Destination "C:\\usr\\data\\openapispecs\\.git" -Recurse
+
+cd "C:\\usr\\data\\openapispecs\\"
+$timestamp= Get-Date -UFormat "%A %m/%d/%Y %R %Z"
+echo "[$timestamp]  Running 'git checkout [$specRetrievalGitBranch] -f'"
+git checkout master -f
 
 $timestamp= Get-Date -UFormat "%A %m/%d/%Y %R %Z"
 echo "[$timestamp] Deleting directory /usr/data/specrepo/"
-Remove-Item -Path /usr/data/specrepo/ -Recurse
+Remove-Item -Path C:\\usr\\data\\specrepo -Recurse
 
 $timestamp= Get-Date -UFormat "%A %m/%d/%Y %R %Z"
-echo "[$timestamp] Init completed successfully. OpenAPI specs are present in C:\RPSaaS\onebox\specifications."
+echo "[$timestamp] Init completed successfully. OpenAPI specs are present in C:\usr\data\openapispecs "
