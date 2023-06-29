4592ca6b1d94ea2f0bf72be8c4a8ee11ce258c0f
Albert <albertcheng@microsoft.com>
2023-06-29 16:09:47 -0700

    scoping to eng folder

--- eng/tools/TypeSpecValidation/src/TypeSpecValidation.ts
+++ eng/tools/TypeSpecValidation/src/TypeSpecValidation.ts
@@ -1,25 +1,33 @@
 import { exec } from "child_process";
-import { access } from "fs/promises"
-import { parseArgs, ParseArgsConfig } from 'node:util';
+import { access } from "fs/promises";
+import { parseArgs, ParseArgsConfig } from "node:util";
 import path from "path";
-import { simpleGit } from 'simple-git';
+import { simpleGit } from "simple-git";
 
 async function runCmd(cmd: string, cwd: string) {
-    console.log(`run command:${cmd}`)
-    const { err, stdout, stderr } = await new Promise((res) =>
+    console.log(`run command:${cmd}`);
+    const { err, stdout, stderr } = (await new Promise((res) =>
         exec(
             cmd,
-            { encoding: "utf8", maxBuffer: 1024 * 1024 * 64, cwd: cwd },
+            {
+                encoding: "utf8",
+                maxBuffer: 1024 * 1024 * 64,
+                cwd: cwd,
+            },
             (err: unknown, stdout: unknown, stderr: unknown) =>
-                res({ err: err, stdout: stdout, stderr: stderr })
+                res({
+                    err: err,
+                    stdout: stdout,
+                    stderr: stderr,
+                })
         )
-    ) as any;
+    )) as any;
     let resultString = stderr + stdout;
-    console.log("Stdout output:")
-    console.log(stdout)
+    console.log("Stdout output:");
+    console.log(stdout);
     if (stderr) {
-        console.log("Stderr output:")
-        console.log(stderr)
+        console.log("Stderr output:");
+        console.log(stderr);
     }
     if (stderr || err) {
         throw new Error(err);
@@ -30,35 +38,43 @@ async function runCmd(cmd: string, cwd: string) {
 async function checkFileExists(file: string) {
     return access(file)
         .then(() => true)
-        .catch(() => false)
+        .catch(() => false);
 }
 
 export async function main() {
     const args = process.argv.slice(2);
     const options = {
         folder: {
-            type: 'string',
-            short: 'f',
+            type: "string",
+            short: "f",
         },
     };
-    const parsedArgs = parseArgs({ args, options, allowPositionals: true } as ParseArgsConfig);
+    const parsedArgs = parseArgs({
+        args,
+        options,
+        allowPositionals: true,
+    } as ParseArgsConfig);
     const folder = parsedArgs.positionals[0];
     console.log("Running TypeSpecValidation on folder:", folder);
 
     // Verify all specs are using root level pacakge.json
-    let expected_npm_prefix = process.cwd()
-    const actual_npm_prefix = (await runCmd(`npm prefix`, folder)).trim()
+    let expected_npm_prefix = process.cwd();
+    const actual_npm_prefix = (await runCmd(`npm prefix`, folder)).trim();
     if (expected_npm_prefix !== actual_npm_prefix) {
-        console.log("ERROR: TypeSpec folders MUST NOT contain a package.json, and instead MUST rely on the package.json at repo root.")
-        throw new Error("Expected npm prefix: " + expected_npm_prefix + "\nActual npm prefix: " + actual_npm_prefix)
+        console.log(
+            "ERROR: TypeSpec folders MUST NOT contain a package.json, and instead MUST rely on the package.json at repo root."
+        );
+        throw new Error(
+            "Expected npm prefix: " +
+                expected_npm_prefix +
+                "\nActual npm prefix: " +
+                actual_npm_prefix
+        );
     }
 
     // Spec compilation check
     if (await checkFileExists(path.join(folder, "main.tsp"))) {
-        await runCmd(
-            `npx --no tsp compile . --warn-as-error`,
-            folder
-        );
+        await runCmd(`npx --no tsp compile . --warn-as-error`, folder);
     }
     if (await checkFileExists(path.join(folder, "client.tsp"))) {
         await runCmd(
@@ -68,21 +84,20 @@ export async function main() {
     }
 
     // Format parent folder to include shared files
-    await runCmd(
-        `npx tsp format ../**/*.tsp`,
-        folder
-    );
+    await runCmd(`npx tsp format ../**/*.tsp`, folder);
 
     // Verify generated swagger file is in sync with one on disk
     const git = simpleGit();
-    let gitStatusIsClean = await (await git.status(['--porcelain'])).isClean()
+    let gitStatusIsClean = await (await git.status(["--porcelain"])).isClean();
     if (!gitStatusIsClean) {
-        let gitStatus = await git.status()
-        let gitDiff = await git.diff()
-        console.log("git status")
-        console.log(gitStatus)
-        console.log("git diff")
-        console.log(gitDiff)
-        throw new Error("Generated swagger file does not match swagger file on disk")
+        let gitStatus = await git.status();
+        let gitDiff = await git.diff();
+        console.log("git status");
+        console.log(gitStatus);
+        console.log("git diff");
+        console.log(gitDiff);
+        throw new Error(
+            "Generated swagger file does not match swagger file on disk"
+        );
     }
 }
