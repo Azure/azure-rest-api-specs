7818abca64c49fe8ea2ddb5c5a939814a0f7bd9a
Albert <albertcheng@microsoft.com>
2023-07-10 17:16:50 -0700

    get typespec folder script

---
+++ eng/scripts/Get-Typespec-Folders.ps1.ps1
@@ -0,0 +1,26 @@
+[CmdletBinding()]
+param (
+  [Parameter(Position = 0, Mandatory = $true)]
+  [string] $SpecsRepoRootDirectory,
+  [Parameter(Position = 1, Mandatory = $false)]
+  [string]$TargetBranch,
+  [Parameter(Position = 2, Mandatory = $false)]
+  [string]$SourceBranch
+)
+
+$tspFiles = @()
+if ([string]::IsNullOrEmpty($TargetBranch) -or [string]::IsNullOrEmpty($SourceBranch)) {
+  $tspFiles = (Get-ChildItem -path ./specification *.tsp -Recurse).FullName -replace "$($pwd.Path)/"
+}
+else {
+  $tspFiles = git -c core.quotepath=off -c i18n.logoutputencoding=utf-8 diff "$TargetBranch"..."$SourceBranch" --name-only | Where-Object {$_.StartsWith('specification')}
+}
+
+$typeSpecFolders = @()
+foreach ($file in $tspFiles) {
+  $file -match 'specification\/[^\/]*\/' | out-null
+  $typeSpecFolders += $matches[0]
+}
+$typeSpecFolders = $typeSpecFolders | Select-Object -Unique
+
+return $typeSpecFolders
