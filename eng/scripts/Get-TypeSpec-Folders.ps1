2d36e523dd39e32bd0df34b959805b8f20dea643
Albert <albertcheng@microsoft.com>
2023-07-18 10:32:48 -0700

    adding root repo files

--- eng/scripts/Get-TypeSpec-Folders.ps1
+++ eng/scripts/Get-TypeSpec-Folders.ps1
@@ -17,7 +17,8 @@ else {
   Write-Host "git -c core.quotepath=off -c i18n.logoutputencoding=utf-8 diff --name-only `"$TargetBranch...$SourceBranch`" -- | Where-Object {`$_.StartsWith('specification')}"
   $diffFiles = git -c core.quotepath=off -c i18n.logoutputencoding=utf-8 diff --name-only `"$TargetBranch...$SourceBranch`" --
   $engFiles = $diffFiles | Where-Object {$_.StartsWith('eng')}
-  if ($engFiles) {
+  $repoRootFiles = $diffFiles | Where-Object {$_ -notmatch '/'}
+  if ($engFiles -or $repoRootFiles) {
     $tspFiles = $allTspFiles
   }
   else {
