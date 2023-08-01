3f4b7213c37116d2e58da741ebb0ee7f7e3480f5
Albert <albertcheng@microsoft.com>
2023-08-01 16:51:12 -0700

    refactor tsv

--- eng/tools/TypeSpecValidation/src/TypeSpecValidation.ts
+++ eng/tools/TypeSpecValidation/src/TypeSpecValidation.ts
@@ -7,6 +7,127 @@ import { simpleGit } from "simple-git";
 
 debug.enable("simple-git");
 
+interface TSVRule {
+  readonly name: string;
+  readonly description: string;
+  execute(folder?: string): Promise<TSVRuleResult>;
+}
+
+interface TSVRuleResult {
+  readonly success: boolean;
+  readonly stdOutput?: string;
+  readonly errorOutput?: string;
+}
+
+class NpmPrefixRule implements TSVRule {
+  readonly name = "NpmPrefix";
+  readonly description = "Verify spec is using root level package.json";
+
+  async execute(folder: string): Promise<TSVRuleResult> {
+    let expected_npm_prefix = process.cwd();
+    const actual_npm_prefix = (await runCmd(`npm prefix`, folder))[0].trim();
+
+    let success = true;
+    let stdOutput =
+      "Expected npm prefix: " +
+      expected_npm_prefix +
+      "\n" +
+      "Actual npm prefix: " +
+      actual_npm_prefix;
+    let errorOutput: string | undefined;
+
+    if (expected_npm_prefix !== actual_npm_prefix) {
+      success = false;
+      errorOutput =
+        "TypeSpec folders MUST NOT contain a package.json, and instead MUST rely on the package.json at repo root";
+    }
+
+    return {
+      success: success,
+      stdOutput: stdOutput,
+      errorOutput: errorOutput,
+    };
+  }
+}
+
+class CompileRule implements TSVRule {
+  readonly name = "Compile";
+  readonly description = "Compile TypeSpec";
+
+  async execute(folder: string): Promise<TSVRuleResult> {
+    let success = true;
+    let stdOutput = "";
+    let errorOutput: string | undefined;
+
+    if (await checkFileExists(path.join(folder, "main.tsp"))) {
+      let [std, err] = await runCmd(`npx --no tsp compile . --warn-as-error`, folder);
+      stdOutput += std;
+      if (err) {
+        success = false;
+        errorOutput += err;
+      }
+    }
+    if (await checkFileExists(path.join(folder, "client.tsp"))) {
+      let [std, err] = await runCmd(
+        `npx --no tsp compile client.tsp --no-emit --warn-as-error`,
+        folder
+      );
+      stdOutput += std;
+      if (err) {
+        success = false;
+        errorOutput += err;
+      }
+    }
+
+    return {
+      success: success,
+      stdOutput: stdOutput,
+      errorOutput: errorOutput,
+    };
+  }
+}
+
+class FormatRule implements TSVRule {
+  readonly name = "Format";
+  readonly description = "Format TypeSpec";
+
+  async execute(folder: string): Promise<TSVRuleResult> {
+    // Format parent folder to include shared files
+    let [stdOutput, errorOutput] = await runCmd(`npx tsp format ../**/*.tsp`, folder);
+
+    let success = errorOutput ? false : true;
+    return {
+      success: success,
+      stdOutput: stdOutput,
+      errorOutput: errorOutput,
+    };
+  }
+}
+
+class GitDiffRule implements TSVRule {
+  readonly name = "GitDiff";
+  readonly description = "Checks if previous rules resulted in a git diff";
+
+  async execute(): Promise<TSVRuleResult> {
+    const git = simpleGit();
+    let gitStatusIsClean = await (await git.status(["--porcelain"])).isClean();
+
+    let success = true;
+    let errorOutput: string | undefined;
+
+    if (!gitStatusIsClean) {
+      success = false;
+      errorOutput = JSON.stringify(await git.status());
+      errorOutput += await git.diff();
+    }
+
+    return {
+      success: success,
+      errorOutput: errorOutput,
+    };
+  }
+}
+
 async function runCmd(cmd: string, cwd: string) {
   console.log(`run command:${cmd}`);
   const { err, stdout, stderr } = (await new Promise((res) =>
@@ -17,17 +138,8 @@ async function runCmd(cmd: string, cwd: string) {
         res({ err: err, stdout: stdout, stderr: stderr })
     )
   )) as any;
-  let resultString = stderr + stdout;
-  console.log("Stdout output:");
-  console.log(stdout);
-  if (stderr) {
-    console.log("Stderr output:");
-    console.log(stderr);
-  }
-  if (stderr || err) {
-    throw new Error(err);
-  }
-  return resultString as string;
+
+  return [stdout, stderr + err.message] as string[];
 }
 
 async function checkFileExists(file: string) {
@@ -48,39 +160,21 @@ export async function main() {
   const folder = parsedArgs.positionals[0];
   console.log("Running TypeSpecValidation on folder:", folder);
 
-  // Verify all specs are using root level pacakge.json
-  let expected_npm_prefix = process.cwd();
-  const actual_npm_prefix = (await runCmd(`npm prefix`, folder)).trim();
-  if (expected_npm_prefix !== actual_npm_prefix) {
-    console.log(
-      "ERROR: TypeSpec folders MUST NOT contain a package.json, and instead MUST rely on the package.json at repo root."
-    );
-    throw new Error(
-      "Expected npm prefix: " + expected_npm_prefix + "\nActual npm prefix: " + actual_npm_prefix
-    );
+  let TSVRules = [new NpmPrefixRule(), new CompileRule(), new FormatRule(), new GitDiffRule()];
+  let success = true;
+  for (let i = 0; i < TSVRules.length; i++) {
+    const rule = TSVRules[i];
+    console.log("Executing rule: " + rule.name);
+    const result = await rule.execute(folder);
+    console.log(result.stdOutput);
+    if (!result.success) {
+      success = false;
+      console.log(result.errorOutput);
+    }
   }
 
-  // Spec compilation check
-  if (await checkFileExists(path.join(folder, "main.tsp"))) {
-    await runCmd(`npx --no tsp compile . --warn-as-error`, folder);
+  if (!success) {
+    return process.exit(1);
   }
-  if (await checkFileExists(path.join(folder, "client.tsp"))) {
-    await runCmd(`npx --no tsp compile client.tsp --no-emit --warn-as-error`, folder);
-  }
-
-  // Format parent folder to include shared files
-  await runCmd(`npx tsp format ../**/*.tsp`, folder);
-
-  // Verify generated swagger file is in sync with one on disk
-  const git = simpleGit();
-  let gitStatusIsClean = await (await git.status(["--porcelain"])).isClean();
-  if (!gitStatusIsClean) {
-    let gitStatus = await git.status();
-    let gitDiff = await git.diff();
-    console.log("git status");
-    console.log(gitStatus);
-    console.log("git diff");
-    console.log(gitDiff);
-    throw new Error("Generated swagger file does not match swagger file on disk");
-  }
+  return;
 }
