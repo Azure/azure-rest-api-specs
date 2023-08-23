5afe78633f375501a863602b966be62127142434
Albert Cheng <38804567+ckairen@users.noreply.github.com>
2023-08-22 17:59:01 -0700

    Update eng/tools/TypeSpecValidation/src/rules/format.ts

--- eng/tools/TypeSpecValidation/src/rules/format.ts
+++ eng/tools/TypeSpecValidation/src/rules/format.ts
@@ -10,6 +10,8 @@ export class FormatRule implements Rule {
     // Format parent folder to include shared files
 
     let [err, stdOutput, errorOutput] = await runCmd(`npx tsp format ../**/*.tsp`, folder);
+    // Failing on both err and errorOutput because of known bug in tsp format where it returns 0 on failed formatting
+    // https://github.com/microsoft/typespec/issues/2323
     let success = err || errorOutput ? false : true;
     return {
       success: success,
