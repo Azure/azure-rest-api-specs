108735d74d262a3f4b2358f0318e40db219fece8
Albert Cheng <38804567+ckairen@users.noreply.github.com>
2023-08-22 18:33:17 -0700

    Update eng/tools/TypeSpecValidation/src/rules/format.ts
    Co-authored-by: Mike Harder <mharder@microsoft.com>

--- eng/tools/TypeSpecValidation/src/rules/format.ts
+++ eng/tools/TypeSpecValidation/src/rules/format.ts
@@ -12,7 +12,7 @@ export class FormatRule implements Rule {
     let [err, stdOutput, errorOutput] = await runCmd(`npx tsp format ../**/*.tsp`, folder);
     // Failing on both err and errorOutput because of known bug in tsp format where it returns 0 on failed formatting
     // https://github.com/microsoft/typespec/issues/2323
-    let success = err || errorOutput ? false : true;
+    let success = !err && !errorOutput;
     return {
       success: success,
       stdOutput: stdOutput,
