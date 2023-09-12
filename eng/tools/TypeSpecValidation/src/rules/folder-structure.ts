fef875814860d19f9586cab2236f8340e77bfb9d
Albert <albertcheng@microsoft.com>
2023-09-12 14:05:26 -0700

    typespec folder depth limit

--- eng/tools/TypeSpecValidation/src/rules/folder-structure.ts
+++ eng/tools/TypeSpecValidation/src/rules/folder-structure.ts
@@ -21,34 +21,48 @@ export class FolderStructureRule implements Rule {
     });
 
     // Verify top level folder is lower case
-    let folderStruct = folder.split(path.sep)
+    let folderStruct = folder.split(path.sep);
     if (folderStruct[1].match(/[A-Z]/g)) {
       success = false;
-      errorOutput += `Invalid folder name. Folders under specification/ must be lower case.\n`
+      errorOutput += `Invalid folder name. Folders under specification/ must be lower case.\n`;
     }
-    
-    // Verify second level folder is capitalized after each '.'
-    if (/(^|\. *)([a-z])/g.test(folderStruct[2]) && !["data-plane", "resource-manager"].includes(folderStruct[2])) {
+
+    let packageFolder = folderStruct[folderStruct.length - 1];
+
+    // Verify package folder is at most 3 levels deep
+    if (folderStruct.length > 4) {
       success = false;
-      errorOutput += `Invalid folder name. Folders under specification/${folderStruct[1]} must be capitalized after each '.'\n`
+      errorOutput += `Please limit TypeSpec folder depth to 3 levels or less`;
+    }
+
+    // Verify second level folder is capitalized after each '.'
+    if (
+      /(^|\. *)([a-z])/g.test(packageFolder) &&
+      !["data-plane", "resource-manager"].includes(packageFolder)
+    ) {
+      success = false;
+      errorOutput += `Invalid folder name. Folders under specification/${folderStruct[1]} must be capitalized after each '.'\n`;
     }
 
     // Verify 'Shared' follows 'Management'
-    if (folderStruct[2].includes("Management") && folderStruct[2].includes("Shared")) {
-      if (!folderStruct[2].includes("Management.Shared")) {
+    if (packageFolder.includes("Management") && packageFolder.includes("Shared")) {
+      if (!packageFolder.includes("Management.Shared")) {
         success = false;
-        errorOutput += `Invalid folder name. For management libraries with a shared component, 'Shared' should follow 'Management'.`
+        errorOutput += `Invalid folder name. For management libraries with a shared component, 'Shared' should follow 'Management'.`;
       }
     }
 
     // Verify tspconfig, main.tsp, examples/
-    let containsMinStruct = await checkFileExists(path.join(folder, "main.tsp")) && await checkFileExists(path.join(folder, "examples"))
-    if (!folderStruct[2].includes("Shared")) {
-      containsMinStruct = containsMinStruct && await checkFileExists(path.join(folder, "tspconfig.yaml"))
+    let containsMinStruct =
+      (await checkFileExists(path.join(folder, "main.tsp"))) &&
+      (await checkFileExists(path.join(folder, "examples")));
+    if (!packageFolder.includes("Shared")) {
+      containsMinStruct =
+        containsMinStruct && (await checkFileExists(path.join(folder, "tspconfig.yaml")));
     }
     if (!containsMinStruct) {
       success = false;
-      errorOutput += `Invalid folder structure. Package must contain tspconfig.yaml, main.tsp, and examples folder.`
+      errorOutput += `Invalid folder structure. Package must contain tspconfig.yaml, main.tsp, and examples folder.`;
     }
 
     return {
