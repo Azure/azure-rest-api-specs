9412836147cb2d0c185e42cfe3cbfdc76c108dab
Mike Harder <mharder@microsoft.com>
2023-10-03 00:50:35 +0000

    Code cleanup

--- eng/tools/TypeSpecValidation/src/rules/linter-ruleset.ts
+++ eng/tools/TypeSpecValidation/src/rules/linter-ruleset.ts
@@ -1,69 +1,60 @@
 import { readFile } from "fs/promises";
 import { join } from "path";
 import { parse } from "yaml";
-import { checkFileExists } from "../utils.js";
 import { Rule } from "../rule.js";
 import { RuleResult } from "../rule-result.js";
 
 export class LinterRulesetRule implements Rule {
   readonly name = "LinterRuleset";
+
   readonly description =
     "Ensures each spec includes the correct linter ruleset (data-plane or management-plane)";
+
   async execute(folder: string): Promise<RuleResult> {
     let success = true;
+    let stdOutput = "";
     let errorOutput = "";
 
     const configFile = join(folder, "tspconfig.yaml");
-
-    // FolderStructure rule should ensure config file exists
-    console.assert(checkFileExists(configFile));
-
     const configText = await readFile(configFile, "utf8");
     const config = parse(configText);
 
-    try {
-      const isDataPlane = this.isDataPlane(config);
-      const requiredRuleset = isDataPlane
-        ? "@azure-tools/typespec-azure-core/all"
-        : "@azure-tools/typespec-azure-resource-manager/all";
+    const specType =
+      config.options?.["@azure-tools/typespec-autorest"]?.["azure-resource-provider-folder"];
+    stdOutput += `azure-resource-provider-folder: ${specType}\n`;
 
-      const linterExtends = config.linter?.extends;
-      if (!linterExtends?.includes(requiredRuleset)) {
-        success = false;
-        errorOutput =
-          "tspconfig.yaml must define the following property:\n" +
-          "\n" +
-          "linter:\n" +
-          "  extends:\n" +
-          `    - "${requiredRuleset}"`;
-      }
-    } catch (e) {
+    const linterExtends = config.linter?.extends;
+    stdOutput += `linter.extends: ${linterExtends}`;
+
+    let requiredRuleset = "";
+    if (specType == "data-plane") {
+      requiredRuleset = "@azure-tools/typespec-azure-core/all";
+    } else if (specType == "resource-manager") {
+      requiredRuleset = "@azure-tools/typespec-azure-core/all";
+    } else {
       success = false;
-      errorOutput += e instanceof Error ? e.message : e;
+      errorOutput +=
+        "tspconfig.yaml must define the following property:\n" +
+        "\n" +
+        "options:\n" +
+        '  "@azure-tools/typespec-autorest":\n' +
+        '    azure-resource-provider-folder: "data-plane" | "resource-manager"\n';
+    }
+
+    if (requiredRuleset && !linterExtends?.includes(requiredRuleset)) {
+      success = false;
+      errorOutput +=
+        "tspconfig.yaml must define the following property:\n" +
+        "\n" +
+        "options:\n" +
+        '  "@azure-tools/typespec-autorest":\n' +
+        '    azure-resource-provider-folder: "data-plane" | "resource-manager"';
     }
 
     return {
       success: success,
+      stdOutput: stdOutput,
       errorOutput: errorOutput,
     };
-  }
-
-  private isDataPlane(config: any): boolean {
-    const specType =
-      config.options?.["@azure-tools/typespec-autorest"]?.["azure-resource-provider-folder"];
-
-    if (specType == "data-plane") {
-      return true;
-    } else if (specType == "resource-manager") {
-      return false;
-    } else {
-      throw new Error(
-        "tspconfig.yaml must define the following property:\n" +
-          "\n" +
-          "options:\n" +
-          '  "@azure-tools/typespec-autorest":\n' +
-          '    azure-resource-provider-folder: "data-plane" | "resource-manager"',
-      );
-    }
   }
 }
