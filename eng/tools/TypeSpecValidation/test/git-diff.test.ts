a9c85beb9a681bc23548bd7e467e69506a162a6f
Mike Harder <mharder@microsoft.com>
2023-10-26 11:53:44 -0700

    [TypeSpec Validation] Refactor TsvHost (#26411)
    - Export TsvRunnerHost and TsvTestHost as classes rather than consts
    - Change test names to lower-case for consistency with typespec repo
    - Delete incomplete FolderStructure rule
    - TsvRunnerHost.gitOperation should succeed by default

--- eng/tools/TypeSpecValidation/test/git-diff.test.ts
+++ eng/tools/TypeSpecValidation/test/git-diff.test.ts
@@ -1,12 +1,11 @@
 import { GitDiffRule } from "../src/rules/git-diff.js";
-import { DefaultTsvTestHost } from "./tsv-test-host.js";
+import { TsvTestHost } from "./tsv-test-host.js";
 import { strict as assert } from "node:assert";
 
-describe("#GitDiff", function () {
-  it("Should fail if git diff produces output.", async function () {
-    let gitDiffRule = new GitDiffRule();
-    const result = await gitDiffRule.execute(DefaultTsvTestHost, "mockFolder");
-    assert(result.errorOutput);
-    assert(result.errorOutput.length !== 0);
+describe("git-diff", function () {
+  it("should succeed if git diff produces no output", async function () {
+    const result = await new GitDiffRule().execute(new TsvTestHost(), TsvTestHost.folder);
+
+    assert(result.success);
   });
 });
