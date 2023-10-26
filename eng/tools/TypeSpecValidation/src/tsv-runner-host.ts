15b65ae66658b7e5e21be192bc92e6cd8da69375
Mike Harder <mharder@microsoft.com>
2023-10-26 11:53:44 -0700

    [TypeSpec Validation] Refactor TsvHost (#26411)
    - Export TsvRunnerHost and TsvTestHost as classes rather than consts
    - Change test names to lower-case for consistency with typespec repo
    - Delete incomplete FolderStructure rule
    - TsvRunnerHost.gitOperation should succeed by default

--- eng/tools/TypeSpecValidation/src/tsv-runner-host.ts
+++ eng/tools/TypeSpecValidation/src/tsv-runner-host.ts
@@ -1,10 +1,17 @@
-import { TsvHost } from "./tsv-host.js";
+import { IGitOperation, TsvHost } from "./tsv-host.js";
 import { simpleGit } from "simple-git";
 import { runCmd, checkFileExists } from "./utils.js";
 
-export const TsvRunnerHost: TsvHost = {
-  gitOperation: simpleGit,
-  runCmd: runCmd,
-  checkFileExists: checkFileExists,
-  // TODO: Other functions that need mocks
-};
+export class TsvRunnerHost implements TsvHost {
+  checkFileExists(file: string): Promise<boolean> {
+    return checkFileExists(file);
+  }
+
+  gitOperation(folder: string): IGitOperation {
+    return simpleGit(folder);
+  }
+
+  runCmd(cmd: string, cwd: string): Promise<[Error | null, string, string]> {
+    return runCmd(cmd, cwd);
+  }
+}
