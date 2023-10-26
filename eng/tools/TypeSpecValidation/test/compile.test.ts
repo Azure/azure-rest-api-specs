f3c5259f2d2476f80d22d607500d0ef7ea9d5bfd
Mike Harder <mharder@microsoft.com>
2023-10-26 11:53:44 -0700

    [TypeSpec Validation] Refactor TsvHost (#26411)
    - Export TsvRunnerHost and TsvTestHost as classes rather than consts
    - Change test names to lower-case for consistency with typespec repo
    - Delete incomplete FolderStructure rule
    - TsvRunnerHost.gitOperation should succeed by default

--- eng/tools/TypeSpecValidation/test/compile.test.ts
+++ eng/tools/TypeSpecValidation/test/compile.test.ts
@@ -1,15 +1,10 @@
-// import { error } from "node:console";
 import { CompileRule } from "../src/rules/compile.js";
-import { TsvTestHostGenerator } from "./tsv-test-host.js";
+import { TsvTestHost } from "./tsv-test-host.js";
 import { strict as assert } from "node:assert";
-describe("#Compile", function () {
-  it("Should fail if project cannot compile.", async function () {
-    let compileRule = new CompileRule();
-    let TsvTestHost = TsvTestHostGenerator({
-      runCmd: Promise.resolve([null, "success", "success"]),
-    });
-    const result = await compileRule.execute(TsvTestHost, "mockFolder");
+describe("compile", function () {
+  it("should succeed if project can compile", async function () {
+    const result = await new CompileRule().execute(new TsvTestHost(), TsvTestHost.folder);
+
     assert(result.success);
-    assert(result.stdOutput === "successsuccess");
   });
 });
