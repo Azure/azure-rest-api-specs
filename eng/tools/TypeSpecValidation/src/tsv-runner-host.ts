18be4423aceba737ced05e790a49314af2d04890
Mike Harder <mharder@microsoft.com>
2023-10-26 09:21:04 +0000

    [TypeSpec Validation] Refactor TestHost

--- eng/tools/TypeSpecValidation/src/tsv-runner-host.ts
+++ eng/tools/TypeSpecValidation/src/tsv-runner-host.ts
@@ -1,10 +1,23 @@
-import { TsvHost } from "./tsv-host.js";
+import { join } from "path";
+import { readFile } from "fs/promises";
+import { IGitOperation, TsvHost } from "./tsv-host.js";
 import { simpleGit } from "simple-git";
 import { runCmd, checkFileExists } from "./utils.js";
 
-export const TsvRunnerHost: TsvHost = {
-  gitOperation: simpleGit,
-  runCmd: runCmd,
-  checkFileExists: checkFileExists,
-  // TODO: Other functions that need mocks
-};
+export class TsvRunnerHost implements TsvHost {
+  checkFileExists(file: string): Promise<boolean> {
+    return checkFileExists(file);
+  }
+
+  gitOperation(folder: string): IGitOperation {
+    return simpleGit(folder);
+  }
+
+  readTspConfig(folder: string): Promise<string> {
+    return readFile(join(folder, "tspconfig.yaml"), "utf-8");
+  }
+
+  runCmd(cmd: string, cwd: string): Promise<[Error | null, string, string]> {
+    return runCmd(cmd, cwd);
+  }
+}
--- eng/tools/TypeSpecValidation/test/compile.test.ts
+++ eng/tools/TypeSpecValidation/test/compile.test.ts
@@ -1,14 +1,13 @@
-// import { error } from "node:console";
 import { CompileRule } from "../src/rules/compile.js";
-import { TsvTestHostGenerator } from "./tsv-test-host.js";
+import { TsvTestHost } from "./tsv-test-host.js";
 import { strict as assert } from "node:assert";
-describe("#Compile", function () {
-  it("Should fail if project cannot compile.", async function () {
-    let compileRule = new CompileRule();
-    let TsvTestHost = TsvTestHostGenerator({
-      runCmd: Promise.resolve([null, "success", "success"]),
-    });
-    const result = await compileRule.execute(TsvTestHost, "mockFolder");
+describe("compile", function () {
+  it("should succeed if project can compile", async function () {
+    let host = new TsvTestHost();
+    host.runCmd = async (_cmd: string, _cwd: string) => [null, "success", "success"];
+
+    const result = await new CompileRule().execute(host, TsvTestHost.folder);
+
     assert(result.success);
     assert(result.stdOutput === "successsuccess");
   });
