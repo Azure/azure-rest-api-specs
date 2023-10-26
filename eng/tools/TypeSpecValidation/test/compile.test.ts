6581ab77de6e236a00bf219ebcfc82c8d8756bc5
Mike Harder <mharder@microsoft.com>
2023-10-26 07:45:48 +0000

    Fix compile test

--- eng/tools/TypeSpecValidation/test/compile.test.ts
+++ eng/tools/TypeSpecValidation/test/compile.test.ts
@@ -3,12 +3,10 @@ import { TsvTestHost } from "./tsv-test-host.js";
 import { strict as assert } from "node:assert";
 describe("#Compile", function () {
   it("Should succeed if project can compile.", async function () {
-    class Host extends TsvTestHost {}
-
     let host = new TsvTestHost();
     host.runCmd = async (_cmd: string, _cwd: string) => [null, "success", "success"];
 
-    const result = await new CompileRule().execute(new Host(), TsvTestHost.folder);
+    const result = await new CompileRule().execute(host, TsvTestHost.folder);
 
     assert(result.success);
     assert(result.stdOutput === "successsuccess");
