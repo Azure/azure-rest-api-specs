5cd7a963ba9d8fb555dd80ba13e659d302a6c60e
David Wilson <david@daviwil.com>
2023-12-17 21:21:59 +0200

    TEMPORARY: Add merge-swaggers.js for diagnostic use

---
+++ specification/chaos/merge-swaggers.js
@@ -0,0 +1,95 @@
+const fs = require("fs");
+const path = require("path");
+
+// Function to merge OpenAPI specifications
+function mergeOpenAPISpecs(filePaths, basePath = ".") {
+  let mergedSpec = {};
+
+  filePaths.forEach((filePath) => {
+    let rawData = fs.readFileSync(path.resolve(basePath, filePath));
+    let spec = JSON.parse(rawData);
+
+    mergedSpec = mergeObjects(mergedSpec, spec);
+  });
+
+  return mergedSpec;
+}
+
+// Utility function to merge two objects
+function mergeObjects(obj1, obj2) {
+  for (let key in obj2) {
+    if (obj2.hasOwnProperty(key)) {
+      if (
+        typeof obj2[key] === "object" &&
+        obj2[key] !== null &&
+        !(obj2[key] instanceof Array)
+      ) {
+        if (!obj1[key]) {
+          obj1[key] = {};
+        }
+        mergeObjects(obj1[key], obj2[key]);
+      } else {
+        obj1[key] = obj2[key];
+      }
+    }
+  }
+  return obj1;
+}
+
+// Function to sort components of the OpenAPI specification
+function sortObjectByKeys(obj) {
+  if (Array.isArray(obj)) {
+    // Sort arrays directly
+    obj.sort();
+  } else if (typeof obj === "object" && obj !== null) {
+    // Sort other objects
+    let sortedObj = {};
+    Object.keys(obj)
+      .sort(function (a, b) {
+        return a.toLowerCase().localeCompare(b.toLowerCase());
+      })
+      .forEach((key) => {
+        sortedObj[key] = sortObjectByKeys(obj[key]);
+      });
+
+    return sortedObj;
+  }
+
+  return obj;
+}
+
+const basePath = "./resource-manager/Microsoft.Chaos/stable/2023-11-01";
+
+// Example usage
+let mergedSpec = mergeOpenAPISpecs(
+  [
+    "capabilities.json",
+    "capabilityTypes.json",
+    "experiments.json",
+    "operationStatuses.json",
+    "operations.json",
+    "targetTypes.json",
+    "targets.json",
+    "types/capabilities.json",
+    "types/capabilityTypes.json",
+    "types/common.json",
+    "types/experiments.json",
+    "types/targetTypes.json",
+    "types/targets.json",
+  ],
+  basePath,
+);
+
+let sortedSpec = sortObjectByKeys(mergedSpec);
+fs.writeFileSync(
+  path.resolve("./", "openapi-merged.json"),
+  JSON.stringify(sortedSpec, null, 2),
+);
+
+let rawData = fs.readFileSync(path.resolve(basePath, "openapi.json"));
+let newSpec = JSON.parse(rawData);
+let newSortedSpec = sortObjectByKeys(newSpec);
+fs.writeFileSync(
+  path.resolve("./", "openapi-sorted.json"),
+  JSON.stringify(newSortedSpec, null, 2),
+);
