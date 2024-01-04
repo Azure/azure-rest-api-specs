82cb1212847a1a6fcbba713e80c3b0f40c22729b
Mike Harder <mharder@microsoft.com>
2024-01-04 03:00:29 +0000

    Check file contents and path in main

--- eng/scripts/Require-TypeSpec.ps1
+++ eng/scripts/Require-TypeSpec.ps1
@@ -6,7 +6,9 @@ Set-StrictMode -Version 3
 . $PSScriptRoot/ChangedFiles-Functions.ps1
 . $PSScriptRoot/Logging-Functions.ps1
 
+# Example: /home/username/azure-rest-api-specs
 $repoPath = Resolve-Path "$PSScriptRoot/../.."
+
 $pathsWithErrors = @()
 
 $filesToCheck = @(Get-ChangedSwaggerFiles)
@@ -14,18 +16,38 @@ if (!$filesToCheck) {
     LogInfo "No specification files found to check."
 }
 else {
+    # Example: specification/foo/resource-manager/Microsoft.Foo/stable/2023-01-01/Foo.json
     foreach ($file in $filesToCheck) {
         LogInfo "Checking $repoPath/$file"
 
-        # ToDo: If the swagger file is in a "new service" (defined as
-        # "not having a API version in a stable folder in public/main"),
-        # and the swagger does not contain "x-typespec-generated", the
-        # path is in error.
+        $jsonContent = Get-Content $repoPath/$file | ConvertFrom-Json -AsHashtable
 
-        # npx --no -- prettier --check $repoPath/$file --log-level warn
-        # if ($LASTEXITCODE) {
-        #     $pathsWithErrors += $file
-        # }
+        if ($null -ne $jsonContent["info"]["x-typespec-generated"]) {          
+          LogInfo "  Contains '/info/x-typespec-generated', so spec is using TypeSpec."
+          # Skip future checks for perf
+          continue
+        }
+        else {
+          LogInfo "  Does not contain '/info/x-typespec-generated', so spec is not using TypeSpec."
+        }
+
+        # Example: specification/foo/resource-manager/Microsoft.Foo
+        # ToDo: Support Windows paths       
+        $pathToServiceName = ($file -split '/')[0..3] -join '/'
+
+        $urlToStableFolder = "https://github.com/Azure/azure-rest-api-specs/tree/main/$pathToServiceName/stable"
+
+        $response = Invoke-WebRequest -Uri $urlToStableFolder -Method Head -SkipHttpErrorCheck
+        if ($response.StatusCode -eq 404) {
+          LogInfo "  Main does not contain path '$pathToServiceName/stable', so spec is new and must use TypeSpec."
+          $pathsWithErrors += $file
+        }
+        elseif ($response.StatusCode -eq 200) {
+          LogInfo "  Main contains path '$pathToServiceName/stable', so spec already exists and is NOT required to use TypeSpec."
+        }
+        else {
+          LogError "  Unexpected response from ${urlToStableFolder}: ${response.StatusCode}"
+        }
     }
 }
 
