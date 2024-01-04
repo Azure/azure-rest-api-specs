70e0a7a97938b81031de0966f6dd4937d151135c
Mike Harder <mharder@microsoft.com>
2024-01-04 06:39:23 +0000

    Improve powershell code and error messages

--- eng/scripts/TypeSpec-Requirement.ps1
+++ eng/scripts/TypeSpec-Requirement.ps1
@@ -6,49 +6,55 @@ Set-StrictMode -Version 3
 . $PSScriptRoot/ChangedFiles-Functions.ps1
 . $PSScriptRoot/Logging-Functions.ps1
 
-# Example: /home/username/azure-rest-api-specs
-$repoPath = Resolve-Path "$PSScriptRoot/../.."
-
 $pathsWithErrors = @()
 
 $filesToCheck = @(Get-ChangedSwaggerFiles)
 if (!$filesToCheck) {
-    LogInfo "No specification files found to check."
+  LogInfo "No swagger files found to check"
 }
 else {
-    # Example: specification/foo/resource-manager/Microsoft.Foo/stable/2023-01-01/Foo.json
-    foreach ($file in $filesToCheck) {
-        LogInfo "Checking $repoPath/$file"
+  # Example: specification/foo/resource-manager/Microsoft.Foo/stable/2023-01-01/Foo.json
+  # - Forward slashes on both Linux and Windows
+  foreach ($file in $filesToCheck) {
+    LogInfo "Checking $file"
 
-        $jsonContent = Get-Content $repoPath/$file | ConvertFrom-Json -AsHashtable
+    $jsonContent = Get-Content $file | ConvertFrom-Json -AsHashtable
 
-        if ($null -ne $jsonContent["info"]["x-typespec-generated"]) {          
-          LogInfo "  Contains '/info/x-typespec-generated', so spec is using TypeSpec."
-          # Skip future checks for perf
-          continue
-        }
-        else {
-          LogInfo "  Does not contain '/info/x-typespec-generated', so spec is not using TypeSpec."
-        }
-
-        # Example: specification/foo/resource-manager/Microsoft.Foo
-        # ToDo: Support Windows paths       
-        $pathToServiceName = ($file -split '/')[0..3] -join '/'
-
-        $urlToStableFolder = "https://github.com/Azure/azure-rest-api-specs/tree/main/$pathToServiceName/stable"
-
-        $response = Invoke-WebRequest -Uri $urlToStableFolder -Method Head -SkipHttpErrorCheck
-        if ($response.StatusCode -eq 404) {
-          LogInfo "  Main does not contain path '$pathToServiceName/stable', so spec is new and must use TypeSpec."
-          $pathsWithErrors += $file
-        }
-        elseif ($response.StatusCode -eq 200) {
-          LogInfo "  Main contains path '$pathToServiceName/stable', so spec already exists and is NOT required to use TypeSpec."
-        }
-        else {
-          LogError "  Unexpected response from ${urlToStableFolder}: ${response.StatusCode}"
-        }
+    if ($null -ne $jsonContent["info"]["x-typespec-generated"]) {
+      LogInfo "  Swagger was generated from TypeSpec (contains '/info/x-typespec-generated')"
+      # Skip future checks for perf
+      continue
     }
+    else {
+      LogInfo "  Swagger was not generated from TypeSpec (missing '/info/x-typespec-generated')"
+    }
+
+    # Example: specification/foo/resource-manager/Microsoft.Foo
+    $pathToServiceName = ($file -split '/')[0..3] -join '/'
+
+    $urlToStableFolder = "https://github.com/Azure/azure-rest-api-specs/tree/main/$pathToServiceName/stable"
+
+    try {
+      $response = Invoke-WebRequest -Uri $urlToStableFolder -Method Head -SkipHttpErrorCheck
+      if ($response.StatusCode -eq 200) {
+        LogInfo "  Main contains path '$pathToServiceName/stable', so spec already exists and is not required to use TypeSpec"
+      }
+      elseif ($response.StatusCode -eq 404) {
+        LogInfo "  Main does not contain path '$pathToServiceName/stable', so spec is new and must use TypeSpec"
+        $pathsWithErrors += $file
+      }
+      else {
+        LogError "Unexpected response from ${urlToStableFolder}: ${response.StatusCode}"
+        LogJobFailure
+        exit 1
+      }
+    }
+    catch {
+      LogError "  Exception making web request to ${urlToStableFolder}: $_"
+      LogJobFailure
+      exit 1
+    }
+  }
 }
 
 if ($pathsWithErrors.Count -gt 0)
@@ -60,7 +66,7 @@ if ($pathsWithErrors.Count -gt 0)
 
   foreach ($path in $pathsWithErrors)
   {
-    LogErrorForFile $path "New spec is not using TypeSpec."
+    LogErrorForFile $path "Swagger was not generated from TypeSpec, and spec appears to be new"
   }
   exit 1
 }
