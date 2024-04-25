d209947314aa87baac318afa79bf015c673dcb59
Zhenhua Hu <zhhu@microsoft.com>
2024-04-26 07:15:14 +0800

    add .tf file

---
+++ specification/purview/resource-manager/Microsoft.Purview/stable/2021-12-01/terraform/Microsoft.Purview/main.tf
@@ -0,0 +1,149 @@
+terraform {
+  required_providers {
+    azapi = {
+      source = "Azure/azapi"
+    }
+  }
+}
+
+provider "azapi" {
+  skip_provider_registration = false
+}
+
+variable "resource_name" {
+  type    = string
+  default = "acctest0001"
+}
+
+variable "location" {
+  type    = string
+  default = "westeurope"
+}
+
+resource "azapi_resource" "resourceGroup" {
+  type     = "Microsoft.Resources/resourceGroups@2020-06-01"
+  name     = var.resource_name
+  location = var.location
+}
+
+resource "azapi_resource" "virtualNetwork" {
+  type      = "Microsoft.Network/virtualNetworks@2022-07-01"
+  parent_id = azapi_resource.resourceGroup.id
+  name      = var.resource_name
+  location  = var.location
+  body = jsonencode({
+    properties = {
+      addressSpace = {
+        addressPrefixes = [
+          "10.0.0.0/16",
+        ]
+      }
+      dhcpOptions = {
+        dnsServers = [
+        ]
+      }
+      subnets = [
+      ]
+    }
+  })
+  schema_validation_enabled = false
+  response_export_values    = ["*"]
+  ignore_body_changes       = ["properties.subnets"]
+}
+
+resource "azapi_resource" "subnet" {
+  type      = "Microsoft.Network/virtualNetworks/subnets@2022-07-01"
+  parent_id = azapi_resource.virtualNetwork.id
+  name      = var.resource_name
+  body = jsonencode({
+    properties = {
+      addressPrefix = "10.0.2.0/24"
+      delegations = [
+      ]
+      privateEndpointNetworkPolicies    = "Enabled"
+      privateLinkServiceNetworkPolicies = "Enabled"
+      serviceEndpointPolicies = [
+      ]
+      serviceEndpoints = [
+      ]
+    }
+  })
+  schema_validation_enabled = false
+  response_export_values    = ["*"]
+}
+
+resource "azapi_resource" "networkInterface" {
+  type      = "Microsoft.Network/networkInterfaces@2022-07-01"
+  parent_id = azapi_resource.resourceGroup.id
+  name      = var.resource_name
+  location  = var.location
+  body = jsonencode({
+    properties = {
+      enableAcceleratedNetworking = false
+      enableIPForwarding          = false
+      ipConfigurations = [
+        {
+          name = "testconfiguration1"
+          properties = {
+            primary                   = true
+            privateIPAddressVersion   = "IPv4"
+            privateIPAllocationMethod = "Dynamic"
+            subnet = {
+              id = azapi_resource.subnet.id
+            }
+          }
+        },
+      ]
+    }
+  })
+  schema_validation_enabled = false
+  response_export_values    = ["*"]
+}
+
+resource "azapi_resource" "virtualMachine" {
+  type      = "Microsoft.Compute/virtualMachines@2023-03-01"
+  parent_id = azapi_resource.resourceGroup.id
+  name      = var.resource_name
+  location  = var.location
+  body = jsonencode({
+    properties = {
+      hardwareProfile = {
+        vmSize = "Standard_F2"
+      }
+      networkProfile = {
+        networkInterfaces = [
+          {
+            id = azapi_resource.networkInterface.id
+            properties = {
+              primary = false
+            }
+          },
+        ]
+      }
+      osProfile = {
+        adminPassword = "Password1234!"
+        adminUsername = "testadmin"
+        computerName  = "hostname230630032848831819"
+        linuxConfiguration = {
+          disablePasswordAuthentication = false
+        }
+      }
+      storageProfile = {
+        imageReference = {
+          offer     = "UbuntuServer"
+          publisher = "Canonical"
+          sku       = "16.04-LTS"
+          version   = "latest"
+        }
+        osDisk = {
+          caching                 = "ReadWrite"
+          createOption            = "FromImage"
+          name                    = "myosdisk1"
+          writeAcceleratorEnabled = false
+        }
+      }
+    }
+  })
+  schema_validation_enabled = false
+  response_export_values    = ["*"]
+}
