adc706183c899b75c1f5f599547b36ef594b4cc7
Mike Harder <mharder@microsoft.com>
2024-05-01 06:17:41 +0000

    Add usage help and start implementing

--- eng/tools/suppressions/src/index.ts
+++ eng/tools/suppressions/src/index.ts
@@ -1,3 +1,29 @@
+import { resolve } from "path";
+
 export async function main() {
-  console.log(`get-suppressions(${process.argv.slice(2)})`);
+  const args = process.argv.slice(2);
+
+  if (args.length === 2) {
+    const tool = args[0];
+    const path = args[1];
+    await getSuppressions(tool, resolve(path));
+  } else {
+    console.log("  Usage: npx get-suppressions <tool-name> <path-to-file>");
+    console.log(
+      "Returns: JSON array of suppressions, with specified tool name, applying to file (may be empty)",
+    );
+    console.log();
+    console.log(
+      "Example: npx get-suppressions TypeSpecRequirement specification/foo/data-plane/Foo/stable/2023-01-01/Foo.json",
+    );
+    console.log(
+      'Returns: [{"tool":"TypeSpecRequirement","path":"/home/user/.../data-plane/**/*.json","reason":"foo"}]',
+    );
+  }
 }
+
+async function getSuppressions(tool: string, path: string) {
+  console.log(`getSuppressions(${tool}, ${path})`);
+}
+
+// async function findSuppressionsYaml(file: string) {}
