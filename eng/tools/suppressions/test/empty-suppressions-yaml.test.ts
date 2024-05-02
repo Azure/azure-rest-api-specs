d279cd4c4e5ad1e08ee85621f6fd2976e5bd4c5f
Mike Harder <mharder@microsoft.com>
2024-05-02 23:56:44 +0000

    Simplify to one test file

--- eng/tools/suppressions/test/empty-suppressions-yaml.test.ts
+++
@@ -1,8 +0,0 @@
-// Placeholder to generate dist/src and dist/test folders
-import { expect, test } from "vitest";
-import { _getSuppressionsFromYaml } from "../src/index.js";
-
-test("empty suppressions.yaml", () => {
-  const suppressions = _getSuppressionsFromYaml("TestTool", "test.json", "suppressions.yaml", "");
-  expect(suppressions).toEqual([]);
-});
--- eng/tools/suppressions/test/index.test.ts
+++ eng/tools/suppressions/test/index.test.ts
@@ -2,6 +2,21 @@
 import { expect, test } from "vitest";
 import { _getSuppressionsFromYaml } from "../src/index.js";
 
+test("empty suppressions.yaml", () => {
+  const suppressions = _getSuppressionsFromYaml("TestTool", "test.json", "suppressions.yaml", "");
+  expect(suppressions).toEqual([]);
+});
+
+test("one suppression no match", () => {
+  const suppressions = _getSuppressionsFromYaml(
+    "TestTool",
+    "foo.json",
+    "suppressions.yaml",
+    '- tool: TestTool\n  path: "bar.json"\n  reason: test',
+  );
+  expect(suppressions).toEqual([]);
+});
+
 test("one suppression match", () => {
   const suppressions = _getSuppressionsFromYaml(
     "TestTool",
