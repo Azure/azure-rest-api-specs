dd48b66a06e0d8a4e959ff55cbd13d09a7f248e0
Mike Harder <mharder@microsoft.com>
2024-05-07 01:28:35 +0000

    Use shared component get-suppressions - Aligns with #28927

--- eng/scripts/Armstrong-Validation.ps1
+++ eng/scripts/Armstrong-Validation.ps1
@@ -10,73 +10,16 @@ Set-StrictMode -Version 3
 . $PSScriptRoot/ChangedFiles-Functions.ps1
 . $PSScriptRoot/Logging-Functions.ps1
 
-$script:psYamlInstalled = $false
-function Ensure-PowerShell-Yaml-Installed {
-  if ($script:psYamlInstalled) {
-    # If already checked once in this script, don't log anything further
-    return;
-  }
-
-  $script:psYamlInstalled = [bool] (Get-Module -ListAvailable -Name powershell-yaml | Where-Object { $_.Version -eq "0.4.7" })
-
-  if ($script:psYamlInstalled) {
-    LogInfo "Module powershell-yaml@0.4.7 already installed"
-  }
-  else {
-    LogInfo "Installing module powershell-yaml@0.4.7"
-    Install-Module -Name powershell-yaml -RequiredVersion 0.4.7 -Force -Scope CurrentUser
-    $script:psYamlInstalled = $true
-  }
-}
-
-function Find-Suppressions-Yaml {
-  param (
-    [string]$fileInSpecFolder
-  )
-
-  $currentDirectory = Get-Item (Split-Path -Path $fileInSpecFolder)
-
-  while ($currentDirectory) {
-    $suppressionsFile = Join-Path -Path $currentDirectory.FullName -ChildPath "suppressions.yaml"
-
-    if (Test-Path $suppressionsFile) {
-      return $suppressionsFile
-    }
-    else {
-      $currentDirectory = $currentDirectory.Parent
-    }
-  }
-
-  return $null
-}
-
 function Get-Suppression {
   param (
     [string]$fileInSpecFolder
   )
 
-  $suppressionsFile = Find-Suppressions-Yaml $fileInSpecFolder
-  if ($suppressionsFile) {
-    Ensure-PowerShell-Yaml-Installed
+  # -NoEnumerate to prevent single-element arrays from being collapsed to a single object
+  # -AsHashtable is closer to raw JSON than PSCustomObject
+  $suppressions = npx get-suppressions ArmstrongValidation $fileInSpecFolder | ConvertFrom-Json -NoEnumerate -AsHashtable
 
-    $suppressions = Get-Content -Path $suppressionsFile -Raw | ConvertFrom-Yaml
-    foreach ($suppression in $suppressions) {
-      $tool = $suppression["tool"]
-      $path = $suppression["path"]
-
-      if ($tool -eq "ArmstrongValidation") {
-        # Paths in suppressions.yml are relative to the file itself
-        $fullPath = Join-Path -Path (Split-Path -Path $suppressionsFile) -ChildPath $path
-
-        # If path is not specified, suppression applies to all files
-        if (!$path -or ($fileInSpecFolder -like $fullPath)) {
-          return $suppression
-        }
-      }
-    }
-  }
-
-  return $null
+  return $suppressions ? $suppressions[0] : $null
 }
 
 function Get-ChangedTerraformFiles($changedFiles = (Get-ChangedFiles)) {
