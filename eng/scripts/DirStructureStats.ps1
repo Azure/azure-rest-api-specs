d8ea9d8786dd6c2d24b5f260d29fe7d565f45245
Konrad Jamrozik <kojamroz@microsoft.com>
2024-06-04 00:48:11 -0700

    ongoing

---
+++ eng/scripts/DirStructureStats.ps1
@@ -0,0 +1,24 @@
+<#
+.SYNOPSIS
+This file contains scripts that can be used as a starting point for specs directory structure analysis.
+
+#>
+
+
+function Get-AllSpecDirsPaths(
+    [ValidateScript({Test-Path $_})]
+    [String] $Path="../../specification") {
+
+    return Get-ChildItem -Path $Path -Recurse -Directory | ForEach-Object {
+        Write-Output $_.FullName
+    }
+}
+
+function Get-AllReadmeMdFilesPaths(
+    [ValidateScript({Test-Path $_})]
+    [String] $Path= "../../specification") {
+
+    return Get-ChildItem -Path $Path -Recurse -File -Filter "readme.md" | ForEach-Object {
+        Write-Output $_.FullName
+    }
+}
