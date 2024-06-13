7975cd743a426aa44af91492e2d5dd4c728bb3ba
Jiahui Li <jiahuili@microsoft.com>
2024-06-13 10:50:36 -0700

    remove

--- documentation/release-request/rules-for-release-request.md
+++
@@ -1,3 +0,0 @@
-# How to ask for official SDK 
-
-For information on how to make an SDK release see our official documentation in [azsdk/releases/partnerinfo](https://aka.ms/azsdk/releases/partnerinfo).
--- eng/tools/typespec-validation/src/rule.ts
+++
@@ -1,8 +0,0 @@
-import { RuleResult } from "./rule-result.js";
-import { TsvHost } from "./tsv-host.js";
-
-export interface Rule {
-  readonly name: string;
-  readonly description: string;
-  execute(host?: TsvHost, folder?: string): Promise<RuleResult>;
-}
