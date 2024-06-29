05f2d46eff66e19b4be5ce5acd9e1d402191fd85
Mike Harder <mharder@microsoft.com>
2024-06-29 08:01:15 +0000

    Add concurrent tests

--- eng/tools/typespec-requirement/test/typespec-requirement.test.ts
+++ eng/tools/typespec-requirement/test/typespec-requirement.test.ts
@@ -1,5 +1,22 @@
-import { expect, test } from "vitest";
+import { execa } from 'execa';
+import { join } from 'path';
+import { test } from 'vitest';
 
-test("true is true", () => {
-  expect(true).toEqual(true);
+async function checkAllUnder(path: string) {
+  const typeSpecRequirementPS1 = join(__dirname, '..', '..', '..', 'scripts', 'TypeSpec-Requirement.ps1')
+  return await execa("pwsh", ["-File", typeSpecRequirementPS1, "-CheckAllUnder", path]);
+}
+
+test.concurrent("No files to check", async ({ expect }) => {
+  const { stdout, exitCode } = await checkAllUnder("specification/empty");
+
+  expect(stdout).toMatchInlineSnapshot(`"No OpenAPI files found to check"`);
+  expect(exitCode).toBe(0);
+});
+
+test.concurrent("No files to check 2", async ({ expect }) => {
+  const { stdout, exitCode } = await checkAllUnder("specification/empty");
+
+  expect(stdout).toMatchInlineSnapshot(`"No OpenAPI files found to check"`);
+  expect(exitCode).toBe(0);
 });
