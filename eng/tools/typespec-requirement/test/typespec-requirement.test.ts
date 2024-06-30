e17cf79349a4b74c6a7ef89afc70a808fbc8fcfa
Mike Harder <mharder@microsoft.com>
2024-06-30 05:35:47 +0000

    Add test for suppressions

--- eng/tools/typespec-requirement/test/typespec-requirement.test.ts
+++ eng/tools/typespec-requirement/test/typespec-requirement.test.ts
@@ -3,8 +3,10 @@ import { join } from 'path';
 import { test } from 'vitest';
 
 async function checkAllUnder(path: string) {
-  const typeSpecRequirementPS1 = join(__dirname, '..', '..', '..', 'scripts', 'TypeSpec-Requirement.ps1')
-  return await execa("pwsh", ["-File", typeSpecRequirementPS1, "-CheckAllUnder", path]);
+  const repoRoot = join(__dirname, '..', '..', '..', '..');
+  return await execa("pwsh",
+    ["-File", join('eng', 'scripts', 'TypeSpec-Requirement.ps1'), "-CheckAllUnder", join(__dirname, path)],
+    { cwd: repoRoot });
 }
 
 test.concurrent("No files to check", async ({ expect }) => {
@@ -14,9 +16,9 @@ test.concurrent("No files to check", async ({ expect }) => {
   expect(exitCode).toBe(0);
 });
 
-test.concurrent("No files to check 2", async ({ expect }) => {
-  const { stdout, exitCode } = await checkAllUnder("specification/empty");
+test.concurrent("Suppression", async ({ expect }) => {
+  const { stdout, exitCode } = await checkAllUnder("specification/suppression");
 
-  expect(stdout).toMatchInlineSnapshot(`"No OpenAPI files found to check"`);
+  expect(stdout).contains("Suppressed");
   expect(exitCode).toBe(0);
 });
