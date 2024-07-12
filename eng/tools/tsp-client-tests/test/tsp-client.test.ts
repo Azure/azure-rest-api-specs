605ac4ea1bb86e0ca0d8fbabe5783e057cb4e926
Mike Harder <mharder@microsoft.com>
2024-07-12 22:40:24 +0000

    Add test for keyvault/data-plane

--- eng/tools/tsp-client-tests/test/tsp-client.test.ts
+++ eng/tools/tsp-client-tests/test/tsp-client.test.ts
@@ -1,16 +1,51 @@
-import { execa } from 'execa';
-import { join } from 'path';
-import { test } from 'vitest';
+import { execa } from "execa";
+import { mkdir, rm } from "fs/promises";
+import { dirname, join } from "path";
+import { ExpectStatic, test } from "vitest";
+
+const repoRoot = join(__dirname, "..", "..", "..", "..");
 
 async function tspClient(...args: string[]) {
-  const repoRoot = join(__dirname, '..', '..', '..', '..');
-  const execArgs = ["exec", "--no", "--", "tsp-client"]
-  return await execa("npm", execArgs.concat(args), {cwd: repoRoot, reject: false});
+  const execArgs = ["exec", "--no", "--", "tsp-client"];
+  return await execa("npm", execArgs.concat(args), { cwd: repoRoot, reject: false });
+}
+
+async function convert(expect: ExpectStatic, readme: string) {
+  const specFolder = dirname(dirname(join(repoRoot, readme)));
+  const outputFolder = join(specFolder, "Test.TspClientConvert");
+
+  try {
+    await mkdir(outputFolder);
+  } catch {
+    // Delete and retry
+    await rm(outputFolder, { recursive: true, force: true });
+    await mkdir(outputFolder);
+  }
+
+  try {
+    const { stdout, exitCode } = await tspClient(
+      "convert",
+      "--no-prompt",
+      "--swagger-readme",
+      readme,
+      "-o",
+      outputFolder,
+    );
+
+    expect(stdout).toContain("Converting");
+    expect(exitCode).toBe(0);
+  } finally {
+    await rm(outputFolder, { recursive: true, force: true });
+  }
 }
 
 test.concurrent("Usage", async ({ expect }) => {
   const { stdout, exitCode } = await tspClient();
-  
+
   expect(stdout).toContain("Usage");
   expect(exitCode).not.toBe(0);
+});
+
+test.concurrent("Convert keyvault/data-plane", async ({ expect }) => {
+  await convert(expect, "specification/keyvault/data-plane/readme.md");
 });
