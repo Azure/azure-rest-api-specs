4fc3917aea3a9f5e18df3c4a689aa69080272ddf
Mike Harder <mharder@microsoft.com>
2024-07-12 22:05:44 +0000

    Add usage test

--- eng/tools/tsp-client-tests/test/tsp-client.test.ts
+++ eng/tools/tsp-client-tests/test/tsp-client.test.ts
@@ -1,5 +1,16 @@
+import { execa } from 'execa';
+import { join } from 'path';
 import { test } from 'vitest';
 
-test.concurrent("True", async ({ expect }) => {
-  expect(true).toBeTruthy();
+async function tspClient(...args: string[]) {
+  const repoRoot = join(__dirname, '..', '..', '..', '..');
+  const execArgs = ["exec", "--no", "--", "tsp-client"]
+  return await execa("npm", execArgs.concat(args), {cwd: repoRoot, reject: false});
+}
+
+test.concurrent("Usage", async ({ expect }) => {
+  const { stdout, exitCode } = await tspClient();
+  
+  expect(stdout).toContain("Usage");
+  expect(exitCode).not.toBe(0);
 });
