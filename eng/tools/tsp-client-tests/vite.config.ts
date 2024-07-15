35a1dcc95203e12109032fe5f0eda46e3df316a2
Mike Harder <mharder@microsoft.com>
2024-07-15 23:00:23 +0000

    Increase timeout to 2 minutes

--- eng/tools/tsp-client-tests/vite.config.ts
+++ eng/tools/tsp-client-tests/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   test: {
-    // Default timeout of 5 seconds is too low
-    testTimeout: 60000,
+    testTimeout: 120000,
   },
 });
