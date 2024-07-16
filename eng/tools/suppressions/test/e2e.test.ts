7c42b66c705c8545740ea740cb361da4ea270132
Mike Harder <mharder@microsoft.com>
2024-07-16 22:08:33 +0000

    [suppressions] Add basic end-to-end tests

---
+++ eng/tools/suppressions/test/e2e.test.ts
@@ -0,0 +1,38 @@
+import { expect, test } from "vitest";
+import { getSuppressions } from "../src/suppressions.js";
+import { join } from "path";
+
+async function getTestSuppressions(path: string, tool: string = "TestTool") {
+  return await getSuppressions(tool, join(__dirname, "e2e", path));
+}
+
+test.concurrent("no suppressions.yaml", async () => {
+  const suppressions = await getTestSuppressions("noSuppressionsYaml");
+  expect(suppressions).toEqual([]);
+});
+
+test.concurrent("empty suppressions.yaml", async () => {
+  const suppressions = await getTestSuppressions("emptySuppressionsYaml");
+  expect(suppressions).toEqual([]);
+});
+
+test.concurrent("suppress foo.json, get foo.json", async () => {
+  const suppressions = await getTestSuppressions(join("suppressFooJson", "foo.json"));
+  expect(suppressions).toEqual([
+    {
+      tool: "TestTool",
+      paths: ["foo.json"],
+      reason: "test",
+    },
+  ]);
+});
+
+test.concurrent("suppress foo.json, get bar.json", async () => {
+  const suppressions = await getTestSuppressions(join("suppressFooJson", "bar.json"));
+  expect(suppressions).toEqual([]);
+});
+
+test.concurrent("suppress foo.json, get foo.json w/ different tool", async () => {
+  const suppressions = await getTestSuppressions(join("suppressFooJson", "foo.json"), "TestTool2");
+  expect(suppressions).toEqual([]);
+});
