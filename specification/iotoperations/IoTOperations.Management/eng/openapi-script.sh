85705fbda1291d469ba2abb77a87c4ac926c4ff5
chgennar <56695749+chgennar@users.noreply.github.com>
2024-08-08 13:26:50 +0000

    Update examples

--- specification/iotoperations/IoTOperations.Management/eng/openapi-script.sh
+++ specification/iotoperations/IoTOperations.Management/eng/openapi-script.sh
@@ -66,6 +66,7 @@ for file in resource-manager/Microsoft.IoTOperations/preview/2024-08-15-preview/
     # Replace identity strings with GUIDs.
     jq 'walk(if type == "object" and .principalId? then .principalId = "4a6e4195-75b8-4685-aa0c-0b5704779327" else . end)' $file > temp.json && mv temp.json $file
     jq 'walk(if type == "object" and .tenantId? then .tenantId = "ed060aa2-71ff-4d3f-99c4-a9138356fdec" else . end)' $file > temp.json && mv temp.json $file
+    jq 'walk(if type == "object" and .clientId? then .clientId = "fb90f267-8872-431a-a76a-a1cec5d3c4d2" else . end)' $file > temp.json && mv temp.json $file
 done
 
 # Copy the examples to the management directory
