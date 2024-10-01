9fb7fe90ef66a5210f105ab8cad53b3458abd50c
Rahul Singh <rahuls@microsoft.com>
2024-09-30 21:12:30 -0700

    Revert "Updates after npx ci"
    This reverts commit bf1aed71f182bc43cfe5efd23a929a6c49912f29.

--- specification/azurefleet/resource-manager/$(typescript-sdks-folder)/sdk/computefleet/arm-computefleet/src/index.ts
+++
@@ -1,5 +0,0 @@
-/// <reference lib="esnext.asynciterable" />
-export { getContinuationToken } from "./pagingHelper";
-export * from "./models";
-export { MicrosoftAzureFleet } from "./microsoftAzureFleet";
-export * from "./operationsInterfaces";
