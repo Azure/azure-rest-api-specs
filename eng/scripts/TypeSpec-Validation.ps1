d6af6046483af885128b56afe562aadbbd83626b
Daniel Jurek <djurek@microsoft.com>
2024-10-16 21:23:09 +0000

    LogError

--- eng/scripts/TypeSpec-Validation.ps1
+++ eng/scripts/TypeSpec-Validation.ps1
@@ -17,22 +17,23 @@ $typespecFolders, $checkedAll = &"$PSScriptRoot/Get-TypeSpec-Folders.ps1" `
   -TargetCommitish:$TargetCommitish `
   -CheckAll:$CheckAll
 
+# Use -FolderCount to do performance testing on Typespec-Validation.ps1 with 
+# -CheckAll set to $true. This constrains the typeSpec folders to the first N 
+# folders. 
 if ($FolderCount) { 
   $typespecFolders = $typespecFolders[0..($FolderCount - 1)]
 }
 
-# Group typespec folders by "service" (the first folder in the path)
-# Example: 'specification/ai/DocumentIntelligence' -> 'specification/ai'
-
-$serviceFolders = $typespecFolders | Group-Object -Property { Split-Path $_ -Parent }
-
 $typespecFoldersWithFailures = @()
 if ($typespecFolders) {
-  # TODO: Does this do anything? 
-  # $typespecFolders = $typespecFolders.Split('',[System.StringSplitOptions]::RemoveEmptyEntries)
+  # Group typespec folders by "service" (the first folder in the path). This
+  # parallel validation at the level of the service.
+  # Example: 'specification/ai/DocumentIntelligence' -> 'specification/ai'.
 
+$serviceFolders = $typespecFolders | Group-Object -Property { Split-Path $_ -Parent }
   Write-Host "Starting per-service parallel validation..."
-  $serviceFolders | ForEach-Object -ThrottleLimit $Parallelism -Parallel {
+  $serviceFolders `
+  | ForEach-Object -ThrottleLimit $Parallelism -Parallel {
     $service = $_.Name
     $typespecFolders = $_.Group
 
@@ -73,35 +74,32 @@ if ($typespecFolders) {
         # TODO: Ensure this is proper and correct. Can something in service A
         # affect files in Service B? Can they impact files in the root? Worth 
         # examining.
-        git restore $typespecFolder/.. | Out-Null
-        git clean -df $typespecFolder/.. | Out-Null
+        git restore $service | Out-Null
+        git clean -df $service | Out-Null
       }
     }
   
     # Return results
     return $result
-  } | ForEach-Object { 
-    # TODO: MORE LOGGING
-    Write-Host "Validation for $($_.Service)"
+  } | ForEach-Object {
+    Write-Host "Validation for Service: $($_.Service):"
     if ($_.Suppressed.Count) { 
       Write-Host "Suppressions in $($_.Service): "
       foreach ($item in $_.Suppressed) {
         Write-Host " > $item"
       }
     }
-    Write-Host "Services:"
     foreach ($item in $_.Logs.GetEnumerator()) {
       LogGroupStart "Validation for $($item.Key)"
       $item.Value | Write-Host
       LogGroupEnd
     }
     if($_.Errors.Count) {
-      Write-Host "Errors in $($_.Service)"
+      Write-Host "Errors in $($_.Service):"
       # TODO: Ensure errors are properly surfaced in DevOps
       foreach ($item in $_.Errors.GetEnumerator()) {
         $typespecFoldersWithFailures += $item.Key
-        LogError "Failure validating spec: $($item.Key)"
-        $item.Value | Write-Host
+        LogError $item.Value
       }
     }
   }
@@ -114,7 +112,7 @@ if ($typespecFolders) {
 }
 
 if ($typespecFoldersWithFailures.Count -gt 0) {
-  LogInfo "TypeSpec Validation failed for some folder to fix run and address any errors:"
+  LogInfo "TypeSpec Validation failed for some folders. To fix run and address any errors:"
   LogInfo " > npm ci"
   foreach ($typespecFolderWithFailure in $typespecFoldersWithFailures) {
     LogInfo " > npx tsv $typespecFolderWithFailure"
