13a6e5d7c02cb5923cfc4ee4a5e5e96078e47b91
Daniel Jurek <djurek@microsoft.com>
2024-10-22 17:57:44 +0000

    Hardcode parallelism 5

--- eng/scripts/TypeSpec-Validation.ps1
+++ eng/scripts/TypeSpec-Validation.ps1
@@ -6,7 +6,7 @@ param (
   [string]$BaseCommitish = "HEAD^",
   [string]$TargetCommitish = "HEAD",
   [int]$FolderCount = 0,
-  [int] $Parallelism = $env:TSV_PARALLELISM
+  [int] $Parallelism = 5
 )
 
 . $PSScriptRoot/Logging-Functions.ps1
