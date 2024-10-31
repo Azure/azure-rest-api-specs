7d699042e1d6972780723b787d37a66efa92e1c9
Mike Harder <mharder@microsoft.com>
2024-10-31 22:26:22 +0000

    Pass owner, repo, and run_id via env vars

--- .github/scripts/get-keyvalue-artifacts.js
+++ .github/scripts/get-keyvalue-artifacts.js
@@ -2,22 +2,15 @@
 
 /**
  * @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments
- * @param {string?} owner The account owner of the repository. The name is not case sensitive.
- * @param {string?} repo The name of the repository without the .git extension. The name is not case sensitive.
- * @param {number?} run_id The unique identifier of the workflow run.
  */
-module.exports = async ({ github, context, core }, owner, repo, run_id) => {
+module.exports = async ({ github, context, core }) => {
   console.log("context: " + JSON.stringify(context, null, 2));
 
-  let params;
+  let owner = process.env.OWNER;
+  let repo = process.env.REPO;
+  let run_id = parseInt(process.env.RUN_ID || "");
 
-  if (owner && repo && run_id) {
-    params = {
-      owner: owner,
-      repo: repo,
-      run_id: run_id,
-    };
-  } else if (!owner && !repo && !run_id) {
+  if (!owner && !repo && !run_id) {
     if (context.eventName !== "workflow_run" || context.action == "completed") {
       throw new Error(
         `Invalid context: '${context.eventName}:${context.action}'.  Expected 'workflow_run:completed'.`,
@@ -29,41 +22,35 @@ module.exports = async ({ github, context, core }, owner, repo, run_id) => {
         context.payload
       );
 
-    params = {
-      owner: payload.workflow_run.repository.owner.login,
-      repo: payload.workflow_run.repository.name,
-      run_id: payload.workflow_run.id,
-    };
-  } else {
+    owner = payload.workflow_run.repository.owner.login;
+    repo = payload.workflow_run.repository.name;
+    run_id = payload.workflow_run.id;
+  } else if (!owner || !repo || !run_id) {
     throw new Error(
       "Parameters 'owner', 'repo', and 'run_id' must be all either set or unset",
     );
   }
 
   const artifacts = await github.rest.actions.listWorkflowRunArtifacts({
-    owner: params.owner,
-    repo: params.repo,
-    run_id: params.run_id,
+    owner: owner,
+    repo: repo,
+    run_id: run_id,
   });
 
   console.log("artifacts: " + JSON.stringify(artifacts, null, 2));
 
   const artifactNames = artifacts.data.artifacts.map((a) => a.name);
 
-  core.exportVariable()
-
-  // const label = "brownfield";
-  // if (
-  //   artifactNames.includes("spec-lifecycle-data-plane=brownfield") ||
-  //   artifactNames.includes("spec-lifecycle-resource-manager=brownfield")
-  // ) {
-  //   await addLabelIfNotExists(
-  //     github,
-  //     context,
-  //     core,
-  //     label,
-  //   );
-  // } else {
-  //   await removeLabelIfExists(github, context, core, label);
-  // }
+  for (const artifactName of artifactNames) {
+    // If artifactName has format `key=value`, add the key and value as env vars.
+    // If artifactName does not contain "=", ignore.
+    // If artifactName contains multiple "=", the key is everything before the first "=",
+    // and the value is everything else.
+    const firstEquals = artifactName.indexOf("=");
+    if (firstEquals !== -1) {
+      const key = artifactName.substring(0, firstEquals);
+      const value = artifactName.substring(firstEquals + 1);
+      core.exportVariable(key, value);
+    }
+  }
 };
