e24152e618c58a5ec91d677d23edd88b5b795806
Mike Harder <mharder@microsoft.com>
2024-10-31 00:57:57 +0000

    Add script file

---
+++ .github/scripts/get-keyvalue-artifacts.js
@@ -0,0 +1,6 @@
+// @ts-check
+
+/** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
+module.exports = async ({ github, context, core }) => {
+  core.info("hello world!");
+}
