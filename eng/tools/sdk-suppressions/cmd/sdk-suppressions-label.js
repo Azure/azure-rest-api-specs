5fd1f327e5853eaa3aee21797602a8ad21b4fdac
v-tianxi <v-tianxi@microsoft.com>
2024-12-05 19:25:00 +0800

    test read env in node script

--- eng/tools/sdk-suppressions/cmd/sdk-suppressions-label.js
+++ eng/tools/sdk-suppressions/cmd/sdk-suppressions-label.js
@@ -7,5 +7,16 @@ const result = { removeLabel: "remove-label", addLabel: "add-label" };
 fs.writeFileSync("output.json", JSON.stringify(result));
 console.log("JSON output saved to output.json");
 
+const prHeaderBranch = process.env.GITHUB_HEAD_REF;  // The branch from which the PR is created
+const prBaseBranch = process.env.GITHUB_BASE_REF;    // The target branch of the PR
+const prNumber = process.env.GITHUB_PR_NUMBER;       // The PR number
+
+console.log(`
+    Pull Request Information:
+    Header Branch: ${prHeaderBranch}
+    Base Branch: ${prBaseBranch}
+    PR Number: ${prNumber}`
+);
+
 console.log("Ending log");
 
