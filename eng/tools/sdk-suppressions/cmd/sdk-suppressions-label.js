f62f9a2eb50f11d55cf610bf0e803c03f820aac6
v-tianxi <v-tianxi@microsoft.com>
2024-12-05 20:14:53 +0800

    add new param

--- eng/tools/sdk-suppressions/cmd/sdk-suppressions-label.js
+++ eng/tools/sdk-suppressions/cmd/sdk-suppressions-label.js
@@ -10,9 +10,6 @@ console.log("JSON output saved to output.json");
 const prHeaderBranch = process.env.GITHUB_HEAD_REF;  // The branch from which the PR is created
 const prBaseBranch = process.env.GITHUB_BASE_REF;    // The target branch of the PR
 const prNumber = process.env.GITHUB_PR_NUMBER;       // The PR number
-const eventName = process.env.GITHUB_EVENT_NAME;
-
-console.log(`This workflow was triggered by the ${eventName} event.`);
 
 console.log(`
     Pull Request Information:
@@ -20,6 +17,17 @@ console.log(`
     Base Branch: ${prBaseBranch}
     PR Number: ${prNumber}`
 );
+
+const eventName = process.env.GITHUB_EVENT_NAME;
+console.log(`This workflow was triggered by the ${eventName} event.`);
+
+const ref = process.env.GITHUB_PR_REF;
+const refName = process.env.GITHUB_PR_REF_NAME;
+const run_number = process.env.GITHUB_PR_RUN_NUMBER;
+console.log(`This workflow ref ${ref} `);
+console.log(`This workflow ref name ${refName} `);
+console.log(`This workflow run_number ${run_number} `);
+
 
 console.log("Ending log");
 
