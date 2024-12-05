679192117f4b3431f4287d15019ab4247087b8cd
v-tianxi <v-tianxi@microsoft.com>
2024-12-06 01:28:31 +0800

    using suppressions.js to test

--- .github/actions/handle-suppressions/suppressions.js
+++ .github/actions/handle-suppressions/suppressions.js
@@ -1,15 +1,16 @@
 
 module.exports = async ({ github, context, core }) => {
     
-    core.info(`extractInputs(${context.eventName}, ${context.payload.action})`);
+    core.info(`(${context.eventName}, ${context.payload.action})`);
 
     console.log('context', JSON.stringify(context));
     console.log('context.payload', JSON.stringify(context.payload));
-    github.rest.issues.createComment({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      issue_number: context.issue.number,
-      body: "Thank you for your contribution! 123123🚀"
-    });
+    const {SHA} = process.env
+    const commit = await github.rest.repos.getCommit({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        ref: `${SHA}`
+    })
+    core.exportVariable('author', commit.data.commit.author.email)
 
 }
