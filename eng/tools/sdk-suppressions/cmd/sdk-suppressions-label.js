dbb6b25bb9a46dcfe76cf822acc8330a0e2fec91
v-tianxi <v-tianxi@microsoft.com>
2024-12-05 17:28:13 +0800

    test variabel can used in next step

--- eng/tools/sdk-suppressions/cmd/sdk-suppressions-label.js
+++ eng/tools/sdk-suppressions/cmd/sdk-suppressions-label.js
@@ -1,3 +1,9 @@
 
 
 console.log('test sdk-suppressions-label.js is working');
+
+// script.js
+const result = { key: "value", success: true };
+console.log(JSON.stringify(result)); // 以 JSON 格式输出
+
+
