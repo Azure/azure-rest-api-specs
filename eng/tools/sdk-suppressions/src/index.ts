b2275f3ac4b113b07deb9f96cd2d745df01ebb0a
v-tianxi <v-tianxi@microsoft.com>
2024-12-06 19:13:26 +0800

    fix process env error

--- eng/tools/sdk-suppressions/src/index.ts
+++ eng/tools/sdk-suppressions/src/index.ts
@@ -9,14 +9,16 @@ function getUsage(): string {
   }
 
 export async function main() {
-    const PullRequestContext = process.env.GITHUB_PULL_REQUEST_CONTEXT as unknown as PullRequestContext;
-    const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-    console.log(`PullRequestContext: JSON.stringify(${PullRequestContext})`);
+    const GITHUB_PULL_REQUEST_CONTEXT = process.env.GITHUB_PULL_REQUEST_CONTEXT as string;
+    const GITHUB_TOKEN = process.env.GITHUB_TOKEN as string;
+    console.log(`PullRequestContext: (${GITHUB_PULL_REQUEST_CONTEXT})`);
     console.log(`GITHUB_TOKEN: is exist ? ${!!GITHUB_TOKEN}`);
-    console.log(`GITHUB_PULL_REQUEST_CONTEXT: is exist ? ${!!PullRequestContext}`);
+    console.log(`GITHUB_PULL_REQUEST_CONTEXT: is exist ? ${!!GITHUB_PULL_REQUEST_CONTEXT}`);
 
-    if (PullRequestContext && GITHUB_TOKEN) {
-      const changedLabels: {labelsToAdd: String[], labelsToRemove: String[]} = await updateSdkSuppressionsLabels(PullRequestContext, GITHUB_TOKEN);
+    if (GITHUB_PULL_REQUEST_CONTEXT && GITHUB_TOKEN) {
+      const pullRequestContext = JSON.parse(GITHUB_PULL_REQUEST_CONTEXT) as unknown as PullRequestContext;
+      const token = GITHUB_TOKEN;
+      const changedLabels: {labelsToAdd: String[], labelsToRemove: String[]} = await updateSdkSuppressionsLabels(pullRequestContext, token);
       console.log(JSON.stringify(changedLabels));
       exit(0);
     } else {
