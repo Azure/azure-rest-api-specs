c05a5c61f75c8bcc6adbdc6384310085fee20a03
v-tianxi <v-tianxi@microsoft.com>
2024-12-06 17:28:10 +0800

    add feature

--- eng/tools/sdk-suppressions/cmd/sdk-suppressions-label.js
+++ eng/tools/sdk-suppressions/cmd/sdk-suppressions-label.js
@@ -1,34 +1,5 @@
-const fs = require("fs");
+#!/usr/bin/env node
 
-console.log('test sdk-suppressions-label.js is working 2');
+import { main } from "../dist/src/index.js";
 
-const result = { removeLabel: "remove-label", addLabel: "add-label" };
-
-fs.writeFileSync("output.json", JSON.stringify(result));
-console.log("JSON output saved to output.json");
-
-const prHeaderBranch = process.env.GITHUB_HEAD_REF;  // The branch from which the PR is created
-const prBaseBranch = process.env.GITHUB_BASE_REF;    // The target branch of the PR
-const prNumber = process.env.GITHUB_PR_NUMBER;       // The PR number
-
-console.log(`
-    Pull Request Information:
-    Header Branch: ${prHeaderBranch}
-    Base Branch: ${prBaseBranch}
-    PR Number: ${prNumber}`
-);
-
-const eventName = process.env.GITHUB_EVENT_NAME;
-console.log(`This workflow was triggered by the ${eventName} event.`);
-
-const ref = process.env.GITHUB_PR_REF;
-const refName = process.env.GITHUB_PR_REF_NAME;
-const run_number = process.env.GITHUB_PR_RUN_NUMBER;
-console.log(`This workflow ref ${ref} `);
-console.log(`This workflow ref name ${refName} `);
-console.log(`This workflow run_number ${run_number} `);
-
-console.log("Ending log");
-
-return result;
-
+await main();
