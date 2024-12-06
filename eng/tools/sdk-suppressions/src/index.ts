f8261af08ad9123dd803976b4ce8bca9dc98b578
v-tianxi <v-tianxi@microsoft.com>
2024-12-06 18:58:33 +0800

    fix npm ci error

--- eng/tools/sdk-suppressions/src/index.ts
+++ eng/tools/sdk-suppressions/src/index.ts
@@ -4,20 +4,12 @@ import { updateSdkSuppressionsLabels, PullRequestContext } from "./updateSdkSupp
 
 function getUsage(): string {
     return (
-      "  Usage: npx get-specs-model <path-to-specs-file-or-directory>\n" +
-      "Returns: JSON metadata for the input file or directory.\n" +
-      "\n" +
-      "The input path:\n" +
-      "- Must be an absolute or relative path to local clone of the https://github.com/Azure/azure-rest-api-specs or https://github.com/Azure/azure-rest-api-specs-pr repo.\n" +
-      "- Must point to the <local_clone>/specification directory or one of its descendants.\n" +
-      "\n" +
-      "Example: npx get-specs-model $HOME/repos/azure-rest-api-specs/specification\n" +
-      "Returns: JSON with metadata for the entire 'specification' directory of the local clone of 'azure-rest-api-specs' repo.\n"
+      "parameter missing! \n"
     );
   }
 
 export async function main() {
-    const PullRequestContext = process.env.PullRequestContext as unknown as PullRequestContext;
+    const PullRequestContext = process.env.GITHUB_PULL_REQUEST_CONTEXT as unknown as PullRequestContext;
     const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
     console.log(`PullRequestContext: JSON.stringify(${PullRequestContext})`);
 
