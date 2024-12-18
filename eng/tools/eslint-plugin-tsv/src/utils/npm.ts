659ed1e9bd31292b003408ec760b296eab96160a
Mike Harder <mharder@microsoft.com>
2024-12-18 23:07:21 +0000

    Use memfs to test fs code

--- eng/tools/eslint-plugin-tsv/src/utils/npm.ts
+++ eng/tools/eslint-plugin-tsv/src/utils/npm.ts
@@ -1,9 +1,20 @@
-import { dirname } from "path";
+import * as path from "path";
+import * as fs from "fs/promises";
 
 export class Npm {
   // Simulates `npm prefix` by finding the nearest parent directory containing `package.json` or `node_modules`.
   // If neither exist in any parent directories, returns the directory containing the path itself.
-  static prefix(path: string): string {
-    return dirname(path);
+  static async prefix(filePath: string): Promise<string> {
+    const stats = await fs.stat(filePath);
+
+    let currentDirectory: string;
+    if (stats.isDirectory()) {
+        currentDirectory = path.resolve(filePath);
+    }
+    else {
+        currentDirectory = path.dirname(filePath);
+    }
+
+    return currentDirectory;
   }
 }
--- eng/tools/eslint-plugin-tsv/test/utils/npm.test.ts
+++ eng/tools/eslint-plugin-tsv/test/utils/npm.test.ts
@@ -1,16 +1,29 @@
-// import { beforeEach, test, vi } from "vitest";
-import { describe, expect, it } from "vitest";
-
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { vol } from "memfs";
 import { Npm } from "../../src/utils/npm.js";
 
-// vi.mock('fs')
+vi.mock("fs/promises", async () => {
+  const memfs = await import("memfs");
+  return {
+    ...memfs.fs.promises,
+  };
+});
+
 describe("prefix", () => {
+  beforeEach(() => {
+    vol.reset();
+  });
+
   describe("returns current directory if no match", () => {
     it.each([
       ["/tmp/foo/tspconfig.yaml", "/tmp/foo"],
       ["/tmp/foo", "/tmp/foo"],
     ])("%s", async (path, expected) => {
-      expect(Npm.prefix(path)).toBe(expected);
+      vol.fromJSON({
+        "/tmp/foo/tspconfig.yaml": "1",
+      });
+
+      expect(await Npm.prefix(path)).toBe(expected);
     });
   });
 });
