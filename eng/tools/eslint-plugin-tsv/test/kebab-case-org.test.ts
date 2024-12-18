9c098940d9231cdb12f665769dd84a200b154f71
Mike Harder <mharder@microsoft.com>
2024-12-18 02:04:09 +0000

    Add tests for disabled rule

--- eng/tools/eslint-plugin-tsv/test/kebab-case-org.test.ts
+++ eng/tools/eslint-plugin-tsv/test/kebab-case-org.test.ts
@@ -4,7 +4,7 @@ import { test } from "vitest";
 
 import kebabCaseOrg from "../src/kebab-case-org.js";
 
-test(kebabCaseOrg.meta.name, () => {
+test("tsv/" + kebabCaseOrg.meta.name, () => {
   const ruleTester = new RuleTester({
     languageOptions: {
       parser: parser,
@@ -15,7 +15,7 @@ test(kebabCaseOrg.meta.name, () => {
     valid: [
       { code: "", filename: "/specification/contoso/Contoso.WidgetManager/tspconfig.yaml" },
       {
-        code: "# eslint-disable",
+        code: `# eslint-disable rule-to-test/${kebabCaseOrg.meta.name}`,
         filename: "/specification/Not-Kebab-Case/Not.KebabCase/tspconfig.yaml",
       },
     ],
@@ -23,7 +23,7 @@ test(kebabCaseOrg.meta.name, () => {
       {
         code: "",
         filename: "/specification/Not-Kebab-Case/Not.KebabCase/tspconfig.yaml",
-        errors: 1,
+        errors: ["Organization name (first path segment after 'specification') does not use kebab-case: 'Not-Kebab-Case'"],
       },
     ],
   });
