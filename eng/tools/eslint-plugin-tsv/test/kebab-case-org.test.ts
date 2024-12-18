b03b6665042019c706faab1a78ddba223e0291f9
Mike Harder <mharder@microsoft.com>
2024-12-18 01:30:09 +0000

    Test eslint disablement comment

--- eng/tools/eslint-plugin-tsv/test/kebab-case-org.test.ts
+++ eng/tools/eslint-plugin-tsv/test/kebab-case-org.test.ts
@@ -1,13 +1,24 @@
 import { Rule, RuleTester } from "eslint";
+import parser from "yaml-eslint-parser";
 import { test } from "vitest";
 
 import kebabCaseOrg from "../src/kebab-case-org.js";
 
 test(kebabCaseOrg.meta.name, () => {
-  const ruleTester = new RuleTester();
+  const ruleTester = new RuleTester({
+    languageOptions: {
+      parser: parser,
+    },
+  });
 
   ruleTester.run(kebabCaseOrg.meta.name, kebabCaseOrg as Rule.RuleModule, {
-    valid: [{ code: "", filename: "/specification/contoso/Contoso.WidgetManager/tspconfig.yaml" }],
+    valid: [
+      { code: "", filename: "/specification/contoso/Contoso.WidgetManager/tspconfig.yaml" },
+      {
+        code: "# eslint-disable",
+        filename: "/specification/Not-Kebab-Case/Not.KebabCase/tspconfig.yaml",
+      },
+    ],
     invalid: [
       {
         code: "",
