c8285c69b1b435aad1ab9f3893e661af5f5133d5
v-tianxi <v-tianxi@microsoft.com>
2024-12-19 16:21:28 +0800

    add args for node script

--- eng/tools/sdk-suppressions/src/index.ts
+++ eng/tools/sdk-suppressions/src/index.ts
@@ -2,28 +2,30 @@
 import { exit } from "process";
 import { updateSdkSuppressionsLabels, PullRequestContext } from "./updateSdkSuppressionsLabel.js";
 
-function getUsage(): string {
-    return (
-      "Parameter missing! \n" + 
-      "Check GITHUB_PULL_REQUEST_CONTEXT where defined in step: Get GitHub PullRequest Context" +
-      "Check OUTPUT_FILE in step env: Run get suppressions label script " +
-      "Check The pr has change files in GITHUB_PULL_REQUEST_CHANGE_FILES "
-    );
-  }
+function getArgsError(): string {
+  return (
+    "Usage: node eng/tools/sdk-suppressions/cmd/sdk-suppressions-label.js baseCommitHash headCommitHash\n" +
+    "Returns: {labelsToAdd: [label1, label2],labelsToRemove: [lable3, label4]}\n" 
+  );
+}
 
 export async function main() {
+  const args: string[] = process.argv.slice(2);
+  if (args.length === 2) {
+    const baseCommitHash: string = args[0];
+    const headCommitHash: string = args[1];
     const pullRequestContext = process.env.GITHUB_PULL_REQUEST_CONTEXT as string;
     const outputFile = process.env.OUTPUT_FILE as string;
     const prChangeFiles = process.env.GITHUB_PULL_REQUEST_CHANGE_FILES as string;
-    if (pullRequestContext && outputFile && prChangeFiles) {
-      const _pullRequestContext = JSON.parse(pullRequestContext) as unknown as PullRequestContext;
-      const changedLabels: {labelsToAdd: String[], labelsToRemove: String[]} = await updateSdkSuppressionsLabels(_pullRequestContext, prChangeFiles, outputFile);
-      console.log(JSON.stringify(changedLabels));
-      exit(0);
-    } else {
-      console.error(getUsage());
-      exit(1);
-    }
+    const _pullRequestContext = JSON.parse(pullRequestContext) as unknown as PullRequestContext;
+    const changedLabels: {labelsToAdd: String[], labelsToRemove: String[]} = await updateSdkSuppressionsLabels(_pullRequestContext, prChangeFiles, outputFile, baseCommitHash, headCommitHash);
+    console.log(JSON.stringify(changedLabels));
+    exit(0);
+  } else {
+    console.error(getArgsError());
+    exit(1);
+  }
+
 }
   
 export { updateSdkSuppressionsLabels };
