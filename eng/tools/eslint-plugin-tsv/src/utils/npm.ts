1afea81b4e7b057b8d792d22b6d6c069130cb6ba
Mike Harder <mharder@microsoft.com>
2024-12-19 00:40:52 +0000

    Implement prefix()

--- eng/tools/eslint-plugin-tsv/src/utils/npm.ts
+++ eng/tools/eslint-plugin-tsv/src/utils/npm.ts
@@ -1,20 +1,36 @@
-import * as path from "path";
-import * as fs from "fs/promises";
+import { dirname, join, resolve } from "path";
+import { stat, access } from "fs/promises";
 
 export class Npm {
   // Simulates `npm prefix` by finding the nearest parent directory containing `package.json` or `node_modules`.
   // If neither exist in any parent directories, returns the directory containing the path itself.
-  static async prefix(filePath: string): Promise<string> {
-    const stats = await fs.stat(filePath);
+  static async prefix(path: string): Promise<string> {
+    const stats = await stat(path);
 
-    let currentDirectory: string;
+    let initialDir: string;
     if (stats.isDirectory()) {
-        currentDirectory = path.resolve(filePath);
-    }
-    else {
-        currentDirectory = path.dirname(filePath);
+      initialDir = resolve(path);
+    } else {
+      initialDir = dirname(path);
     }
 
-    return currentDirectory;
+    for (
+      var currentDir = initialDir;
+      dirname(currentDir) != currentDir;
+      currentDir = dirname(currentDir)
+    ) {
+      try {
+        await access(join(currentDir, "package.json"));
+        return currentDir;
+      } catch {}
+
+      try {
+        await access(join(currentDir, "node_modules"));
+        return currentDir;
+      } catch {}
+    }
+
+    // Neither found in an parent dir
+    return initialDir;
   }
 }
