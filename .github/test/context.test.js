8b21790a5bdb27a7f3170532a9afa147c1baa90a
Mike Harder <mharder@microsoft.com>
2025-01-29 01:44:57 +0000

    Add TODOs

--- .github/test/context.test.js
+++ .github/test/context.test.js
@@ -2,6 +2,7 @@ import { describe, expect, it } from "vitest";
 import { extractInputs } from "../src/context.js";
 
 describe("context", () => {
+  // TODO: Replace with better tests
   it("throws if inputs null", async () => {
     await expect(extractInputs(null, null, null)).rejects.toThrow();
   });
