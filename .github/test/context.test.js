998cb7bacea0daf947d0d3580603e82faf5f8bf7
Mike Harder <mharder@microsoft.com>
2025-01-30 03:43:00 +0000

    Add test for workflow_run

--- .github/test/context.test.js
+++ .github/test/context.test.js
@@ -34,40 +34,47 @@ describe("extractInputs", () => {
     });
   });
 
-  // it("workflow_run (same repo)", async () => {
-  //   const core = createMockCore();
+  it("workflow_run (same repo)", async () => {
+    const core = createMockCore();
 
-  //   const context = {
-  //     eventName: "workflow_run",
-  //     payload: {
-  //       action: "completed",
-  //       workflow_run
+    const context = {
+      eventName: "workflow_run",
+      payload: {
+        action: "completed",
+        workflow_run: {
+          head_repository: {
+            name: "TestRepoName",
+            owner: {
+              login: "TestRepoOwnerLogin",
+            },
+          },
+          head_sha: "abc123",
+          id: 456,
+          repository: {
+            name: "TestRepoName",
+            owner: {
+              login: "TestRepoOwnerLogin",
+            },
+          },
+          pull_requests: [{ number: 123 }],
+        },
+      },
+    };
 
-  //       repository: {
-  //         name: "TestRepoName",
-  //         owner: {
-  //           login: "TestRepoOwnerLogin",
-  //         },
-  //       },
-  //       pull_request: {
-  //         head: {
-  //           sha: "abc123",
-  //         },
-  //         number: 123,
-  //       },
-  //     },
-  //   };
+    const inputs = await extractInputs(null, context, core);
 
-  //   const inputs = await extractInputs(null, context, core);
+    expect(inputs).toEqual({
+      owner: "TestRepoOwnerLogin",
+      repo: "TestRepoName",
+      head_sha: "abc123",
+      issue_number: 123,
+      run_id: 456,
+    });
+  });
 
-  //   expect(inputs).toEqual({
-  //     owner: "TestRepoOwnerLogin",
-  //     repo: "TestRepoName",
-  //     head_sha: "abc123",
-  //     issue_number: 123,
-  //     run_id: NaN,
-  //   });
-  // });
+  // Fork Repo (context.pull_requests null or empty)
+  // const github = createMockGithub();
+  // github.rest.repos.listPullRequestsAssociatedWithCommit.mockResolvedValue({data: [{number: 123}]})
 });
 
 function createMockGithub() {
