a85815ef549354bd0afd2351fc2fe23b0b74ce4b
Mike Harder <mharder@microsoft.com>
2025-01-30 10:04:50 +0000

    add comment

--- .github/test/mocks.js
+++ .github/test/mocks.js
@@ -1,5 +1,6 @@
 import { vi } from "vitest";
 
+// Partial mock of `core` parameter passed into to github-script actions
 export function createMockCore() {
   return {
     debug: vi.fn(console.debug),
