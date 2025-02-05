00a3f296f0d7cc1b9b4f0d54f8a5b37af0bbb712
Mike Harder <mharder@microsoft.com>
2025-02-05 05:48:22 +0000

    move createMockGithub to shared code

--- .github/test/mocks.js
+++ .github/test/mocks.js
@@ -1,9 +1,29 @@
 import { vi } from "vitest";
 
+// Partial mock of `github` parameter passed into github-script actions
+export function createMockGithub() {
+  return {
+    rest: {
+      actions: {
+        listWorkflowRunArtifacts: vi
+          .fn()
+          .mockResolvedValue({ data: { artifacts: [] } }),
+      },
+      issues: {
+        addLabels: vi.fn(),
+        removeLabel: vi.fn(),
+      },
+    },
+  };
+}
+
 // Partial mock of `core` parameter passed into to github-script actions
 export function createMockCore() {
   return {
     debug: vi.fn(console.debug),
     info: vi.fn(console.log),
+    setOutput: vi.fn((name, value) =>
+      console.log(`setOutput('${name}', '${value}')`),
+    ),
   };
 }
