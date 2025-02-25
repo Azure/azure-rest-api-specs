dd79b891c99bd5b8c6a846417ceb65a9efae3019
Daniel Jurek <djurek@microsoft.com>
2025-02-25 00:02:05 +0000

    Use openapi-validator from node_modules, bypassing autorest's package manager

--- eng/tools/lint-diff/src/util.ts
+++ eng/tools/lint-diff/src/util.ts
@@ -1,4 +1,6 @@
-import { access } from "fs/promises";
+import { access, constants } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
 
 /**
  * Enumerate files in a directory that match the given string ending
@@ -21,5 +23,31 @@ export async function pathExists(path: string): Promise<boolean> {
     return true;
   } catch {
     return false;
+  }
+}
+
+// Copied from azure-sdk-tools repo
+// TODO: This should probably be moved to another more general location
+export async function getPathToDependency(dependency: string): Promise<string> {
+  // Example: /home/user/foo/node_modules/@autorest/bar/dist/index.js
+  const entrypoint = fileURLToPath(import.meta.resolve(dependency));
+
+  // Walk up directory tree to first folder containing "package.json"
+  let currentDir = dirname(entrypoint);
+  while (true) {
+    const packageJsonFile = join(currentDir, "package.json");
+    try {
+      // Throws if file cannot be read
+      await access(packageJsonFile, constants.R_OK);
+      return currentDir;
+    } catch {
+      const parentDir = dirname(currentDir);
+      if (parentDir !== currentDir) {
+        currentDir = parentDir;
+      } else {
+        // Reached fs root but no package.json found
+        throw new Error(`Unable to find package.json in folder tree above '${entrypoint}'`);
+      }
+    }
   }
 }
