eca29a8f253c4922c91d9ab7159086c913cc366a
Wanpeng Li <wanl@microsoft.com>
2025-03-04 17:01:06 +0800

    Add Python Data Plane Rules of tspconfig.yaml for TSV (#32942)
    Co-authored-by: albertxavier100 <albertxavier001@icloud.com>

--- eng/tools/typespec-validation/src/rules/sdk-tspconfig-validation.ts
+++ eng/tools/typespec-validation/src/rules/sdk-tspconfig-validation.ts
@@ -345,7 +345,7 @@ export class TspConfigGoAzInjectSpansTrueSubRule extends TspconfigEmitterOptions
   }
 }
 
-// ----- Python management sub rules -----
+// ----- Python management plane sub rules -----
 export class TspConfigPythonMgmtPackageDirectorySubRule extends TspconfigEmitterOptionsSubRuleBase {
   constructor() {
     super("@azure-tools/typespec-python", "package-dir", new RegExp(/^azure-mgmt(-[a-z]+){1,2}$/));
@@ -355,30 +355,32 @@ export class TspConfigPythonMgmtPackageDirectorySubRule extends TspconfigEmitter
   }
 }
 
-export class TspConfigPythonMgmtPackageNameEqualStringSubRule extends TspconfigEmitterOptionsSubRuleBase {
+// ----- Python data plane sub rules -----
+export class TspConfigPythonDpPackageDirectorySubRule extends TspconfigEmitterOptionsSubRuleBase {
+  constructor() {
+    super("@azure-tools/typespec-python", "package-dir", new RegExp(/^azure(-[a-z]+){1,3}$/));
+  }
+  protected skip(_: any, folder: string) {
+    return skipForManagementPlane(folder);
+  }
+}
+
+// ----- Python azure sub rules -----
+export class TspConfigPythonAzPackageNameEqualStringSubRule extends TspconfigEmitterOptionsSubRuleBase {
   constructor() {
     super("@azure-tools/typespec-python", "package-name", "{package-dir}");
   }
-  protected skip(_: any, folder: string) {
-    return skipForDataPlane(folder);
-  }
 }
 
-export class TspConfigPythonMgmtGenerateTestTrueSubRule extends TspconfigEmitterOptionsSubRuleBase {
+export class TspConfigPythonAzGenerateTestTrueSubRule extends TspconfigEmitterOptionsSubRuleBase {
   constructor() {
     super("@azure-tools/typespec-python", "generate-test", true);
   }
-  protected skip(_: any, folder: string) {
-    return skipForDataPlane(folder);
-  }
 }
 
-export class TspConfigPythonMgmtGenerateSampleTrueSubRule extends TspconfigEmitterOptionsSubRuleBase {
+export class TspConfigPythonAzGenerateSampleTrueSubRule extends TspconfigEmitterOptionsSubRuleBase {
   constructor() {
     super("@azure-tools/typespec-python", "generate-sample", true);
-  }
-  protected skip(_: any, folder: string) {
-    return skipForDataPlane(folder);
   }
 }
 
@@ -431,9 +433,10 @@ export const defaultRules = [
   new TspConfigGoDpPackageDirectoryMatchPatternSubRule(),
   new TspConfigGoDpModuleMatchPatternSubRule(),
   new TspConfigPythonMgmtPackageDirectorySubRule(),
-  new TspConfigPythonMgmtPackageNameEqualStringSubRule(),
-  new TspConfigPythonMgmtGenerateTestTrueSubRule(),
-  new TspConfigPythonMgmtGenerateSampleTrueSubRule(),
+  new TspConfigPythonDpPackageDirectorySubRule(),
+  new TspConfigPythonAzPackageNameEqualStringSubRule(),
+  new TspConfigPythonAzGenerateTestTrueSubRule(),
+  new TspConfigPythonAzGenerateSampleTrueSubRule(),
   new TspConfigCsharpAzPackageDirectorySubRule(),
   new TspConfigCsharpAzNamespaceEqualStringSubRule(),
   new TspConfigCsharpAzClearOutputFolderTrueSubRule(),
