15ef483622b1f460022267d827b4c656ec169691
yogms <yoghcl@gmail.com>
2025-03-06 14:10:31 +0530

    Corrected the parameter name

---
+++ Runnpmprettier.ps1
@@ -0,0 +1,19 @@
+﻿# Define the path to the log/txt file
+$logFilePath = "C:\Users\yosharma\Desktop\11.txt"
+
+# Read all lines from the log file
+$lines = Get-Content -Path $logFilePath
+
+# Iterate through each line
+foreach ($line in $lines) {
+    # Check if the line contains 'npx prettier'
+    if ($line -match "npx prettier") {
+        # Extract the command to run
+        #$command = $line.TrimStart('>').Trim()
+        $command = $line.Substring(2).Trim()
+        #$command = $line -replace '^>\s*', ''
+        # Execute the command
+        Write-Host $line.Substring(2).Trim()
+        Invoke-Expression $command
+    }
+}
