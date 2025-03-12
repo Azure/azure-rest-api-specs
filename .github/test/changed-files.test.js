054a87882f6247477dc2aa3c6604c647a7938f08
Mike Harder <mharder@microsoft.com>
2025-03-12 06:24:09 +0000

    Move shared code under .github/src

--- .github/test/changed-files.test.js
+++ .github/test/changed-files.test.js
@@ -1,5 +1,4 @@
 import { describe, expect, it, vi } from "vitest";
-import { createMockCore } from "../../test/mocks.js";
 import {
   dataPlane,
   example,
@@ -11,11 +10,12 @@ import {
   swagger,
 } from "../src/changed-files.js";
 import * as git from "../src/git.js";
+import { createMockCore } from "./mocks.js";
 
 describe("changedFiles", () => {
   it("getChangedFiles", async () => {
     const files = [
-      ".github/workflows/src/git.js",
+      ".github/src/git.js",
       "specification/contosowidgetmanager/Contoso.Management/main.tsp",
       "specification/contosowidgetmanager/resource-manager/Microsoft.Contoso/stable/2021-11-01/contoso.json",
       "specification/contosowidgetmanager/resource-manager/Microsoft.Contoso/stable/2021-11-01/examples/Employees_Get.json",
