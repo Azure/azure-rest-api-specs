90c1ae715f66791a64b855e3deaa0933f85f1eb9
Mike Harder <mharder@microsoft.com>
2025-03-12 18:22:28 +0000

    failing test for readme.getInputFiles()

---
+++ .github/src/readme.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+/**
+ * @param {string} markdown
+ * @param {import('github-script').AsyncFunctionArguments['core']} core
+ * @returns {Promise<string[]>}
+ */
+
+export async function getInputFiles(markdown, core) {
+  return [];
+}
