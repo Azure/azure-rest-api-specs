90f17e72cbef5740afded7bda40fbd970aa7e5be
Mike Harder <mharder@microsoft.com>
2025-03-12 23:46:10 +0000

    Implement getInputFiles using deps "marked" and "yaml"

--- .github/src/readme.js
+++ .github/src/readme.js
@@ -1,18 +1,26 @@
 // @ts-check
 
+import { marked } from "marked";
+import yaml from "yaml";
+
 /**
  * @param {string} markdown
  * @param {import('github-script').AsyncFunctionArguments['core']} core
  * @returns {Promise<Set<string>>} All input files for all tags
  */
 export async function getInputFiles(markdown, core) {
-  // TODO: Replace with calls to "marked" and "yaml" libraries
-  // Hacky implementation to pass test
-  const lines = markdown.split("\n");
-  const inputFileLines = lines.filter(
-    (l) => l.startsWith("  - ") && l.trimEnd().toLowerCase().endsWith(".json"),
-  );
-  const inputFiles = inputFileLines.map((l) => l.substring(4));
+  const tokens = marked.lexer(markdown);
+
+  const yamlBlocks = tokens
+    .filter((token) => token.type === "code")
+    .map((token) => /** @type import("marked").Tokens.Code */ (token))
+    // Include default block and tagged blocks (```yaml $(tag) == 'package-2021-11-01')
+    .filter((token) => token.lang?.toLowerCase().startsWith("yaml"));
+
+  const inputFiles = yamlBlocks.flatMap((block) => {
+    const obj = yaml.parse(block.text);
+    return /** @type string[] */ (obj["input-file"] || []);
+  });
 
   return new Set(inputFiles);
 }
