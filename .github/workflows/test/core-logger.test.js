5b58553ea43baf7b8608ca2b83b23df86f7348de
Mike Harder <mharder@microsoft.com>
2025-03-17 18:36:56 +0000

    CoreLogger to 100% coverage

--- .github/workflows/test/core-logger.test.js
+++ .github/workflows/test/core-logger.test.js
@@ -11,4 +11,24 @@ describe("CoreLogger", () => {
 
     expect(core.debug).toBeCalledWith("test debug");
   });
+
+  it("info", async () => {
+    const core = createMockCore();
+    const logger = new CoreLogger(core);
+
+    logger.info("test info");
+
+    expect(core.info).toBeCalledWith("test info");
+  });
+
+  it("isDebug", async () => {
+    const core = createMockCore();
+    core.isDebug.mockReturnValue(true);
+
+    const logger = new CoreLogger(core);
+
+    expect(logger.isDebug()).toBe(true);
+
+    expect(core.isDebug).toBeCalled();
+  });
 });
