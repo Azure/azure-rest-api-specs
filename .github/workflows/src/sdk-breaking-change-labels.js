d1372b76ea6f9c260993f9634923fd8a19529f71
ray chen <raychen@microsoft.com>
2025-03-18 22:25:56 +0000

    removed unused import

--- .github/workflows/src/sdk-breaking-change-labels.js
+++ .github/workflows/src/sdk-breaking-change-labels.js
@@ -1,7 +1,6 @@
 // @ts-check
 import { sdkLabels } from "../../src/sdk-types.js";
 import { LabelAction } from "./label.js";
-import { stringify } from "querystring";
 import { extractInputs } from "./context.js";
 import { getIssueNumber } from "./issues.js";
 
@@ -46,71 +45,67 @@ export async function getLabelAndActionImpl({ado_build_id, ado_project_url, head
   const artifactName = "spec-gen-sdk-breaking-change-artifact";
   const artifactFileName = artifactName + ".json";
   const apiUrl = `${ado_project_url}/_apis/build/builds/${ado_build_id}/artifacts?artifactName=${artifactName}&api-version=7.0`;
-  try {
-    core.info(`Calling Azure DevOps API to get the artifact: ${apiUrl}`);
+  core.info(`Calling Azure DevOps API to get the artifact: ${apiUrl}`);
 
-    // Use Node.js fetch to call the API
-    const response = await fetch(apiUrl, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-
-    if (response.ok) {
-      // Step 1: Get the download URL for the artifact
-      /** @type {Artifacts} */
-      const artifacts = /** @type {Artifacts} */ (await response.json());
-      core.info(`Artifacts found: ${JSON.stringify(artifacts)}`);
-      if (!artifacts.resource || !artifacts.resource.downloadUrl) {
-        throw new Error(`Download URL not found for the artifact ${artifactName}`);
-      }
-
-      let downloadUrl = artifacts.resource.downloadUrl;
-      const index = downloadUrl.indexOf("?format=zip");
-      if (index !== -1) {
-        // Keep everything up to (but not including) "?format=zip"
-        downloadUrl = downloadUrl.substring(0, index);
-      }
-      downloadUrl += `?format=file&subPath=/${artifactFileName}`;
-      core.info(`Downloading artifact from: ${downloadUrl}`);
-
-      // Step 2: Fetch Artifact Content (as a Buffer)
-      const artifactResponse = await fetch(downloadUrl);
-      if (!artifactResponse.ok) {
-        throw new Error(`Failed to fetch artifact: ${artifactResponse.statusText}`);
-      }
-
-      const artifactData = await artifactResponse.text();
-      core.info(`Artifact content: ${artifactData}`);
-
-      // Parse the JSON data
-      const breakingChangeResult = JSON.parse(artifactData);
-      const labelActionText = breakingChangeResult.labelAction;
-      const breakingChangeLanguage = breakingChangeResult.language;
-      if (breakingChangeLanguage) {
-        labelName = sdkLabels[`${breakingChangeLanguage}`].breakingChange;
-      }
-
-      // Set label action and name based on the artifacts
-      if (labelActionText === true) {
-        labelAction = LabelAction.Add;
-      } else if (labelActionText === false) {
-        labelAction = LabelAction.Remove;
-      }
-
-      // Get the issue number from the check run
-      if (!issue_number) {
-        const { issueNumber } = await getIssueNumber({head_sha, core, github});
-        issue_number = issueNumber;
-      }
-    } else {
-      core.error(`Failed to fetch artifacts: ${response.status}, ${response.statusText}`);
-      const errorText = await response.text();
-      core.error(`Error details: ${errorText}`);
+  // Use Node.js fetch to call the API
+  const response = await fetch(apiUrl, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
     }
-  } catch (error) {
-    throw error;
+  });
+
+  if (response.ok) {
+    // Step 1: Get the download URL for the artifact
+    /** @type {Artifacts} */
+    const artifacts = /** @type {Artifacts} */ (await response.json());
+    core.info(`Artifacts found: ${JSON.stringify(artifacts)}`);
+    if (!artifacts.resource || !artifacts.resource.downloadUrl) {
+      throw new Error(`Download URL not found for the artifact ${artifactName}`);
+    }
+
+    let downloadUrl = artifacts.resource.downloadUrl;
+    const index = downloadUrl.indexOf("?format=zip");
+    if (index !== -1) {
+      // Keep everything up to (but not including) "?format=zip"
+      downloadUrl = downloadUrl.substring(0, index);
+    }
+    downloadUrl += `?format=file&subPath=/${artifactFileName}`;
+    core.info(`Downloading artifact from: ${downloadUrl}`);
+
+    // Step 2: Fetch Artifact Content (as a Buffer)
+    const artifactResponse = await fetch(downloadUrl);
+    if (!artifactResponse.ok) {
+      throw new Error(`Failed to fetch artifact: ${artifactResponse.statusText}`);
+    }
+
+    const artifactData = await artifactResponse.text();
+    core.info(`Artifact content: ${artifactData}`);
+
+    // Parse the JSON data
+    const breakingChangeResult = JSON.parse(artifactData);
+    const labelActionText = breakingChangeResult.labelAction;
+    const breakingChangeLanguage = breakingChangeResult.language;
+    if (breakingChangeLanguage) {
+      labelName = sdkLabels[`${breakingChangeLanguage}`].breakingChange;
+    }
+
+    // Set label action and name based on the artifacts
+    if (labelActionText === true) {
+      labelAction = LabelAction.Add;
+    } else if (labelActionText === false) {
+      labelAction = LabelAction.Remove;
+    }
+
+    // Get the issue number from the check run
+    if (!issue_number) {
+      const { issueNumber } = await getIssueNumber({head_sha, core, github});
+      issue_number = issueNumber;
+    }
+  } else {
+    core.error(`Failed to fetch artifacts: ${response.status}, ${response.statusText}`);
+    const errorText = await response.text();
+    core.error(`Error details: ${errorText}`);
   }
 
   if (!labelAction) {
