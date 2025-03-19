9fd73bac29f4acaca354c7cfedd5111eb31ce377
Mike Harder <mharder@microsoft.com>
2025-03-19 16:56:02 +0000

    full test matrix

--- .github/test/equality.test.js
+++ .github/test/equality.test.js
@@ -4,23 +4,35 @@ import { describe, expect, it } from "vitest";
 import { setEquals } from "../src/equality.js";
 
 describe("equality", () => {
-  // TODO: replace sets with arrays in inputs, add sets of size 2 and 3
   it.each([
+    // null and undefined are never equal to anything
     [null, null, false],
     [undefined, undefined, false],
     [null, undefined, false],
-    [null, new Set(), false],
-    [undefined, new Set(), false],
-    [new Set(), new Set(), true],
-    [new Set(), new Set([1]), false],
-    [new Set([1]), new Set([1]), true],
-    [new Set([1]), new Set([2]), false],
-  ])("setEquals(%s, %s, %s)", (set1, set2, expected) => {
+    [null, [], false],
+    [undefined, [], false],
+    // different lengths
+    [[], [1], false],
+    [[1], [1, 2], false],
+    [[1, 2], [1, 2, 3], false],
+    // different elements
+    [[1], [2], false],
+    [[1, 2], [1, 3], false],
+    [[1, 2], [2, 3], false],
+    [[1, 2, 3], [2, 3, 4], false],
+    // sets with same elements, size >= 0
+    [[], [], true],
+    [[1], [1], true],
+    [[1, 2], [1, 2], true],
+    [[1, 2, 3], [1, 2, 3], true],
+  ])("setEquals(%s, %s, %s)", (input1, input2, expected) => {
+    const [set1, set2] = [input1, input2].map((input) =>
+      input instanceof Array ? new Set(input) : input,
+    );
+
     // @ts-ignore
     expect(setEquals(set1, set2)).toBe(expected);
     // @ts-ignore
     expect(setEquals(set2, set1)).toBe(expected);
   });
-
-  // TODO: Add more tests
 });
