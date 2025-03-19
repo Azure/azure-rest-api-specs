d6eced77769d9e42788450e73e745e0682fadaec
Mike Harder <mharder@microsoft.com>
2025-03-19 07:32:58 +0000

    more equality tests

--- .github/test/equality.test.js
+++ .github/test/equality.test.js
@@ -4,9 +4,22 @@ import { describe, expect, it } from "vitest";
 import { setEquals } from "../src/equality.js";
 
 describe("equality", () => {
-  it("setEquals", () => {
+  // TODO: replace sets with arrays in inputs, add sets of size 2 and 3
+  it.each([
+    [null, null, false],
+    [undefined, undefined, false],
+    [null, undefined, false],
+    [null, new Set(), false],
+    [undefined, new Set(), false],
+    [new Set(), new Set(), true],
+    [new Set(), new Set([1]), false],
+    [new Set([1]), new Set([1]), true],
+    [new Set([1]), new Set([2]), false],
+  ])("setEquals(%s, %s, %s)", (set1, set2, expected) => {
     // @ts-ignore
-    expect(setEquals(null, null)).toBe(false);
+    expect(setEquals(set1, set2)).toBe(expected);
+    // @ts-ignore
+    expect(setEquals(set2, set1)).toBe(expected);
   });
 
   // TODO: Add more tests
