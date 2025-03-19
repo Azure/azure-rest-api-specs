56af88f657312fad0e1a8db96085915eb8629e8f
Mike Harder <mharder@microsoft.com>
2025-03-19 17:27:29 +0000

    improve test matrix

--- .github/test/equality.test.js
+++ .github/test/equality.test.js
@@ -5,31 +5,32 @@ import { setEquals } from "../src/equality.js";
 
 describe("equality", () => {
   it.each([
-    // null and undefined are never equal to anything
+    // not instanceof Set: null and undefined
     [null, null, false],
     [undefined, undefined, false],
     [null, undefined, false],
     [null, [], false],
     [undefined, [], false],
+    // not instanceof Set: number, string, object, array
+    [1, 1, false],
+    ["a", "a", false],
+    [{}, {}, false],
+    [[], [], false],
     // different lengths
-    [[], [1], false],
-    [[1], [1, 2], false],
-    [[1, 2], [1, 2, 3], false],
+    [new Set(), new Set([1]), false],
+    [new Set([1]), new Set([1, 2]), false],
+    [new Set([1, 2]), new Set([1, 2, 3]), false],
     // different elements
-    [[1], [2], false],
-    [[1, 2], [1, 3], false],
-    [[1, 2], [2, 3], false],
-    [[1, 2, 3], [2, 3, 4], false],
+    [new Set([1]), new Set([2]), false],
+    [new Set([1, 2]), new Set([1, 3]), false],
+    [new Set([1, 2]), new Set([2, 3]), false],
+    [new Set([1, 2, 3]), new Set([2, 3, 4]), false],
     // sets with same elements, size >= 0
-    [[], [], true],
-    [[1], [1], true],
-    [[1, 2], [1, 2], true],
-    [[1, 2, 3], [1, 2, 3], true],
-  ])("setEquals(%s, %s, %s)", (input1, input2, expected) => {
-    const [set1, set2] = [input1, input2].map((input) =>
-      input instanceof Array ? new Set(input) : input,
-    );
-
+    [new Set(), new Set(), true],
+    [new Set([1]), new Set([1]), true],
+    [new Set([1, 2]), new Set([1, 2]), true],
+    [new Set([1, 2, 3]), new Set([1, 2, 3]), true],
+  ])("setEquals(%s, %s, %s)", (set1, set2, expected) => {
     // @ts-ignore
     expect(setEquals(set1, set2)).toBe(expected);
     // @ts-ignore
