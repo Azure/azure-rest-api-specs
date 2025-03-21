f657cd5c7b1869d19665ca2a7e33f8abe97780e9
Mike Harder <mharder@microsoft.com>
2025-03-21 07:31:56 +0000

    add test to load all modules

---
+++ .github/workflows/src/github-test.js
@@ -0,0 +1,8 @@
+// @ts-check
+
+/**
+ * @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments
+ */
+export default async function importAllModules({ core }) {
+  core.info("importAllModules()");
+}
