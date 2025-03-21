eaa39c15bf005c15e9ab241a0feafe72c95edeef
Mike Harder <mharder@microsoft.com>
2025-03-21 07:48:23 +0000

    Get list of scripts under .github

--- .github/workflows/src/github-test.js
+++ .github/workflows/src/github-test.js
@@ -1,8 +1,25 @@
 // @ts-check
 
+import { readdir } from "fs/promises";
+import { basename, join, normalize, sep } from "path";
+
 /**
  * @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments
  */
 export default async function importAllModules({ core }) {
   core.info("importAllModules()");
+
+  const workspace = process.env.GITHUB_WORKSPACE;
+  if (!workspace) {
+    throw new Error("Env var GITHUB_WORKSPACE must be set");
+  }
+
+  const githubPath = join(workspace, ".github");
+  const scripts = (await readdir(githubPath, { recursive: true })).filter(
+    (f) =>
+      normalize(f).split(sep).includes("src") &&
+      basename(f).toLowerCase().endsWith(".js"),
+  );
+
+  core.info(JSON.stringify(scripts));
 }
