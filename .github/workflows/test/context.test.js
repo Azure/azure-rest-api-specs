0342c0618248172ba95c39095620dec15a99938d
Mike Harder <mharder@microsoft.com>
2025-03-23 05:46:23 +0000

    100% coverage

--- .github/workflows/test/context.test.js
+++ .github/workflows/test/context.test.js
@@ -236,13 +236,28 @@ describe("extractInputs", () => {
         },
       );
 
-      github.rest.search.issuesAndPullRequests.mockResolvedValue({
-        data: { total_count: 1, items: [{ number: 789 }] },
-      });
-
-      const inputsPromise = extractInputs(github, context, createMockCore());
       if (numPullRequests === 0) {
-        await expect(inputsPromise).resolves.toEqual({
+        github.rest.search.issuesAndPullRequests.mockResolvedValue({
+          data: { total_count: 0, items: [] },
+        });
+
+        await expect(
+          extractInputs(github, context, createMockCore()),
+        ).resolves.toEqual({
+          owner: "TestRepoOwnerLogin",
+          repo: "TestRepoName",
+          head_sha: "abc123",
+          issue_number: NaN,
+          run_id: 456,
+        });
+
+        github.rest.search.issuesAndPullRequests.mockResolvedValue({
+          data: { total_count: 1, items: [{ number: 789 }] },
+        });
+
+        await expect(
+          extractInputs(github, context, createMockCore()),
+        ).resolves.toEqual({
           owner: "TestRepoOwnerLogin",
           repo: "TestRepoName",
           head_sha: "abc123",
@@ -252,18 +267,19 @@ describe("extractInputs", () => {
 
         expect(github.rest.search.issuesAndPullRequests).toHaveBeenCalled();
       } else if (numPullRequests === 1) {
-        await expect(inputsPromise).resolves.toEqual({
+        await expect(
+          extractInputs(github, context, createMockCore()),
+        ).resolves.toEqual({
           owner: "TestRepoOwnerLogin",
           repo: "TestRepoName",
           head_sha: "abc123",
           issue_number: 123,
           run_id: 456,
         });
-
-        // Search API should not be needed
-        expect(github.rest.search.issuesAndPullRequests).toBeCalledTimes(0);
       } else {
-        await expect(inputsPromise).rejects.toThrow();
+        await expect(
+          extractInputs(github, context, createMockCore()),
+        ).rejects.toThrow("Unexpected number of pull requests");
       }
 
       expect(
