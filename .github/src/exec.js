b6d47e24a8dc804528dad722abe0c2b86f90da29
Mike Harder <mharder@microsoft.com>
2025-03-26 13:49:39 -0700

    [github] Add "cwd" (current working directory) to options bags (#33520)
    - Keep defaulting to GITHUB_WORKSPACE if available, else process.cwd()
    - Fixes #33284

--- .github/src/exec.js
+++ .github/src/exec.js
@@ -2,26 +2,29 @@
 
 import child_process from "child_process";
 import { promisify } from "util";
-const exec = promisify(child_process.exec);
+const execImpl = promisify(child_process.exec);
 
 /**
  * @param {string} command
  * @param {Object} [options]
+ * @param {string} [options.cwd] Current working directory.  Default is process.env.GITHUB_WORKSPACE or process.cwd().
  * @param {import('./types.js').ILogger} [options.logger]
  * @param {number} [options.maxBuffer]
  */
-export async function execRoot(command, options = {}) {
-  // Node default is 1024 * 1024, which is too small for some git commands returning many entities or large file content.
-  // To support "git show", should be larger than the largest swagger file in the repo (2.5 MB as of 2/28/2025).
-  const defaultMaxBuffer = 16 * 1024 * 1024;
+export async function exec(command, options = {}) {
+  const {
+    cwd = process.env.GITHUB_WORKSPACE,
+    logger,
+    // Node default is 1024 * 1024, which is too small for some git commands returning many entities or large file content.
+    // To support "git show", should be larger than the largest swagger file in the repo (2.5 MB as of 2/28/2025).
+    maxBuffer = 16 * 1024 * 1024,
+  } = options;
 
-  const { logger, maxBuffer = defaultMaxBuffer } = options;
-
-  logger?.info(`execRoot("${command}")`);
+  logger?.info(`exec("${command}")`);
 
   // TODO: Handle errors
-  const result = await exec(command, {
-    cwd: process.env.GITHUB_WORKSPACE,
+  const result = await execImpl(command, {
+    cwd,
     maxBuffer,
   });
 
