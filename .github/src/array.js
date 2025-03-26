0b28ae300dadcec8d2ecbae5710e564d64a46b45
Mike Harder <mharder@microsoft.com>
2025-03-26 06:07:32 +0000

    improve type docs

--- .github/src/array.js
+++ .github/src/array.js
@@ -2,9 +2,9 @@
 
 /**
  * @template T, U
- * @param {T[]} array - The input array of type T.
- * @param {(item: T, index: number) => Promise<U>} callbackfn - Async function to apply to each item.
- * @returns {Promise<U[]>} A promise that resolves to an array of type U.
+ * @param {T[]} array
+ * @param {(value: T, index: number, array: T[]) => Promise<U>} callbackfn
+ * @returns {Promise<U[]>}
  */
 export async function mapAsync(array, callbackfn) {
   return Promise.all(array.map(callbackfn));
