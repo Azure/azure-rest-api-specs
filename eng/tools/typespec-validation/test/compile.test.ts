b08b15604ece89b6530c2f2874c8dcdb9eb1a40e
Mike Harder <mharder@microsoft.com>
2025-04-01 22:06:44 +0000

    Add tests for suppressions

--- eng/tools/typespec-validation/test/compile.test.ts
+++ eng/tools/typespec-validation/test/compile.test.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { describe, expect, it } from "vitest";
 import { RuleResult } from "../src/rule-result.js";
 import { CompileRule } from "../src/rules/compile.js";
@@ -5,16 +6,34 @@ import { TsvHost } from "../src/tsv-host.js";
 import { TsvTestHost } from "./tsv-test-host.js";
 
 const swaggerPath = "data-plane/Azure.Foo/preview/2022-11-01-preview/foo.json";
+const handwrittenSwaggerPath = "data-plane/Azure.Foo/preview/2021-11-01-preview/foo.json";
 
 describe("compile", function () {
   it("should succeed if project can compile", async function () {
     const host = new TsvTestHost();
 
+    const compileOutput =
+      // header, not a filename
+      "header\n" +
+      // windows line endings
+      "\r\n" +
+      // ensure paths are trimmed
+      `\t${swaggerPath} \n` +
+      // ensure paths are normalized
+      `${path.win32.normalize(swaggerPath)}\n` +
+      // ensure filtered to JSON files
+      "data-plane/readme.md\n" +
+      // ensure examples are skipped
+      `${swaggerPath.replace("foo.json", "examples/example.json")}\n`;
+
     host.runCmd = async (_cmd: string, _cwd: string): Promise<[Error | null, string, string]> => {
-      return [null, "version\n" + swaggerPath + "\nsuccess", ""];
+      return [null, compileOutput, ""];
     };
 
-    host.globby = async () => [swaggerPath];
+    // ensure handwritten swaggers are ignored
+    host.globby = async () => [swaggerPath, handwrittenSwaggerPath];
+    host.readFile = async (path) =>
+      path === swaggerPath ? '{"info": {"x-typespec-generated": true}}' : "{}";
 
     await expect(new CompileRule().execute(host, TsvTestHost.folder)).resolves.toMatchObject({
       success: true,
@@ -69,14 +88,22 @@ describe("compile", function () {
   it("should fail if extra swaggers", async function () {
     const host = new TsvTestHost();
 
-    const tspCompileOutput = swaggerPath;
-
     host.runCmd = async (_cmd: string, _cwd: string): Promise<[Error | null, string, string]> => {
-      return [null, tspCompileOutput, ""];
+      return [null, swaggerPath, ""];
     };
 
     // Simulate extra swagger
-    host.globby = async () => [swaggerPath, swaggerPath.replace("2022", "2023")];
+    host.globby = async () => [
+      swaggerPath,
+      swaggerPath.replace("2022", "2023"),
+      swaggerPath.replace("2023", "2024"),
+    ];
+
+    host.readFile = async (path) => {
+      return path.includes("2024")
+        ? '{"info": {"x-typespec-generated": true}}'
+        : '{"info": {"x-cadl-generated": true}}';
+    };
 
     await expect(new CompileRule().execute(host, TsvTestHost.folder)).resolves.toMatchObject({
       success: false,
@@ -84,7 +111,66 @@ describe("compile", function () {
     });
   });
 
-  // TODO: Add tests for all other branches of extra swaggers
+  it("supports suppressions", async function () {
+    const host = new TsvTestHost();
+
+    host.runCmd = async (_cmd: string, _cwd: string): Promise<[Error | null, string, string]> => {
+      return [null, swaggerPath, ""];
+    };
+
+    // Simulate extra swagger
+    host.globby = async () => [
+      swaggerPath,
+      swaggerPath.replace("2022", "2023"),
+      swaggerPath.replace("2023", "2024"),
+    ];
+
+    host.readFile = async (path) => {
+      return path.includes("2024")
+        ? '{"info": {"x-typespec-generated": true}}'
+        : '{"info": {"x-cadl-generated": true}}';
+    };
+
+    host.getSuppressions = async (path) => {
+      return path.includes("2023") || path.includes("2024")
+        ? [
+            {
+              tool: "TypeSpecValidation",
+              rules: ["Compile"],
+              subRules: ["ExtraSwagger"],
+              paths: [swaggerPath.replace("2022", "2023"), swaggerPath.replace("2023", "2024")],
+              reason: "test reason",
+            },
+          ]
+        : [];
+    };
+
+    await expect(new CompileRule().execute(host, TsvTestHost.folder)).resolves.toMatchObject({
+      success: true,
+    });
+  });
+
+  it("throws on invalid suppressions", async function () {
+    const host = new TsvTestHost();
+
+    host.runCmd = async (_cmd: string, _cwd: string): Promise<[Error | null, string, string]> => {
+      return [null, swaggerPath, ""];
+    };
+
+    host.getSuppressions = async () => [
+      {
+        tool: "TypeSpecValidation",
+        rules: ["Compile"],
+        subRules: ["ExtraSwagger"],
+        paths: ["**/*"],
+        reason: "test reason",
+      },
+    ];
+
+    await expect(new CompileRule().execute(host, TsvTestHost.folder)).rejects.toThrow(
+      "Invalid path",
+    );
+  });
 
   it("should skip git diff check if compile fails", async function () {
     let host = new TsvTestHost();
