3287eb3c38b2d457e617a2bc28bc31732d8c9e41
Daniel Jurek <djurek@microsoft.com>
2025-04-02 19:16:55 +0000

    Add logging for e2e test

--- eng/tools/lint-diff/test/lint-diff.test.ts
+++ eng/tools/lint-diff/test/lint-diff.test.ts
@@ -3,8 +3,16 @@ import { test, describe } from "vitest";
 
 // TODO: Actual tests
 describe("e2e", () => {
-  test.concurrent("Executes", async ({ expect }) => {
-    const { exitCode } = await execa("npm", ["exec", "--no", "--", "lint-diff"], { reject: false });
-    expect(exitCode).toBe(1);
+  test.sequential("Executes", async ({ expect }) => {
+    const output = await execa("npm", ["exec", "--no", "--", "lint-diff"], { reject: false });
+
+    try {
+      expect(output.exitCode).toBe(1);
+    } catch (error) {
+      console.log(`stdout: ${output.stdout}`);
+      console.log(`stderr: ${output.stderr}`);
+      console.error("Error:", error);
+      throw error;
+    }
   });
 });
