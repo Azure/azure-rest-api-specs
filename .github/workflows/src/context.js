05fc8e6c5a2cffc038da9ef990562a8bd47ffe57
Daniel Jurek <djurek@microsoft.com>
2025-04-08 15:41:01 -0700

    [EngSys] context.js: Add support for "check_suite" (#33806)
    * context.js: Add support for "check_suite"
    
    * todo, formatting, types
    
    * - getRepositoryInfo
    
    * Apply suggestions from code review
    
    Co-authored-by: Mike Harder <mharder@microsoft.com>
    
    * Review feedback: no spread
    
    * Review feedback
    
    * formatting
    
    ---------
    
    Co-authored-by: Mike Harder <mharder@microsoft.com>

--- .github/workflows/src/context.js
+++ .github/workflows/src/context.js
@@ -246,22 +246,38 @@ export async function extractInputs(github, context, core) {
         `Could not extract build ID or project URL from check run details URL: ${checkRun.details_url}`,
       );
     }
-    if (
-      !context.payload.repository ||
-      !context.payload.repository.owner ||
-      !context.payload.repository.owner.login ||
-      !context.payload.repository.name
-    ) {
-      throw new Error(
-        `Could not extract repository owner or name from context payload: ${JSON.stringify(context.payload.repository)}`,
+
+    const payload =
+      /** @type {import("@octokit/webhooks-types").CheckRunEvent} */ (
+        context.payload
       );
-    }
+    const repositoryInfo = getRepositoryInfo(payload.repository);
     inputs = {
-      owner: context.payload.repository.owner.login,
-      repo: context.payload.repository.name,
+      owner: repositoryInfo.owner,
+      repo: repositoryInfo.repo,
       head_sha: checkRun.head_sha,
       ado_build_id: match[2],
       ado_project_url: match[1],
+      issue_number: NaN,
+      run_id: NaN,
+    };
+  } else if (
+    context.eventName === "check_suite" &&
+    context.payload.action === "completed"
+  ) {
+    const payload =
+      /** @type {import("@octokit/webhooks-types").CheckSuiteCompletedEvent} */ (
+        context.payload
+      );
+
+    const repositoryInfo = getRepositoryInfo(payload.repository);
+    inputs = {
+      owner: repositoryInfo.owner,
+      repo: repositoryInfo.repo,
+      head_sha: payload.check_suite.head_sha,
+
+      // These are NaN today because the only consumer of this event needs only
+      // the head_sha
       issue_number: NaN,
       run_id: NaN,
     };
@@ -273,4 +289,26 @@ export async function extractInputs(github, context, core) {
 
   core.info(`inputs: ${JSON.stringify(inputs)}`);
   return inputs;
+}
+
+/**
+ * @param {import("@octokit/webhooks-types").Repository | undefined} repository
+ * @returns {{ owner: string, repo: string }}
+ */
+function getRepositoryInfo(repository) {
+  if (
+    !repository ||
+    !repository.owner ||
+    !repository.owner.login ||
+    !repository.name
+  ) {
+    throw new Error(
+      `Could not extract repository owner or name from context payload: ${JSON.stringify(repository)}`,
+    );
+  }
+
+  return {
+    owner: repository.owner.login,
+    repo: repository.name,
+  };
 }
