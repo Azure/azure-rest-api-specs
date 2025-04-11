40a68bea1d5929be0c03ded54ff6665af830ac88
Mike Harder <mharder@microsoft.com>
2025-04-11 07:26:21 +0000

    simplify test

--- eng/tools/typespec-validation/test/npm-prefix.test.ts
+++ eng/tools/typespec-validation/test/npm-prefix.test.ts
@@ -7,8 +7,8 @@ import { IGitOperation, TsvTestHost } from "./tsv-test-host.js";
 describe("npm-prefix", function () {
   it("should succeed if node returns inconsistent drive letter capitalization", async function () {
     let host = new TsvTestHost(path.win32);
-    host.runFile = async (file: string, args: string[], _cwd: string): Promise<[Error | null, string, string]> => {
-      if ([file, ...args].join(" ").includes("prefix")) {
+    host.runFile = async (_file: string, args: string[], _cwd: string): Promise<[Error | null, string, string]> => {
+      if (args.includes("prefix")) {
         return [null, `C:${path.sep}Git${path.sep}azure-rest-api-specs`, ""];
       } else {
         return [null, "", ""];
@@ -39,8 +39,8 @@ describe("npm-prefix", function () {
 
   it("should fail if npm prefix mismatch", async function () {
     let host = new TsvTestHost();
-    host.runFile = async (file: string, args: string[], _cwd: string): Promise<[Error | null, string, string]> => {
-      if ([file, ...args].join(" ").includes("prefix")) {
+    host.runFile = async (_file: string, args: string[], _cwd: string): Promise<[Error | null, string, string]> => {
+      if (args.includes("prefix")) {
         return [null, "/Git/azure-rest-api-specs/specification/foo", ""];
       } else {
         return [null, "", ""];
