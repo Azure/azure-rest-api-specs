ef196785552a7e79e5db813cb513bd94c8e7b1e1
Mike Harder <mharder@microsoft.com>
2025-04-11 11:10:06 -0700

    [.github] Use execFile() instead of exec() (#33884)
    - execFile(file, args) is more secure than exec(cmd)
    - exec(cmd) is vulnerable to shell injection

--- .github/shared/test/exec.test.js
+++ .github/shared/test/exec.test.js
@@ -1,65 +1,30 @@
-import { EOL } from "os";
 import { describe, expect, it } from "vitest";
-import { buildCmd, exec } from "../src/exec.js";
+import { execFile } from "../src/exec.js";
 import { createMockLogger } from "./mocks.js";
 
-describe("exec", () => {
-  const str = "test";
-  const cmd = `echo ${str}`;
-  const expected = `${str}${EOL}`;
+describe("execFile", () => {
+  const file = "node";
+  const args = ["-e", `console.log("test")`];
+  const expected = "test\n";
 
   it.each([{}, { logger: createMockLogger() }])(
     "exec succeeds with default buffer (options: %o)",
     async (options) => {
-      await expect(exec(cmd, options)).resolves.toEqual(expected);
+      await expect(execFile(file, args, options)).resolves.toEqual(expected);
     },
   );
 
   it("exec succeeds with exact-sized buffer", async () => {
-    await expect(exec(cmd, { maxBuffer: expected.length })).resolves.toEqual(
-      expected,
-    );
+    await expect(
+      execFile(file, args, { maxBuffer: expected.length }),
+    ).resolves.toEqual(expected);
   });
 
   it("exec fails with too-small buffer", async () => {
     await expect(
-      exec(cmd, { maxBuffer: expected.length - 1 }),
+      execFile(file, args, { maxBuffer: expected.length - 1 }),
     ).rejects.toThrowError(
       expect.objectContaining({ code: "ERR_CHILD_PROCESS_STDIO_MAXBUFFER" }),
-    );
-  });
-
-  it("buildCmd", () => {
-    const cmd = buildCmd(
-      "foo",
-      // Excluded
-      null,
-      // Arg is *not* trimmed, so trailing whitespace is preserved
-      "spaceAfter ",
-      // Excluded since all-whitespace
-      " \t",
-      // Excluded
-      undefined,
-      // Arg is *not* trimmed, so leading whitespace is preserved
-      " spaceBefore",
-      // Non-string values, both truthy and falsy
-      true,
-      false,
-      -1,
-      0,
-      1,
-      "",
-      NaN,
-      // Invalid, but we currently don't exclude it, so test it
-      {},
-      // Converts to "", so excluded as empty string
-      [],
-      // Converts to string
-      ["arrayElement1", "arrayElement2"],
-      ["newArray"],
-    );
-    expect(cmd).toBe(
-      "foo spaceAfter   spaceBefore true false -1 0 1 NaN [object Object] arrayElement1,arrayElement2 newArray",
     );
   });
 });
