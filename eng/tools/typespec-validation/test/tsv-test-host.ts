95eda2b9a673690afab68824d5d68b09c026dcdd
Mike Harder <mharder@microsoft.com>
2025-04-14 19:00:50 -0700

    [github/shared] Change exec and git funcs to return {stdout, stderr} (#33937)
    - Change exec and git funcs to return {stdout: string, stderr: string}
    - Update all consumers of exec and git funcs
    - Add execNpm() helper to shared
    - TypeSpecValidation: Remove host.runFile() and host.runNpm()

--- eng/tools/typespec-validation/test/tsv-test-host.ts
+++ eng/tools/typespec-validation/test/tsv-test-host.ts
@@ -1,5 +1,5 @@
 import { Options as GlobbyOptions } from "globby";
-import defaultPath, { dirname, join, PlatformPath } from "path";
+import defaultPath, { PlatformPath } from "path";
 import { RuleResult } from "../src/rule-result.js";
 import { IGitOperation, TsvHost } from "../src/tsv-host.js";
 
@@ -34,35 +34,9 @@ export class TsvTestHost implements TsvHost {
     };
   }
 
-  async runFile(
-    file: string,
-    args: string[],
-    cwd?: string,
-  ): Promise<[Error | null, string, string]> {
-    let err = null;
-    let stdout = `default ${file} ${args.join(" ")} at ${cwd}`;
-    let stderr = "";
-
-    return [err, stdout, stderr];
-  }
-
-  async runNpm(args: string[], cwd?: string): Promise<[Error | null, string, string]> {
-    const [file, defaultArgs] =
-      process.platform === "win32"
-        ? // Only way I could find to run "npm" on Windows, without using the shell (e.g. "cmd /c npm ...")
-          // "C:\Program Files\nodejs\node.exe", ["C:\Program Files\nodejs\node_modules\npm\bin\npm-cli.js"]
-          [
-            process.execPath,
-            [join(dirname(process.execPath), "node_modules", "npm", "bin", "npm-cli.js")],
-          ]
-        : ["npm", []];
-
-    return this.runFile(file, [...defaultArgs, ...args], cwd);
-  }
-
-  async gitDiffTopSpecFolder(host: TsvHost, folder: string): Promise<RuleResult> {
+  async gitDiffTopSpecFolder(_host: TsvHost, folder: string): Promise<RuleResult> {
     let success = true;
-    let stdout = `Running git diff on folder ${folder}, running default cmd ${host.runFile("", [], "")}`;
+    let stdout = `Running git diff on folder ${folder}}`;
     let stderr = "";
 
     return {
