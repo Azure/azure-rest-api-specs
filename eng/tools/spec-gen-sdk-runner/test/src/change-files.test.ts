87764196c359be267519550126b7676594b2e88b
v-tianxi <v-tianxi@microsoft.com>
2025-04-16 14:44:49 +0800

    add log test

--- eng/tools/spec-gen-sdk-runner/test/src/change-files.test.ts
+++ eng/tools/spec-gen-sdk-runner/test/src/change-files.test.ts
@@ -1,25 +1,25 @@
 import { describe, test, expect, vi, beforeEach } from "vitest";
 import { detectChangedSpecConfigFiles } from "../../src/change-files.js";
 import * as utils from "../../src/utils.js";
-import { logMessage } from "../../src/log.js";
 import { SpecGenSdkCmdInput } from "../../src/types.js";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
 
-vi.mock("../../src/utils.js", () => ({
-  getChangedFiles: vi.fn(),
-  searchRelatedParentFolders: vi.fn(),
-  searchSharedLibrary: vi.fn(),
-  searchRelatedTypeSpecProjectBySharedLibrary: vi.fn().mockReturnValue({}),
-  groupPathsByService: vi.fn(),
-  createCombinedSpecs: vi.fn(),
-}));
-
-vi.mock("../src/log.js", () => ({
-  logMessage: vi.fn(),
-}));
+vi.mock("../../src/utils.js", async () => {
+  const actual = await vi.importActual<typeof import("../../src/utils.js")>("../../src/utils.js")
+  
+  return {
+    ...actual,
+    getChangedFiles: vi.fn(),
+  }
+})
 
 describe("detectChangedSpecConfigFiles", () => {
+  const currentFilePath = fileURLToPath(import.meta.url);
+  const repoRoot = path.resolve(path.dirname(currentFilePath), "../fixtures/");
+
   const mockCommandInput: SpecGenSdkCmdInput = {
-      localSpecRepoPath: "/path/to/spec-repo",
+      localSpecRepoPath: repoRoot,
       workingFolder: "",
       runMode: "",
       localSdkRepoPath: "",
@@ -33,102 +33,114 @@ describe("detectChangedSpecConfigFiles", () => {
     vi.clearAllMocks();
   });
 
-  test("should return an empty array if no files are changed", () => {
+  test("case with empty change files", () => {
     vi.mocked(utils.getChangedFiles).mockReturnValue([]);
     const result = detectChangedSpecConfigFiles(mockCommandInput);
     expect(result).toEqual([]);
-    expect(logMessage).toHaveBeenCalledWith("No files changed in the PR");
   });
 
-  test("should filter and process changed files correctly", () => {
+  test("case with readme files", () => {
     vi.mocked(utils.getChangedFiles).mockReturnValue([
-      "specification/service1/resource-manager/readme.md",
-      "specification/service1/resource-manager/tspconfig.yaml",
-      "specification/service2/data-plane/readme.md",
-      "specification/service2/data-plane/tspconfig.yaml",
+      "specification/contosowidgetmanager/resource-manager/Microsoft.Contoso/preview/2021-10-01-preview/examples/Employees_Get.json",
+      "specification/contosowidgetmanager/data-plane/Azure.Contoso.WidgetManager/preview/2022-11-01-preview/widgets.json",
     ]);
-
-    vi.mocked(utils.searchRelatedParentFolders).mockImplementation((_fileList, options) => {
-      if (options.searchFileRegex?.toString() === /^readme.md$/.toString()) {
-        return {
-          "specification/service1/resource-manager": ["readme.md"],
-          "specification/service2/data-plane": ["readme.md"],
-        };
-      }
-      if (options.searchFileRegex?.toString() === /^tspconfig.yaml$/.toString()) {
-        return {
-          "specification/service1/resource-manager": ["tspconfig.yaml"],
-          "specification/service2/data-plane": ["tspconfig.yaml"],
-        };
-      }
-      return {
-        "specification/service1/resource-manager": [],
-        "specification/service2/data-plane": [],
-      };
-    });
-
-    vi.mocked(utils.groupPathsByService).mockReturnValue(
-      new Map([
-        [
-          "service1",
-          {
-            resourceManagerPaths: [{ path: "specification/service1/resource-manager" }],
-            dataPlanePaths: [],
-            managementPaths: [],
-            otherTypeSpecPaths: [],
-          },
-        ],
-        [
-          "service2",
-          {
-            resourceManagerPaths: [],
-            dataPlanePaths: [{ path: "specification/service2/data-plane" }],
-            managementPaths: [],
-            otherTypeSpecPaths: [],
-          },
-        ],
-      ])
-    );
-
-    vi.mocked(utils.createCombinedSpecs).mockImplementation((readmePath, typespecPaths) => {
-      return [
-        {
-          specs: [readmePath, ...typespecPaths],
-          readmeMd: `${readmePath}/readme.md`,
-          typespecProject: `${typespecPaths[0]}/tspconfig.yaml`,
-        },
-      ];
-    });
 
     const result = detectChangedSpecConfigFiles(mockCommandInput);
 
     expect(result).toHaveLength(2);
     expect(result[0]).toEqual({
-      specs: ["specification/service1/resource-manager/readme.md", "specification/service1/resource-manager/tspconfig.yaml"],
-      readmeMd: "specification/service1/resource-manager/readme.md",
-      typespecProject: "specification/service1/resource-manager/tspconfig.yaml",
+      specs: [
+        "specification/contosowidgetmanager/resource-manager/Microsoft.Contoso/preview/2021-10-01-preview/examples/Employees_Get.json"
+      ],
+      readmeMd: "specification/contosowidgetmanager/resource-manager/readme.md",
     });
     expect(result[1]).toEqual({
-      specs: ["specification/service2/data-plane/readme.md", "specification/service2/data-plane/tspconfig.yaml"],
-      readmeMd: "specification/service2/data-plane/readme.md",
-      typespecProject: "specification/service2/data-plane/tspconfig.yaml",
+      specs: [
+        "specification/contosowidgetmanager/data-plane/Azure.Contoso.WidgetManager/preview/2022-11-01-preview/widgets.json"
+      ],
+      readmeMd: "specification/contosowidgetmanager/data-plane/readme.md"
     });
   });
 
-  test("should handle unmatched paths correctly", () => {
+  test("case with tsp files", () => {
     vi.mocked(utils.getChangedFiles).mockReturnValue([
-      "specification/service3/resource-manager/readme.md",
+      "specification/contosowidgetmanager/Contoso.Management/main.tsp",
+      "specification/contosowidgetmanager/Contoso.Management/client.tsp",
+      "specification/contosowidgetmanager/Contoso.WidgetManager/tspconfig.yaml",
     ]);
-
-    vi.mocked(utils.searchRelatedParentFolders).mockReturnValue({});
-    vi.mocked(utils.groupPathsByService).mockReturnValue(new Map());
 
     const result = detectChangedSpecConfigFiles(mockCommandInput);
 
-    expect(result).toHaveLength(1);
+    expect(result).toHaveLength(2);
     expect(result[0]).toEqual({
-      specs: [],
-      readmeMd: "specification/service3/resource-manager/readme.md",
+      specs: [
+        "specification/contosowidgetmanager/Contoso.Management/main.tsp",
+        "specification/contosowidgetmanager/Contoso.Management/client.tsp"
+      ],
+      typespecProject: "specification/contosowidgetmanager/Contoso.Management/tspconfig.yaml"
+    });
+    expect(result[1]).toEqual({
+      specs: [
+        "specification/contosowidgetmanager/Contoso.WidgetManager/tspconfig.yaml"
+      ],
+      typespecProject: "specification/contosowidgetmanager/Contoso.WidgetManager/tspconfig.yaml"
+    });
+  });
+
+  test("case with shared files", () => {
+    vi.mocked(utils.getChangedFiles).mockReturnValue([
+      "specification/contosowidgetmanager/Contoso.WidgetManager.Shared/main.tsp",
+      "specification/contosowidgetmanager/Contoso.Management/client.tsp",
+      "specification/contosowidgetmanager/Contoso.WidgetManager/tspconfig.yaml",
+    ]);
+
+    const result = detectChangedSpecConfigFiles(mockCommandInput);
+
+    expect(result).toHaveLength(2);
+    expect(result[0]).toEqual({
+      specs: [
+        "specification/contosowidgetmanager/Contoso.Management/client.tsp"
+      ],
+      typespecProject: "specification/contosowidgetmanager/Contoso.Management/tspconfig.yaml"
+    });
+    expect(result[1]).toEqual({
+      specs: [
+        "specification/contosowidgetmanager/Contoso.WidgetManager/tspconfig.yaml",
+        "specification/contosowidgetmanager/Contoso.WidgetManager.Shared/main.tsp"
+      ],
+      typespecProject: "specification/contosowidgetmanager/Contoso.WidgetManager/tspconfig.yaml"
+    });
+  });
+
+  test("case with readme, tsp, shared files", () => {
+    vi.mocked(utils.getChangedFiles).mockReturnValue([
+      "specification/contosowidgetmanager/resource-manager/Microsoft.Contoso/preview/2021-10-01-preview/examples/Employees_Get.json",
+      "specification/contosowidgetmanager/data-plane/Azure.Contoso.WidgetManager/preview/2022-11-01-preview/widgets.json",
+      "specification/contosowidgetmanager/Contoso.WidgetManager.Shared/main.tsp",
+      "specification/contosowidgetmanager/Contoso.Management/client.tsp",
+      "specification/contosowidgetmanager/Contoso.WidgetManager/tspconfig.yaml",
+    ]);
+
+    const result = detectChangedSpecConfigFiles(mockCommandInput);
+
+    console.log(result)
+    expect(result).toHaveLength(2);
+    expect(result[0]).toEqual({
+      specs: [
+        "specification/contosowidgetmanager/resource-manager/Microsoft.Contoso/preview/2021-10-01-preview/examples/Employees_Get.json",
+        "specification/contosowidgetmanager/Contoso.Management/client.tsp"
+      ],
+      readmeMd: "specification/contosowidgetmanager/resource-manager/readme.md",
+      typespecProject: "specification/contosowidgetmanager/Contoso.Management/tspconfig.yaml"
+    });
+    expect(result[1]).toEqual({
+      specs: [
+        "specification/contosowidgetmanager/data-plane/Azure.Contoso.WidgetManager/preview/2022-11-01-preview/widgets.json",
+        "specification/contosowidgetmanager/Contoso.WidgetManager/tspconfig.yaml",
+        "specification/contosowidgetmanager/Contoso.WidgetManager.Shared/main.tsp"
+      ],
+      readmeMd: "specification/contosowidgetmanager/data-plane/readme.md",
+      typespecProject: "specification/contosowidgetmanager/Contoso.WidgetManager/tspconfig.yaml"
     });
   });
 });
