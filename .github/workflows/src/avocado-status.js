34950af1796dc5b37e0c0111ab7a993752dbdff7
Mike Harder <mharder@microsoft.com>
2025-04-16 21:53:01 +0000

    short circuit if label is set

--- .github/workflows/src/avocado-status.js
+++ .github/workflows/src/avocado-status.js
@@ -1,6 +1,7 @@
 // @ts-check
 
 import { extractInputs } from "./context.js";
+import { PER_PAGE_MAX } from "./github.js";
 
 /**
  * @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments
@@ -9,7 +10,37 @@ import { extractInputs } from "./context.js";
 export default async function getStatus({ github, context, core }) {
   core.info("avocado-status.js:getStatus()");
 
-  const { owner, repo, head_sha } = await extractInputs(github, context, core);
+  const { owner, repo, head_sha, issue_number } = await extractInputs(
+    github,
+    context,
+    core,
+  );
+
+  const target_url =
+    `https://github.com/${context.repo.owner}/${context.repo.repo}` +
+    `/actions/runs/${context.runId}`;
+
+  // TODO: Try to extract labels from context (when available) to avoid unnecessary API call
+  const labels = await github.paginate(github.rest.issues.listLabelsOnIssue, {
+    owner: owner,
+    repo: repo,
+    issue_number: issue_number,
+    per_page: PER_PAGE_MAX,
+  });
+  const labelNames = labels.map((label) => label.name);
+
+  if (labelNames.includes("Approved-Avocado")) {
+    await github.rest.repos.createCommitStatus({
+      owner,
+      repo,
+      sha: head_sha,
+      state: "success",
+      context: "[TEST IGNORE] Swagger Avocado",
+      description: "Succeeded due to label 'Approved-Avocado'",
+      target_url,
+    });
+    return;
+  }
 
   // TODO
   // - Get status of check avocado-code
@@ -18,11 +49,23 @@ export default async function getStatus({ github, context, core }) {
   // - If label, set status to neutral
   // - If no label, set status to failed
 
-  const target_url =
-    `https://github.com/${context.repo.owner}/${context.repo.repo}` +
-    `/actions/runs/${context.runId}`;
+  const workflowRuns = await github.paginate(
+    github.rest.actions.listWorkflowRunsForRepo,
+    {
+      owner,
+      repo,
+      event: "pull_request",
+      head_sha,
+      per_page: PER_PAGE_MAX,
+    },
+  );
 
-  github.rest.repos.createCommitStatus({
+  core.info("Workflow Runs:");
+  workflowRuns.forEach((wf) => {
+    core.info(`- ${wf.name}: ${wf.conclusion || wf.status}`);
+  });
+
+  await github.rest.repos.createCommitStatus({
     owner,
     repo,
     sha: head_sha,
