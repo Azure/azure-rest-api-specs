0bc4161a638987d9a89adcb22159a6537a65062e
Mike Harder <mharder@microsoft.com>
2025-04-16 21:00:14 +0000

    Add check avocado-status

---
+++ .github/workflows/src/avocado-status.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/**
+ * @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments
+ * @returns {Promise<void>}
+ */
+export default async function getStatus({ core }) {
+  core.info("avocado-status.js:getSatus()");
+}
