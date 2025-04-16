edf1549e4bb3f0c7855c215f66f5ebcb378ecf72
Mike Harder <mharder@microsoft.com>
2025-04-16 21:25:42 +0000

    Set status

--- .github/workflows/src/avocado-status.js
+++ .github/workflows/src/avocado-status.js
@@ -1,9 +1,34 @@
 // @ts-check
 
+import { extractInputs } from "./context.js";
+
 /**
  * @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments
  * @returns {Promise<void>}
  */
-export default async function getStatus({ core }) {
-  core.info("avocado-status.js:getSatus()");
+export default async function getStatus({ github, context, core }) {
+  core.info("avocado-status.js:getStatus()");
+
+  const { owner, repo, head_sha } = await extractInputs(github, context, core);
+
+  // TODO
+  // - Get status of check avocado-code
+  // - If success, set status to success
+  // - If fail, get status of label Approved-Avocado
+  // - If label, set status to neutral
+  // - If no label, set status to failed
+
+  const target_url =
+    `https://github.com/${context.repo.owner}/${context.repo.repo}` +
+    `/actions/runs/${context.runId}`;
+
+  github.rest.repos.createCommitStatus({
+    owner,
+    repo,
+    sha: head_sha,
+    state: "pending",
+    context: "[TEST IGNORE] Swagger Avocado",
+    description: "Check is running...",
+    target_url,
+  });
 }
