992722118333c59b187623a5ce318fafdefff741
Mike Harder <mharder@microsoft.com>
2025-04-17 22:34:00 +0000

    add specific types

--- .github/workflows/src/github.js
+++ .github/workflows/src/github.js
@@ -6,23 +6,50 @@ export const PER_PAGE_MAX = 100;
  * https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/collaborating-on-repositories-with-code-quality-features/about-status-checks#check-statuses-and-conclusions
  */
 export const CheckStatus = {
-  /** @description The check run completed and has a conclusion. */
+  /**
+   * @type {"completed"}
+   * @description The check run completed and has a conclusion.
+   */
   COMPLETED: "completed",
-  /** @description The check run is waiting for a status to be reported. */
+  /**
+   * @type {"expected"}
+   * @description The check run is waiting for a status to be reported.
+   */
   EXPECTED: "expected",
-  /** @description The check run failed. */
+  /**
+   * @type {"failure"}
+   * @description The check run failed.
+   */
   FAILURE: "failure",
-  /** @description The check run is in progress. */
+  /**
+   * @type {"in_progress"}
+   * @description The check run is in progress.
+   */
   IN_PROGRESS: "in_progress",
-  /** @description The check run is at the front of the queue but the group-based concurrency limit has been reached. */
+  /**
+   * @type {"pending"}
+   * @description The check run is at the front of the queue but the group-based concurrency limit has been reached.
+   */
   PENDING: "pending",
-  /** @description The check run has been queued. */
+  /**
+   * @type {"queued"}
+   * @description The check run has been queued.
+   */
   QUEUED: "queued",
-  /** @description The check run has been created but has not been queued. */
+  /**
+   * @type {"requested"}
+   * @description The check run has been created but has not been queued.
+   */
   REQUESTED: "requested",
-  /** @description The check suite failed during startup. This status is not applicable to check runs. */
+  /**
+   * @type {"startup_failure"}
+   * @description The check suite failed during startup. This status is not applicable to check runs.
+   */
   STARTUP_FAILURE: "startup_failure",
-  /** @description The check run is waiting for a deployment protection rule to be satisfied. */
+  /**
+   * @type {"waiting"}
+   * @description The check run is waiting for a deployment protection rule to be satisfied.
+   */
   WAITING: "waiting",
 };
 
@@ -30,20 +57,44 @@ export const CheckStatus = {
  * https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/collaborating-on-repositories-with-code-quality-features/about-status-checks#check-statuses-and-conclusions
  */
 export const CheckConclusion = {
-  /** @description The check run provided required actions upon its completion. For more information, see Using the REST API to interact with checks. */
+  /**
+   * @type {"action_required"}
+   * @description The check run provided required actions upon its completion. For more information, see Using the REST API to interact with checks.
+   */
   ACTION_REQUIRED: "action_required",
-  /** @description The check run was cancelled before it completed. */
+  /**
+   * @type {"cancelled"}
+   * @description The check run was cancelled before it completed.
+   */
   CANCELLED: "cancelled",
-  /** @description The check run failed. */
+  /**
+   * @type {"failure"}
+   * @description The check run failed.
+   */
   FAILURE: "failure",
-  /** @description The check run completed with a neutral result. This is treated as a success for dependent checks in GitHub Actions. */
+  /**
+   * @type {"neutral"}
+   * @description The check run completed with a neutral result. This is treated as a success for dependent checks in GitHub Actions.
+   */
   NEUTRAL: "neutral",
-  /** @description The check run was skipped. This is treated as a success for dependent checks in GitHub Actions. */
+  /**
+   * @type {"skipped"}
+   * @description The check run was skipped. This is treated as a success for dependent checks in GitHub Actions.
+   */
   SKIPPED: "skipped",
-  /** @description The check run was marked stale by GitHub because it took too long. */
+  /**
+   * @type {"stale"}
+   * @description The check run was marked stale by GitHub because it took too long.
+   */
   STALE: "stale",
-  /** @description The check run completed successfully. */
+  /**
+   * @type {"success"}
+   * @description The check run completed successfully.
+   */
   SUCCESS: "success",
-  /** @description The check run timed out. */
+  /**
+   * @type {"timed_out"}
+   * @description The check run timed out.
+   */
   TIMED_OUT: "timed_out",
 };
