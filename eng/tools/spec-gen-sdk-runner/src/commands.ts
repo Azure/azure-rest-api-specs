4882fa7b732b9d6e4aabbc6a8a851818832df20e
ray chen <raychen@microsoft.com>
2025-04-24 21:16:10 +0000

    testing re-run scenario

--- eng/tools/spec-gen-sdk-runner/src/commands.ts
+++ eng/tools/spec-gen-sdk-runner/src/commands.ts
@@ -512,6 +512,7 @@ function generateArtifact(
       labelAction: hasBreakingChange,
       isSpecGenSdkCheckRequired:
         hasManagementPlaneSpecs && SpecGenSdkRequiredSettings[commandInput.sdkLanguage as SdkName],
+      datetime: new Date().toISOString(),
     };
     fs.writeFileSync(
       path.join(commandInput.workingFolder, specGenSdkArtifactPath, specGenSdkArtifactFileName),
--- eng/tools/spec-gen-sdk-runner/src/types.ts
+++ eng/tools/spec-gen-sdk-runner/src/types.ts
@@ -38,6 +38,7 @@ export interface SpecGenSdkArtifactInfo {
   result: string;
   labelAction?: boolean;
   isSpecGenSdkCheckRequired: boolean;
+  datetime: string;
 }
 
 /**
