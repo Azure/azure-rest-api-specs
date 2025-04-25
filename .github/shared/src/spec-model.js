26d9c2e394da45360af44bead6de369b99bfbdf9
Daniel Jurek <djurek@microsoft.com>
2025-04-25 18:18:46 +0000

    Add spec model features needed to get LintDiff working

--- .github/shared/src/spec-model.js
+++ .github/shared/src/spec-model.js
@@ -3,16 +3,18 @@ import $RefParser from "@apidevtools/json-schema-ref-parser";
 import { readFile, readdir } from "fs/promises";
 import yaml from "js-yaml";
 import { marked } from "marked";
-import { dirname, join, resolve } from "path";
+import { dirname, join, resolve, relative } from "path";
 import { fileURLToPath } from "url";
-import { readme } from "../src/changed-files.js";
+import { example, readme } from "./changed-files.js";
 import { mapAsync } from "./array.js";
+import { ConsoleLogger } from "./logger.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 /**
  * @typedef {Object} SpecModel
+ * @prop {string} repoRoot
  * @prop {Map<string, Readme>} readmes
  */
 
@@ -28,43 +30,53 @@ const __dirname = dirname(__filename);
  * @typedef {Object} Swagger
  * @prop {string} path
  * @prop {string} content
- * @prop {Set<string>} refs
+ * @prop {Map<string, Set<string>>} refs
  */
 
 /**
  * @param {string} folder
  * @param {Object} [options]
- * @param {import('./types.js').ILogger} [options.logger]
+ * @param {import('./logger.js').ILogger} [options.logger]
+ * @param {string} [options.repoRoot]
+ * @param {boolean} [options.debug]
  * @returns {Promise<SpecModel>} All input files for all tags
  */
 export async function getSpecModel(folder, options = {}) {
-  console.log(options);
+  const debug = options.debug || false;
+  const logger = options.logger || new ConsoleLogger(debug);
+  const repoRoot = options.repoRoot || resolve(__dirname, "..", "..", "..");
 
-  const files = await readdir(join(__dirname, "..", "..", "..", folder), {
+  logger.debug(`getSpecModel: ${folder}, options: ${JSON.stringify(options)}`);
+
+  const files = await readdir(join(repoRoot, folder), {
     recursive: true,
   });
   const readmes = files.filter(readme);
+  logger.debug(`Found ${readmes.length} readme files`);
 
   return {
+    repoRoot,
     readmes: new Map(
       await mapAsync(readmes, async (r) => {
-        return [join(folder, r), await getReadme(join(folder, r))];
+        const readmePath = join(folder, r);
+        return [readmePath, await getReadme(readmePath, repoRoot, logger)];
       }),
     ),
   };
 }
 
 /**
- * @param {string} path
+ * @param {string} readmePath
+ * @param {string} repoRoot
+ * @param {import('./logger.js').ILogger} [logger]
  * @returns {Promise<Readme>}
  */
-async function getReadme(path) {
-  // TODO: Do not assume location is with respect to repo root, could be reading
-  // files from a different root location (e.g. "before" state of repo in
-  // another folder).
-  const content = await readFile(join(__dirname, "..", "..", "..", path), {
+async function getReadme(readmePath, repoRoot, logger) {
+  const content = await readFile(join(repoRoot, readmePath), {
     encoding: "utf8",
   });
+
+  logger?.debug(`getReadme: ${readmePath}`);
 
   const tokens = marked.lexer(content);
 
@@ -88,18 +100,46 @@ async function getReadme(path) {
   const tags = new Map();
   for (const block of yamlBlocks) {
     const tagName =
-      block.lang?.match(/yaml \$\(tag\) == '([^']*)'/)?.[1] || "default";
+      block.lang?.match(/yaml.*\$\(tag\) ?== ?'([^']*)'/)?.[1] || "default";
 
-    if (tagName === "default") {
-      // Skip yaml blocks where this is no tag
+    if (tagName === "default" || tagName === "all-api-versions") {
+      // Skip yaml blocks where this is no tag or tag is all-api-versions
       continue;
     }
+
     const obj = /** @type {any} */ (yaml.load(block.text));
+
+    if (!obj) {
+      logger?.debug(`No yaml object found for tag ${tagName} in ${readmePath}`);
+      continue;
+    }
+
+    if (!obj["input-file"]) {
+      // The yaml block does not contain an input-file key
+      continue;
+    }
+
+    // This heuristic assumes that a previous definition of the tag with no 
+    // swaggers means that the previous definition did not have an input-file
+    // key. It's possible that the previous defintion had an `input-file: []`
+    // or something like it.
+    if (tags.has(tagName) && tags.get(tagName).length > 0) {
+      // The tag already exists and has a swagger file. This is an error as
+      // there should only be one definition of input-files per tag. 
+      const message = `Multiple input-file definitions for tag ${tagName} in ${readmePath}`;
+      logger?.error(message);
+      throw new Error(message);
+    }
 
     /** @type Swagger[] */
     const swaggers = [];
-    for (const swaggerPath of obj["input-file"]) {
-      const swagger = await getSwagger(join(dirname(path), swaggerPath));
+
+    // It's possible for input-file to be a string or an array
+    const inputFiles = Array.isArray(obj["input-file"]) 
+      ? obj["input-file"]
+      : [obj["input-file"]];
+    for (const swaggerPath of inputFiles) {
+      const swagger = await getSwagger(swaggerPath, dirname(readmePath), repoRoot, logger);
       swaggers.push(swagger);
     }
 
@@ -107,7 +147,7 @@ async function getReadme(path) {
   }
 
   const readme = {
-    path,
+    path: readmePath,
     content,
     globalConfig,
     tags,
@@ -117,36 +157,164 @@ async function getReadme(path) {
 }
 
 /**
- * @param {string} path
+ * @param {string} swaggerPath
+ * @param {string} readmeFolder
+ * @param {string} repoRoot
+ * @param {import('./logger.js').ILogger} [logger]
  * @returns {Promise<Swagger>}
  */
-async function getSwagger(path) {
-  // TODO: Do not assume location is with respect to repo root, could be reading
-  // files from a different root location (e.g. "before" state of repo in
-  // another folder).
+async function getSwagger(swaggerPath, readmeFolder, repoRoot, logger) {
+  logger?.debug(`getSwagger: ${swaggerPath}, ${readmeFolder}, ${repoRoot}`);
 
-  const fullPath = join(__dirname, "..", "..", "..", path);
+  let normalizedSwaggerPath = swaggerPath;
+  // Ignore uses of "$(this-folder)" in the swagger path. It refers to the 
+  // current folder anyway and can be substituted with "."
+  if (swaggerPath.includes("$(this-folder)")) {
+    normalizedSwaggerPath = swaggerPath.replaceAll("$(this-folder)", ".");
+  }
 
+
+  // Some swagger paths contain backslashes. These should be normalized when 
+  // encountered though the expected format for input-files is forward slashes.
+  if (normalizedSwaggerPath.includes("\\")) {
+    logger?.info(`Found backslash (\\) in swagger path ${swaggerPath}. Replacing with forward slash (/)`);
+    normalizedSwaggerPath = normalizedSwaggerPath.replaceAll("\\", "/");
+  }
+  const fullPath = resolve(repoRoot, readmeFolder, normalizedSwaggerPath);
   const content = await readFile(fullPath, { encoding: "utf8" });
 
   return {
-    path,
+    path: join(readmeFolder, normalizedSwaggerPath),
     content,
-    refs: await getExternalFileRefs(fullPath),
+    refs: await getExternalFileRefs(normalizedSwaggerPath, readmeFolder, repoRoot),
   };
 }
 
 /**
- * @param {string} path
- * @returns {Promise<Set<string>>}
+ * Reads all levels of external file references in a swagger file and returns a 
+ * map where the key is the referenced swagger file and the value is a set of 
+ * all references that the file depends on, including transitive dependencies.
+ * @param {string} swaggerPath,
+ * @param {string} readmePath
+ * @param {string} repoRoot
+ * @returns {Promise<Map<string, Set<string>>>}
  */
-async function getExternalFileRefs(path) {
-  const schema = await $RefParser.resolve(path);
+// TODO: clean up relative path resolution (does resolve need to be called so many times?)
+async function getExternalFileRefs(swaggerPath, readmePath, repoRoot) {
+  const initialSwagger = resolve(repoRoot, readmePath, swaggerPath);
+  const visited = new Set();
+  const toVisit = new Set([initialSwagger]);
+  const externalFileRefs = new Map();
 
-  const refs = schema
-    .paths()
-    .filter((p) => p !== path && !p.startsWith("#"))
-    .map((p) => resolve(path, p));
+  while (toVisit.size > 0) {
+    const currentSwagger = /** @type {string} */ (toVisit.values().next().value);
 
-  return new Set(refs);
+    toVisit.delete(currentSwagger);
+    visited.add(currentSwagger);
+
+    const currentSchema = await $RefParser.resolve(
+      resolve(repoRoot, currentSwagger),
+      { resolve: { http: false } },
+    );
+    const currentRefs = currentSchema
+      .paths("file")
+      .filter((p) => !visited.has(p) && !example(p))
+      .map(p => relative(repoRoot, p));
+
+    // Don't add the initial path to the dependency map
+    if (currentSwagger !== initialSwagger) {
+      externalFileRefs.set(currentSwagger, new Set());
+    }
+
+    for (const ref of currentRefs) {
+      // Add the ref to the dependency map for the current path
+      // TODO: Simplify
+      if (currentSwagger !== initialSwagger) { 
+        externalFileRefs.get(currentSwagger).add(ref);
+      }
+      
+      if (!visited.has(ref)) {
+        toVisit.add(ref);
+      }
+    }
+  }
+
+  return externalFileRefs;
+}
+
+/**
+ * Given a swagger file, return the readme files and tags that reference that 
+ * swagger file.
+ * @param {string} swaggerFile 
+ * @param {SpecModel} specModel 
+ * @returns {Map<string, Set<string>>}
+ */
+export function getAffectedReadmeTags(swaggerFile, specModel) { 
+  const affectedReadmes = new Map();
+
+  for (const [readmePath, readme] of specModel.readmes) {
+    for (const [tag, swaggers] of readme.tags) {
+      for (const swagger of swaggers) {
+        if (swagger.path === swaggerFile) {
+          if (!affectedReadmes.has(readmePath)) {
+            affectedReadmes.set(readmePath, new Set());
+          }
+          affectedReadmes.get(readmePath).add(tag);
+          // No need to check refs if the swagger file is directly referenced
+          continue;
+        }
+
+        // Because refs contains the full set of transitive dependencies, only 
+        // check if the swagger file is in the map keys.
+        if (swagger.refs.has(swaggerFile)) {
+          if (!affectedReadmes.has(readmePath)) {
+            affectedReadmes.set(readmePath, new Set());
+          }
+          affectedReadmes.get(readmePath).add(tag);
+        }
+      }
+    }
+  }
+  return affectedReadmes;
+}
+
+/**
+ * Given a swagger file, return the swagger files that are affected by the
+ * changes in the given swagger file.
+ * @param {string} swaggerFile 
+ * @param {SpecModel} specModel 
+ * @returns {Set<string>}
+ */
+export function getAffectedSwaggers(swaggerFile, specModel) {
+  const affectedSwaggers = new Set();
+  for (const [, readme] of specModel.readmes) {
+    for (const [, swaggers] of readme.tags) {
+      for (const swagger of swaggers) {
+
+        if (swagger.path === swaggerFile) {
+          affectedSwaggers.add(swagger.path);
+        }
+        
+        if (swagger.refs.has(swaggerFile)) { 
+          affectedSwaggers.add(swagger.path);
+        }
+
+        // Check the swagger file's refs and add any which depend on the 
+        // swagger file. These potentially transitive dependencies may not be 
+        // referenced directly in a readme file.
+        for (const [ref, dependsOn] of swagger.refs) { 
+          if (dependsOn.has(swaggerFile)) { 
+            affectedSwaggers.add(ref);
+          }
+        }
+      }
+    }
+  }
+
+  // The swagger file supplied does not exist in the given specModel
+  if (affectedSwaggers.size === 0) {
+    throw new Error(`No affected swaggers found in specModel for ${swaggerFile}`);
+  }
+
+  return affectedSwaggers;
 }
--- .github/shared/test/spec-model.test.js
+++ .github/shared/test/spec-model.test.js
@@ -1,25 +1,36 @@
 import { readFileSync } from "fs";
-import { dirname, join } from "path";
+import { dirname, join, resolve } from "path";
 import { fileURLToPath } from "url";
-import { describe, it } from "vitest";
-import { getSpecModel } from "../src/spec-model.js";
+import { describe, it, expect } from "vitest";
+import { getAffectedReadmeTags, getSpecModel, getAffectedSwaggers } from "../src/spec-model.js";
+import { readdir } from "fs/promises";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const repoRoot = join(__dirname, "..", "..", "..");
 
-describe("spec-model", () => {
-  it("getSpecModel", async ({ expect }) => {
+describe("getSpecModel", () => {
+  it("returns spec model given relative path", async ({ expect }) => {
+    const servicePath = "specification/contosowidgetmanager";
+
+    const actual = await getSpecModel(servicePath);
+    expect(actual).toBeDefined();
+  });
+
+  it("returns spec model", async ({ expect }) => {
+
+    const fixtureRoot = resolve(__dirname, "fixtures/getSpecModel")
+
     const readmePath =
       "specification/contosowidgetmanager/resource-manager/readme.md";
-    const readmeContent = readFileSync(join(repoRoot, readmePath), {
+    const readmeContent = readFileSync(join(fixtureRoot, readmePath), {
       encoding: "utf8",
     });
 
     const swaggerPathPreview =
       "specification/contosowidgetmanager/resource-manager/Microsoft.Contoso/preview/2021-10-01-preview/contoso.json";
     const swaggerContentPreview = readFileSync(
-      join(repoRoot, swaggerPathPreview),
+      join(fixtureRoot, swaggerPathPreview),
       {
         encoding: "utf8",
       },
@@ -28,13 +39,14 @@ describe("spec-model", () => {
     const swaggerPathStable =
       "specification/contosowidgetmanager/resource-manager/Microsoft.Contoso/stable/2021-11-01/contoso.json";
     const swaggerContentStable = readFileSync(
-      join(repoRoot, swaggerPathStable),
+      join(fixtureRoot, swaggerPathStable),
       {
         encoding: "utf8",
       },
     );
 
     const expected = {
+      repoRoot: fixtureRoot,
       readmes: new Map([
         [
           readmePath,
@@ -55,7 +67,7 @@ describe("spec-model", () => {
                     content: swaggerContentStable,
                     // TODO: Absolute paths won't test properly across machines. 
                     // Make paths relative or remove comment
-                    refs: expect.any(Set),
+                    refs: expect.any(Map),
                   },
                 ],
               ],
@@ -67,7 +79,7 @@ describe("spec-model", () => {
                     content: swaggerContentPreview,
                     // TODO: Absolute paths won't test properly across machines. 
                     // Make paths relative or remove comment
-                    refs: expect.any(Set),
+                    refs: expect.any(Map),
                   },
                 ],
               ],
@@ -77,8 +89,239 @@ describe("spec-model", () => {
       ]),
     };
 
-    const specModel = await getSpecModel("specification/contosowidgetmanager/resource-manager");
+    const specModel = await getSpecModel(
+      "specification/contosowidgetmanager/resource-manager", 
+      { repoRoot: fixtureRoot }
+    );
 
     expect(specModel).toEqual(expected);
+  });
+
+  it("throws when a tag is defined more than once", async ({ expect }) => {
+    const fixtureRoot = resolve(__dirname, "fixtures/getSpecModel");
+
+    // data-plane/readme.md defines the `package-2022-12-01` tag twice.
+    const specRoot = "specification/contosowidgetmanager/data-plane";
+
+    await expect(async () => await getSpecModel(specRoot, { repoRoot: fixtureRoot }))
+      .rejects
+      .toThrowError();
+  });
+
+});
+
+describe("getReadme regex", () => {
+  it("uses a regex that finds tags in readme.md", () => {
+    const regex = /yaml.*\$\(tag\) ?== ?'([^']*)'/;
+    const yamlContent = readFileSync(join(repoRoot, 'yamls.txt'), { encoding: 'utf8' });
+    const lines = yamlContent.split(/\r?\n/);
+
+    for (const line of lines) {
+      console.log(line);
+      expect(regex.test(line)).toEqual(true);
+    }
+  });
+
+  it.each([
+    ["```yaml $(package-A-tag) == 'package-A-[[Version]]'", false],
+    ["``` yaml $(tag)=='package-2017-03' && $(go)", true],
+    ["``` yaml $(csharp) && $(tag) == 'release_4_0'", true],
+    ["``` yaml $(tag) == 'package-2021-12-01-preview'", true],
+    ["``` yaml $(tag) == 'package-2025-01-01-preview'", true],
+  ])("ignores tags that don't match the regex: %s", (example, expected) => {
+    const regex = /yaml.*\$\(tag\) ?== ?'([^']*)'/;
+    expect(regex.test(example)).toEqual(expected);
+  });
+});
+
+describe("getAffectedReadmeTags", () => {
+  it("returns affected readme tags", async ({ expect }) => {
+    const specModel = await getSpecModel(
+      "specification/contosowidgetmanager",
+      // { repoRoot: resolve(__dirname, "fixtures/getAffectedReadmeTags") }
+    );
+
+    const actual = getAffectedReadmeTags(
+      "specification/contosowidgetmanager/resource-manager/Microsoft.Contoso/stable/2021-11-01/contoso.json",
+      specModel
+    ); 
+
+    const expected = new Map([[
+      "specification/contosowidgetmanager/resource-manager/readme.md", 
+      new Set (["package-2021-11-01"]),
+      ]])
+    expect(actual).toEqual(expected);
+  });
+
+  it("returns affected readme tags for multiple tags", async ({ expect }) => {
+    const specModel = await getSpecModel("specification/1", { repoRoot: resolve(__dirname, "fixtures/getAffectedSwaggers") });
+    const actual = getAffectedReadmeTags( 
+      "specification/1/data-plane/shared/shared.json",
+      specModel
+    );
+
+    const expected = new Map([[
+      "specification/1/data-plane/readme.md",
+      new Set(["tag-1", "tag-2"]),
+    ]]);
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("getAffectedSwaggers", () => {
+  // TODO: Test with swaggers that have layers of dependencies
+  it("returns affected swaggers", async ({ expect }) => {
+    const specModel = await getSpecModel(
+      "specification/contosowidgetmanager",
+    );
+
+    const actual = getAffectedSwaggers(
+      "specification/contosowidgetmanager/resource-manager/Microsoft.Contoso/stable/2021-11-01/contoso.json",
+      specModel
+    );
+
+    const expected = new Set([
+      "specification/contosowidgetmanager/resource-manager/Microsoft.Contoso/stable/2021-11-01/contoso.json",
+    ]);
+    expect(actual).toEqual(expected);
+  });
+
+  it("returns directly referenced swagger", async ({ expect }) => {
+    const specModel = await getSpecModel("specification/1", { repoRoot: resolve(__dirname, "fixtures/getAffectedSwaggers") });
+
+    const actual = getAffectedSwaggers( 
+      "specification/1/data-plane/a.json",
+      specModel
+    );
+
+    const expected = new Set([
+      "specification/1/data-plane/a.json",
+    ]);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it("throws when swagger file is not found", async ({ expect }) => {
+    const specModel = await getSpecModel("specification/1", { repoRoot: resolve(__dirname, "fixtures/getAffectedSwaggers") });
+    const swaggerFile = "specification/1/data-plane/not-found.json";
+    const actual = () => getAffectedSwaggers( 
+      swaggerFile,
+      specModel
+    );
+    expect(actual).toThrowError(expect.objectContaining({
+      message: expect.stringContaining(swaggerFile),
+    }));
+  });
+
+  it("returns correct swaggers for one layer of dependencies", async ({ expect }) => {
+    const specModel = await getSpecModel("specification/1", { repoRoot: resolve(__dirname, "fixtures/getAffectedSwaggers") });
+
+    const actual = getAffectedSwaggers( 
+      "specification/1/data-plane/nesting/b.json",
+      specModel
+    );
+
+    const expected = new Set([
+      "specification/1/data-plane/a.json",
+      "specification/1/data-plane/nesting/b.json",
+    ]);
+    expect(actual).toEqual(expected);
+  });
+
+  it("returns correct swaggers for two layers of dependencies", async ({ expect }) => {
+    const specModel = await getSpecModel("specification/1", { repoRoot: resolve(__dirname, "fixtures/getAffectedSwaggers") });
+
+    const actual = getAffectedSwaggers( 
+      "specification/1/data-plane/c.json",
+      specModel
+    );
+    const expected = new Set([
+      "specification/1/data-plane/a.json",
+      "specification/1/data-plane/nesting/b.json",
+      "specification/1/data-plane/c.json",
+    ]);
+    expect(actual).toEqual(expected);
+  });
+
+  it("returns correct swaggers for three layers of dependencies", async ({ expect }) => {
+    const specModel = await getSpecModel("specification/1", { repoRoot: resolve(__dirname, "fixtures/getAffectedSwaggers") });
+
+    const actual = getAffectedSwaggers( 
+      "specification/1/data-plane/d.json",
+      specModel
+    );
+    const expected = new Set([
+      "specification/1/data-plane/a.json",
+      "specification/1/data-plane/nesting/b.json",
+      "specification/1/data-plane/c.json",
+      "specification/1/data-plane/d.json",
+    ]);
+    expect(actual).toEqual(expected);
+  });
+
+  it("returns correctly for multiple shared dependencies", async( { expect }) => {
+    const specModel = await getSpecModel("specification/1", { repoRoot: resolve(__dirname, "fixtures/getAffectedSwaggers") });
+
+    const actual = getAffectedSwaggers( 
+      "specification/1/data-plane/shared/shared.json",
+      specModel
+    );
+    const expected = new Set([
+      "specification/1/data-plane/a.json",
+      "specification/1/data-plane/nesting/b.json",
+      "specification/1/data-plane/c.json",
+      "specification/1/data-plane/d.json",
+      "specification/1/data-plane/shared/shared.json",
+      "specification/1/data-plane/e.json",
+    ]);
+    expect(actual).toEqual(expected);
+  });
+});
+
+// Stress test the parser against all specs in the specification/ folder. This 
+// is a long-running test and should be run manually. To run this test, remove
+// the .skip() from the describe block. 
+describe.skip("Parse readmes", () => {
+  it("Does not produce exceptions", { timeout: 30 * 60 * 1000 /* 30 minutes */ }, async ({ expect }) => {
+    const excludeFolders = [
+      "authorization",        // specification/authorization/resource-manager/readme.md defines has duplicate tags including 'package-2020-10-01'
+      "azureactivedirectory", // specification/azureactivedirectory/resource-manager/readme.md has duplicate tags including 'package-preview-2020-07'
+      "cost-management",      // specification/cost-management/resource-manager/readme.md has duplicate tags including 'package-2019-01'
+      "migrate",              // specification/migrate/resource-manager/readme.md has duplicate tags including 'package-migrate-2023-04'
+      "quota",                // specification/quota/resource-manager/readme.md has duplicate tags including 'package-2023-02-01'
+      "redisenterprise",      // specification/redisenterprise/resource-manager/readme.md has duplicate tags including 'package-2024-02'
+      "security",             // specification/security/resource-manager/readme.md has duplicate tags including 'package-2021-07-preview-only'
+      "confidentialledger",   // data-plane/readme.md tag 'package-2022-04-20-preview-ledger' points to a swagger file that doesn't exist
+      "network",              // network takes a long time to evaluate
+      "servicenetworking"     // servicenetworking includes a swagger file which references a file that doesn't exist
+    ];
+    
+    // List all folders under specification/
+    const folders = await readdir(join(repoRoot, "specification"), { withFileTypes: true });
+    const services = folders.filter(f => f.isDirectory() && !excludeFolders.includes(f.name)).map(f => f.name);
+    for (const folder of services) {
+      if (folder < "security") {
+        // Skip folders up to some point
+        console.log(`Skipping service: ${folder}`);
+        continue;
+      }
+
+      console.log(`Testing service: ${folder}`);
+      const specModel = await getSpecModel(`specification/${folder}`, { debug: false });
+
+      expect(specModel).toBeDefined();
+    }
+  });
+
+  it("runs properly against specific services", { timeout: 30 * 60 * 1000 /* 30 minutes */ }, async ({ expect }) => {
+    const folders = [
+      // Fill in services to test here
+    ]; 
+    for (const folder of folders) {
+      console.log(`Testing service: ${folder}`);
+      const specModel = await getSpecModel(`specification/${folder}`, { debug: true });
+
+      expect(specModel).toBeDefined();
+    }
   });
 });
