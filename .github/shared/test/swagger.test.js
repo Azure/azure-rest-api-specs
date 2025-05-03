5e324c0d02ca9dd47132c196fc65eea392b4a3cf
Mike Harder <mharder@microsoft.com>
2025-05-03 01:29:45 -0700

    [spec-model] Move specModel to options (#34433)
    - Only needed for relative paths in toJsonAsync()

--- .github/shared/test/swagger.test.js
+++ .github/shared/test/swagger.test.js
@@ -2,12 +2,11 @@
 
 import { resolve } from "path";
 import { describe, expect, it } from "vitest";
-import { SpecModel } from "../src/spec-model.js";
 import { Swagger } from "../src/swagger.js";
 
 describe("Swagger", () => {
   it("can be created with mock path", async () => {
-    const swagger = new Swagger(new SpecModel("foo"), "bar");
+    const swagger = new Swagger("bar");
     expect(swagger.path).toBe(resolve("bar"));
 
     await expect(swagger.getRefs()).rejects.toThrowError(
