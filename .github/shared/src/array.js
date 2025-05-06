2306b3e321e8a30362c4caff17718283b21e501d
Mike Harder <mharder@microsoft.com>
2025-05-06 07:39:32 +0000

    Add test for flatMapAsync

--- .github/shared/src/array.js
+++ .github/shared/src/array.js
@@ -14,20 +14,20 @@ export async function filterAsync(array, asyncPredicate) {
 /**
  * @template T,U
  * @param {T[]} array
- * @param {(item: T, index: number, array: T[]) => Promise<U>} asyncMapper
- * @returns {Promise<U[]>}
- */
-export async function mapAsync(array, asyncMapper) {
-  return Promise.all(array.map(asyncMapper));
-}
-
-/**
- * @template T,U
- * @param {T[]} array
  * @param {(item: T, index: number, array: T[]) => Promise<U[]>} asyncMapper
  * @returns {Promise<U[]>}
  */
 export async function flatMapAsync(array, asyncMapper) {
   const mapped = await mapAsync(array, asyncMapper);
   return mapped.flat();
+}
+
+/**
+ * @template T,U
+ * @param {T[]} array
+ * @param {(item: T, index: number, array: T[]) => Promise<U>} asyncMapper
+ * @returns {Promise<U[]>}
+ */
+export async function mapAsync(array, asyncMapper) {
+  return Promise.all(array.map(asyncMapper));
 }
