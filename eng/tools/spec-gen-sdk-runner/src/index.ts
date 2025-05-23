b7431b94bc16c98bdd02634a4b1711afbfda2877
ray chen <raychen@microsoft.com>
2025-05-23 18:33:10 +0000

    skip sdk gen in specific batch run

--- eng/tools/spec-gen-sdk-runner/src/index.ts
+++ eng/tools/spec-gen-sdk-runner/src/index.ts
@@ -1,4 +1,6 @@
 import { exit } from "node:process";
+import path from "node:path";
+import { existsSync, mkdirSync } from "node:fs";
 import { getArgumentValue } from "./utils.js";
 import {
   generateSdkForBatchSpecs,
@@ -11,8 +13,17 @@ export async function main() {
   const args: string[] = process.argv.slice(2);
   // Log the arguments to the console
   console.log("Arguments passed to the script:", args.join(" "));
+  console.log("Current working directory:", process.cwd());
   const batchType: string = getArgumentValue(args, "--batch-type", "");
   const pullRequestNumber: string = getArgumentValue(args, "--pr-number", "");
+  const workingFolder: string = getArgumentValue(args, "--wf", path.join(process.cwd(), ".."));
+  const logFolder = path.join(workingFolder, "out/logs");
+  if (!existsSync(logFolder)) {
+    mkdirSync(logFolder, { recursive: true });
+  }
+  if (existsSync(logFolder)) {
+    console.log(`Log folder exists: ${logFolder}`);
+  }
   let statusCode = 0;
   if (batchType) {
     statusCode = await generateSdkForBatchSpecs(batchType);
