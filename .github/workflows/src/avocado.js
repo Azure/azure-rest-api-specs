a19800726a71e39fe3fe323c7f9c2c2b780fbeec
Mike Harder <mharder@microsoft.com>
2025-07-04 00:04:26 +0000

    [Avocado] Render results as markdown to job summary

---
+++ .github/workflows/src/avocado.js
@@ -0,0 +1,22 @@
+// @ts-check
+
+import { readFile } from "fs/promises";
+
+// TODO: Add tests
+/* v8 ignore start */
+/**
+ * @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments
+ * @returns {Promise<void>}
+ */
+export default async function generateJobSummary({ core }) {
+  const avocadoOutputPath = process.env.AVOCADO_OUTPUT_FILE;
+
+  if (!avocadoOutputPath) {
+    throw new Error("Env var AVOCADO_OUTPUT_FILE must be set");
+  }
+
+  const avocadoOutput = await readFile(avocadoOutputPath, { encoding: "utf-8" });
+
+  core.summary.addCodeBlock(avocadoOutput);
+  core.summary.write();
+}
