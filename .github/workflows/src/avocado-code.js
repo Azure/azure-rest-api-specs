42cc46ddbff18d08ca924c58aa271b5b6f64c5f7
Mike Harder <mharder@microsoft.com>
2025-07-08 07:32:02 +0000

    simplify code

--- .github/workflows/src/avocado-code.js
+++ .github/workflows/src/avocado-code.js
@@ -7,30 +7,28 @@ import { readFile } from "fs/promises";
  * @returns {Promise<void>}
  */
 export default async function generateJobSummary({ core }) {
-  let content = "";
+  const avocadoLog = process.env.AVOCADO_LOG;
+  core.info(`avocadoLog: ${avocadoLog}`);
 
   const avocadoOutput = process.env.AVOCADO_OUTPUT;
   core.info(`avocadoOutput: ${avocadoOutput}`);
+
   if (avocadoOutput) {
-    content = await readFileIfExists(avocadoOutput, core);
+    const content = await readFileIfExists(avocadoOutput, core);
+
+    // TODO: Replace with table similar to old check
+    core.summary.addCodeBlock(content);
+  } else if (avocadoLog) {
+    const content = (await readFileIfExists(avocadoLog, core))
+      // Remove lines starting with "##vso"
+      .replace(/^##vso.*(?:\r?\n)?/gm, "");
+    core.summary.addCodeBlock(content);
+  } else {
+    const content = `Unable to read file '${avocadoOutput}' or '${avocadoLog}'`;
+    core.summary.addCodeBlock(content);
   }
 
-  const avocadoLog = process.env.AVOCADO_LOG;
-  core.info(`avocadoLog: ${avocadoLog}`);
-  if (!content && avocadoLog) {
-    content = await readFileIfExists(avocadoLog, core);
-
-    // Remove lines starting with "##vso"
-    content = content.replace(/^##vso.*(?:\r?\n)?/gm, "");
-  }
-
-  if (!content) {
-    content = `Unable to read file '${avocadoOutput}' or '${avocadoLog}'`;
-  }
-
-  core.summary.addCodeBlock(content);
   core.summary.write();
-
   core.setOutput("summary", process.env.GITHUB_STEP_SUMMARY);
 }
 
