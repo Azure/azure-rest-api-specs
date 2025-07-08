ca09565bf6575c136630713e47e58d670c5fe0ed
Mike Harder <mharder@microsoft.com>
2025-07-08 21:39:19 +0000

    Simplify to single output file

--- .github/workflows/src/avocado-code.js
+++ .github/workflows/src/avocado-code.js
@@ -7,47 +7,27 @@ import { readFile } from "fs/promises";
  * @returns {Promise<void>}
  */
 export default async function generateJobSummary({ core }) {
-  const avocadoLog = process.env.AVOCADO_LOG;
-  core.info(`avocadoLog: ${avocadoLog}`);
+  const avocadoOutputFile = process.env.AVOCADO_OUTPUT_FILE;
+  core.info(`avocadoOutputFile: ${avocadoOutputFile}`);
 
-  const avocadoOutput = process.env.AVOCADO_OUTPUT;
-  core.info(`avocadoOutput: ${avocadoOutput}`);
-
-  if (avocadoOutput) {
-    const content = await readFileIfExists(avocadoOutput, core);
-
-    // TODO: Replace with table similar to old check
-    core.summary.addCodeBlock(content);
-  } else if (avocadoLog) {
-    const content = (await readFileIfExists(avocadoLog, core))
-      // Remove lines starting with "##vso"
-      .replace(/^##vso.*(?:\r?\n)?/gm, "");
-    core.summary.addCodeBlock(content);
-  } else {
-    const content = `Unable to read file '${avocadoOutput}' or '${avocadoLog}'`;
-    core.summary.addCodeBlock(content);
+  if (!avocadoOutputFile) {
+    throw new Error("Env var AVOCADO_OUTPUT_FILE must be set");
   }
+
+  /** @type {string} */
+  let content;
+
+  try {
+    content = await readFile(avocadoOutputFile, { encoding: "utf-8" });
+  } catch {
+    // If we can't read the file, the previous step must have failed catastrophically, so don't
+    // even try to generate a summary.
+    return;
+  }
+
+  // TODO: Replace with table similar to old check
+  core.summary.addCodeBlock(content);
 
   core.summary.write();
   core.setOutput("summary", process.env.GITHUB_STEP_SUMMARY);
-}
-
-/**
- * @param {string} file
- * @param {typeof import("@actions/core")} core
- */
-async function readFileIfExists(file, core) {
-  let content = "";
-
-  try {
-    content = await readFile(file, { encoding: "utf-8" });
-  } catch (error) {
-    if (error instanceof Error && "code" in error && error.code === "ENOENT") {
-      core.info(`File '${file}' does not exist`);
-    } else {
-      throw error;
-    }
-  }
-
-  return content;
 }
