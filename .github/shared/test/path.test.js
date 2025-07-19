33c40ecd7b643fda6e79cd9bf1ede9abc0b34e82
Mike Harder <mharder@microsoft.com>
2025-07-19 04:51:55 +0000

    tests for new path utils

--- .github/shared/test/path.test.js
+++ .github/shared/test/path.test.js
@@ -1,27 +1,38 @@
 // @ts-check
 
-import { strict as assert } from "assert";
-import { describe, it } from "vitest";
-import { includesFolder } from "../src/path.js";
+import { tmpdir } from "os";
+import { resolve } from "path";
+import { describe, expect, it } from "vitest";
+import { includesFolder, relativeCwd, resolveCwd } from "../src/path.js";
 
 describe("includesFolder", () => {
   it("should return true when path contains the specified folder", () => {
-    assert.equal(includesFolder("/path/to/examples/file.json", "examples"), true);
+    expect(includesFolder("/path/to/examples/file.json", "examples")).toEqual(true);
   });
 
   it("should return false when path does not contain the specified folder", () => {
-    assert.equal(includesFolder("/path/to/swagger/file.json", "examples"), false);
+    expect(includesFolder("/path/to/swagger/file.json", "examples")).toEqual(false);
   });
 });
 
 describe("relativeCwd", () => {
-  it("TODO", () => {
-    assert.fail();
+  it("relative to current dir by default", () => {
+    const abs = resolve("foo.json");
+    expect(relativeCwd(abs)).toEqual("foo.json");
+  });
+
+  it("relative to options.cwd if set", () => {
+    const abs = resolve(tmpdir(), "foo.json");
+    expect(relativeCwd(abs, { cwd: tmpdir() })).toEqual("foo.json");
   });
 });
 
 describe("resolveCwd", () => {
-  it("TODO", () => {
-    assert.fail();
+  it("resolves against current dir by default", () => {
+    expect(resolveCwd("foo.json")).toEqual(resolve("foo.json"));
+  });
+
+  it("resolves against options.cwd if set", () => {
+    expect(resolveCwd("foo.json", { cwd: tmpdir() })).toEqual(resolve(tmpdir(), "foo.json"));
   });
 });
