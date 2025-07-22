7a6185276659d3a56ee3ab8aed8b0c6d71f89542
Richard Park <51494936+richardpark-msft@users.noreply.github.com>
2025-07-22 00:20:42 +0000

    init.sh is custom code that we can make better, but the rest is pretty standard codespaces.

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -2,6 +2,21 @@
 
 set -ex
 
+# client generators
 npm install -g @typespec/compiler
 npm install -g @azure-tools/typespec-client-generator-cli
 
+# go setup
+go install golang.org/x/tools/cmd/goimports@latest
+
+echo ". /usr/local/share/nvm/nvm.sh" >> ~/.bashrc
+
+pushd /workspaces
+
+langs=(net js java python go)
+for lang in "${langs[@]}"; do
+  git clone --depth 1 https://github.com/Azure/azure-sdk-for-$lang &
+done
+wait
+
+popd
