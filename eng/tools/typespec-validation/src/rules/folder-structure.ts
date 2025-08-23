556e581d5d5d027b3e85768d71a10966f7e41444
catalinaperalta <caperal@microsoft.com>
2025-08-22 17:06:26 -0700

    folder name msg

--- eng/tools/typespec-validation/src/rules/folder-structure.ts
+++ eng/tools/typespec-validation/src/rules/folder-structure.ts
@@ -150,7 +150,7 @@ export class FolderStructureRule implements Rule {
 
       if (!serviceRegex.test(serviceFolder)) {
         success = false;
-        errorOutput += `Service folder '${serviceFolder}' does not match regex ${serviceRegex}`;
+        errorOutput += `Service folder '${serviceFolder}' does not match regex ${serviceRegex}. Data-plane folders must use PascalCase without any special characters, including periods, hyphens, or underscores, among others.`;
       }
     }
 
