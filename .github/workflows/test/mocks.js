6f5305343bf153f20be502f935703e204f6068c4
Mike Harder <mharder@microsoft.com>
2025-08-26 19:43:48 +0000

    [EngSys] Add tests for octokit log hooks

--- .github/workflows/test/mocks.js
+++ .github/workflows/test/mocks.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 import { RequestError } from "@octokit/request-error";
 import { vi } from "vitest";
 
@@ -73,7 +75,7 @@ export function createMockCore() {
 export function createMockRequestError(status) {
   return new RequestError(`mock RequestError with status '${status}'`, status, {
     // request properties "url" and "headers" must be defined to prevent errors
-    request: { url: "test url", headers: {} },
+    request: { method: "GET", url: "test url", headers: {} },
   });
 }
 
@@ -85,5 +87,17 @@ export function createMockContext() {
       owner: "owner",
       repo: "repo",
     },
+  };
+}
+
+/**
+ * @returns {import("../../shared/src/logger").ILogger}
+ */
+export function createMockLogger() {
+  return {
+    debug: vi.fn(),
+    error: vi.fn(),
+    info: vi.fn(),
+    isDebug: vi.fn().mockReturnValue(false),
   };
 }
