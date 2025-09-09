cf2292509c13cd7c445068111685567d6bab213f
Sidharth Khanna (Quadrant Technologies LLC) <v-sikhanna@microsoft.com>
2025-09-09 15:00:00 -0700

    Added DataQuality API spec

--- .github/eslint.config.js
+++
@@ -1,8 +0,0 @@
-import eslint from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-
-/** @type {import('eslint').Linter.Config[]} */
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, {
-  languageOptions: { globals: globals.node },
-});
--- .github/shared/test/repo.js
+++
@@ -1,8 +0,0 @@
-// @ts-check
-
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-export const repoRoot = join(__dirname, "..", "..", "..");
