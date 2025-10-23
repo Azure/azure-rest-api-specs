4e514ba9ebcabc07d9f4d258850e272d0f8931b5
Mike Harder <mharder@microsoft.com>
2025-10-23 00:16:54 +0000

    [path.js] Add helper method untilFolder()

--- .github/shared/src/path.js
+++ .github/shared/src/path.js
@@ -11,3 +11,22 @@ import { resolve, sep } from "path";
 export function includesFolder(path, folder) {
   return resolve(path).split(sep).includes(folder);
 }
+
+/**
+ * @param {string} path
+ * @param {string} folder
+ * @returns {string} Portion of the resolved path up to (but not including) the first occurrence of the named folder
+ */
+export function untilFolder(path, folder) {
+  // Example: /a/b/c.txt -> ["", "a", "b", "c"] (note leading empty string)
+  const segments = resolve(path).split(sep);
+
+  const index = segments.indexOf(folder);
+
+  if (index === -1) {
+    // If path doesn't contain the folder, return an empty string
+    return "";
+  } else {
+    return segments.slice(0, index).join(sep);
+  }
+}
