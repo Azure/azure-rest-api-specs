eb965c9cdeeaa19b7a5db7402a638d56a6bc6a82
Mike Harder <mharder@microsoft.com>
2025-10-24 02:33:59 +0000

    [path] Add includesSegment() and untilSegment()

--- .github/shared/src/path.js
+++ .github/shared/src/path.js
@@ -1,13 +1,37 @@
 // @ts-check
 
-import { resolve, sep } from "path";
+import { basename, dirname, resolve } from "path";
 
 /**
  *
  * @param {string} path
- * @param {string} folder
- * @returns {boolean} True if path contains the named folder
+ * @param {string} segment
+ * @returns {boolean} True if path contains the named segment (file or folder)
  */
-export function includesFolder(path, folder) {
-  return resolve(path).split(sep).includes(folder);
+export function includesSegment(path, segment) {
+  return untilSegment(path, segment) !== "";
+}
+
+/**
+ * @param {string} path
+ * @param {string} segment
+ * @returns {string} Portion of the resolved path up to (but not including) the first occurrence of the named segment (file or folder)
+ */
+export function untilSegment(path, segment) {
+  let current = resolve(path);
+
+  while (true) {
+    const parent = dirname(current);
+
+    if (basename(current) === segment) {
+      // Found the target folder.  Return everything before it.
+      return parent;
+    } else if (parent === current) {
+      // Reached the filesystem root (folder not found).  Return empty string.
+      return "";
+    } else {
+      // Keep walking upward
+      current = parent;
+    }
+  }
 }
