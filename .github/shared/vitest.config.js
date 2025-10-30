726e73a0cda925d7ef090e52de7dde750687f8bd
Mike Harder <mharder@microsoft.com>
2025-10-30 12:05:31 -0700

    comment

--- .github/shared/vitest.config.js
+++ .github/shared/vitest.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     // Ignore tsconfig.json, since it's only used for type checking, and causes
     // a warning if vitest tries to load it
 
-    // @ts-expect-error - 'tsConfig' does not exist in type 'ESBuildOptions'
+    // @ts-expect-error: 'tsConfig' does not exist in type 'ESBuildOptions'
     tsConfig: false,
   },
 
