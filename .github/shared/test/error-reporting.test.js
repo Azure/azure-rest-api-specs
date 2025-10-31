853dbf19971c029bcc19b86e8d9301322913a405
Daniel Jurek <djurek@microsoft.com>
2025-10-31 18:22:44 +0000

    Baseline for .github folder

--- .github/shared/test/error-reporting.test.js
+++ .github/shared/test/error-reporting.test.js
@@ -42,7 +42,7 @@ describe("ErrorReporting", () => {
 
     const content = await fs.readFile(process.env.GITHUB_STEP_SUMMARY, "utf-8");
 
-    // cleanup after the test so nothing is left behi
+    // cleanup after the test so nothing is left behind
     await fs.rm(process.env.GITHUB_STEP_SUMMARY, { force: true });
 
     expect(content).toBe("# Title");
