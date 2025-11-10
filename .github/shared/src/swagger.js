882193c709809abedcea2124076abbd73305dc4c
Mike Harder <mharder@microsoft.com>
2025-11-10 11:26:37 -0800

    [spec-model] Add "parameters" array to path schema (#38687)

--- .github/shared/src/swagger.js
+++ .github/shared/src/swagger.js
@@ -27,16 +27,30 @@ import { embedError } from "./spec-model.js";
  * @property {Object[]} [refs]
  */
 
-const pathSchema = z.record(z.string(), z.object({ operationId: z.string().optional() }));
+// https://swagger.io/specification/v2/#operation-object
+const operationSchema = z.object({ operationId: z.string().optional() });
+/**
+ * @typedef {import("zod").infer<typeof operationSchema>} OperationObject
+ */
+
+// TODO: Consider narrowing to only the field names in the spec ("get", "put", etc)
+// https://swagger.io/specification/v2/#path-item-object
+const pathSchema = z
+  .object({
+    parameters: z.array(z.unknown()).optional(),
+  })
+  .catchall(operationSchema);
 /**
  * @typedef {import("zod").infer<typeof pathSchema>} PathObject
  */
 
+// https://swagger.io/specification/v2/#paths-object
 const pathsSchema = z.record(z.string(), pathSchema);
 /**
  * @typedef {import("zod").infer<typeof pathsSchema>} PathsObject
  */
 
+// https://swagger.io/specification/v2/#swagger-object
 const swaggerSchema = z.object({
   paths: pathsSchema.optional(),
   "x-ms-paths": pathsSchema.optional(),
@@ -48,6 +62,7 @@ const swaggerSchema = z.object({
  * const swagger = {
  *   "paths": {
  *     "/foo": {
+ *       "parameters": [ ... ],
  *       "get": {
  *         "operationId": "Foo_Get"
  *       },
@@ -235,8 +250,10 @@ export class Swagger {
    * @returns {void}
    */
   #addOperations(operations, path, pathObject) {
-    for (const [method, operation] of Object.entries(pathObject)) {
-      if (operation.operationId !== undefined && method !== "parameters") {
+    for (const [method, operation] of Object.entries(
+      /** @type {Omit<PathObject, "parameters">} */ (pathObject),
+    )) {
+      if (method !== "parameters" && operation.operationId !== undefined) {
         const operationObj = {
           id: operation.operationId,
           httpMethod: method.toUpperCase(),
