fc08a74c8fd3b28ce3aba302d53785031ede3189
Mike Harder <mharder@microsoft.com>
2025-11-14 15:19:47 -0800

    [.github] Exclude "shared/coverage" (#38819)

--- .github/eslint.config.js
+++ .github/eslint.config.js
@@ -13,6 +13,6 @@ export default defineConfig(
     languageOptions: { globals: globals.node },
   },
   {
-    ignores: ["coverage/**"],
+    ignores: ["coverage/**", "shared/coverage/**"],
   },
 );
