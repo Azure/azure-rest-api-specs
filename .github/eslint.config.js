7137f3134773626fe2745e7467c154a226385841
Mike Harder <mharder@microsoft.com>
2025-11-18 19:30:59 +0000

    eslint.config.js

--- .github/eslint.config.js
+++ .github/eslint.config.js
@@ -6,19 +6,23 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
-export default defineConfig(eslint.configs.recommended, tseslint.configs.recommendedTypeChecked, {
-  languageOptions: {
-    // we only run in node, not browser
-    globals: globals.node,
-    // required to use tseslint.configs.recommendedTypeChecked
-    parserOptions: {
-      projectService: true,
-      // ensures the tsconfig path resolves relative to this file
-      // default is process.cwd() when running eslint, which may be incorrect
-      tsconfigRootDir: import.meta.dirname,
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      // we only run in node, not browser
+      globals: globals.node,
+      // required to use tseslint.configs.recommendedTypeChecked
+      parserOptions: {
+        projectService: true,
+        // ensures the tsconfig path resolves relative to this file
+        // default is process.cwd() when running eslint, which may be incorrect
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
   {
     ignores: ["coverage/**", "shared/coverage/**"],
   },
-});
+);
