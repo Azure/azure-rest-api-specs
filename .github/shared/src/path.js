8422d6f7a4550f77f5aadf21447b3ff45e63b3ae
Mike Harder <mharder@microsoft.com>
2025-12-02 02:43:12 +0000

    add untilLastSegmentWithParent()

--- .github/shared/src/path.js
+++ .github/shared/src/path.js
@@ -41,3 +41,31 @@ export function untilLastSegment(path, segment) {
     }
   }
 }
+
+/**
+ * @param {string} path Absolute or relative path
+ * @param {string} segment File or folder
+ * @returns {string} Portion of resolved path up to (and including) the last segment with the specified parent
+ *
+ * @example
+ * untilLastParentSegment("stable/2025-01-01/examples/foo.json", "examples")
+ * // -> "{cwd}/stable/2025-01-01"
+ */
+export function untilLastSegmentWithParent(path, segment) {
+  let current = resolve(path);
+
+  while (true) {
+    const parent = dirname(current);
+
+    if (basename(parent) === segment) {
+      // Found the target parent.  Return current;
+      return current;
+    } else if (parent === current) {
+      // Reached the filesystem root (folder not found).  Return empty string.
+      return "";
+    } else {
+      // Keep walking upward
+      current = parent;
+    }
+  }
+}
