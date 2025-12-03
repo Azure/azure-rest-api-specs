ac3a46612d6c6b27da9b1f2714f06a8bf14f59c5
Mike Harder <mharder@microsoft.com>
2025-12-03 22:04:10 +0000

    add defineBaseConfig()

--- .github/eslint.config.js
+++ .github/eslint.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from "eslint/config";
-import baseConfig from "./shared/eslint.base.config.js";
+import { defineBaseConfig } from "./shared/eslint.base.config.js";
 
-/** @type {import('eslint').Linter.Config[]} */
-export default defineConfig(baseConfig, {
-  languageOptions: {
-    parserOptions: {
-      // ensures the tsconfig path resolves relative to this file (so cannot be defined in base file)
-      // default is process.cwd() when running eslint, which may be incorrect
-      tsconfigRootDir: import.meta.dirname,
-    },
-  },
+export default defineBaseConfig({
+  // ensures the tsconfig path resolves relative to this file (so cannot be defined in base file)
+  // default is process.cwd() when running eslint, which may be incorrect
+  tsconfigRootDir: import.meta.dirname,
 });
--- .github/shared/eslint.config.js
+++ .github/shared/eslint.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from "eslint/config";
-import base from "./eslint.base.config.js";
+import { defineBaseConfig } from "./eslint.base.config.js";
 
-/** @type {import('eslint').Linter.Config[]} */
-export default defineConfig(base, {
-  languageOptions: {
-    parserOptions: {
-      // ensures the tsconfig path resolves relative to this file (so cannot be defined in base file)
-      // default is process.cwd() when running eslint, which may be incorrect
-      tsconfigRootDir: import.meta.dirname,
-    },
-  },
+export default defineBaseConfig({
+  // ensures the tsconfig path resolves relative to this file (so cannot be defined in base file)
+  // default is process.cwd() when running eslint, which may be incorrect
+  tsconfigRootDir: import.meta.dirname,
 });
