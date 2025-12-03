656cc0bda671e92ac92db83a7ec67d135237dcd2
Mike Harder <mharder@microsoft.com>
2025-12-03 01:37:32 +0000

    [EngSys] Share eslint config

--- .github/eslint.config.js
+++ .github/eslint.config.js
@@ -1,28 +1,13 @@
-// Keep in sync with .github/shared/eslint.config.js
-
-import eslint from "@eslint/js";
 import { defineConfig } from "eslint/config";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import baseConfig from "./shared/eslint.config.js";
 
 /** @type {import('eslint').Linter.Config[]} */
-export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommendedTypeChecked,
-  {
-    languageOptions: {
-      // we only run in node, not browser
-      globals: globals.node,
-      // required to use tseslint.configs.recommendedTypeChecked
-      parserOptions: {
-        projectService: true,
-        // ensures the tsconfig path resolves relative to this file
-        // default is process.cwd() when running eslint, which may be incorrect
-        tsconfigRootDir: import.meta.dirname,
-      },
+export default defineConfig(baseConfig, {
+  languageOptions: {
+    parserOptions: {
+      // ensures the tsconfig path resolves relative to this file (so cannot be defined in base file)
+      // default is process.cwd() when running eslint, which may be incorrect
+      tsconfigRootDir: import.meta.dirname,
     },
   },
-  {
-    ignores: ["coverage/**", "shared/coverage/**"],
-  },
-);
+});
