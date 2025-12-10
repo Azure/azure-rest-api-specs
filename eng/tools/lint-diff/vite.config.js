ceeb2b325dfd17dd60e0bd7ec3f8ad6271fd75a4
Timothee Guerin <tiguerin@microsoft.com>
2025-12-10 08:20:25 -0500

    Upgrade typespec next

---
+++ eng/tools/lint-diff/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+import { configDefaults } from "vitest/config";
+export default defineConfig({
+    test: Object.assign(Object.assign({}, configDefaults), { testTimeout: 20000, coverage: {
+            provider: "v8",
+            reporter: ["text", "json", "html"],
+        } }),
+});
+//# sourceMappingURL=vite.config.js.map
