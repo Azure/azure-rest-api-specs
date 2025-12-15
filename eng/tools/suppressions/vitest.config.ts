0f6ae39b23a4dcc3441be2da760f070473627c09
Vidyadhari Jami <vijami@microsoft.com>
2025-12-15 12:11:35 +0530

    Main changes

--- eng/tools/suppressions/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { configDefaults, defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    coverage: {
-      exclude: [...configDefaults.coverage.exclude!, "cmd/**", "src/index.ts"],
-    },
-  },
-});
