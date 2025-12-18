400ba0628fe8163c74cf9150e562d3e8ef1ec561
Mike Harder <mharder@microsoft.com>
2025-12-18 19:36:53 +0000

    comment

--- eng/tools/vitest.base.config.js
+++ eng/tools/vitest.base.config.js
@@ -4,6 +4,8 @@ import { defineConfig } from "vitest/config";
 
 export const baseConfig = defineConfig({
   test: {
+    // By default, vitest@4 only excludes tests from "node_modules" and ".git" folders (not "dist").
+    // Recommended fix is to *include* only the folders you want (more performant than excluding).
     dir: "./test",
   },
 });
