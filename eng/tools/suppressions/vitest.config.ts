0cb040d007bcde8aa5397042891f7733f65af113
Mike Harder <mharder@microsoft.com>
2025-12-18 19:49:59 +0000

    suppressions

--- eng/tools/suppressions/vitest.config.ts
+++ eng/tools/suppressions/vitest.config.ts
@@ -2,6 +2,7 @@ import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    dir: "./test",
     coverage: {
       exclude: [...configDefaults.coverage.exclude!, "cmd/**", "src/index.ts"],
     },
