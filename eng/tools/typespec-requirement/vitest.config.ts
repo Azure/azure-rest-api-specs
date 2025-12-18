906f7b61ac373f0a8583650cad391f78fe09b035
Mike Harder <mharder@microsoft.com>
2025-12-18 20:56:24 +0000

    typespec-requirement

--- eng/tools/typespec-requirement/vitest.config.ts
+++ eng/tools/typespec-requirement/vitest.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    dir: "./test",
     // Default timeout of 5 seconds is too low
     testTimeout: 20000,
   },
