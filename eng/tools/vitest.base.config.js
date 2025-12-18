a0203e3fc6642a5fd4c5950f018290c5a978d4e3
Mike Harder <mharder@microsoft.com>
2025-12-18 13:53:13 -0800

    [eng/tools] bump vitest from 3.2.4 to 4.0.15 (#39315)
    - reactions to breaking changes
      - adds config to include only `test` dir
        - by default, vitest@4 only excludes tests from `node_modules` and `.git` folders (not `dist`)
      - [openapi-diff-runner] mock ctors using `function` instead of `=>`

--- eng/tools/vitest.base.config.js
+++ eng/tools/vitest.base.config.js
@@ -1,11 +1,11 @@
+// @ts-check
+
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
+export const baseConfig = defineConfig({
   test: {
     // By default, vitest@4 only excludes tests from "node_modules" and ".git" folders (not "dist").
     // Recommended fix is to *include* only the folders you want (more performant than excluding).
     dir: "./test",
-    // Default timeout of 5 seconds is too low
-    testTimeout: 20000,
   },
 });
