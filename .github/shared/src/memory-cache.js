6ede3d37b6c8ee5a755f7013be5a0829fdce5664
Mike Harder <mharder@microsoft.com>
2026-01-16 19:56:59 +0000

    add MemoryCache2, a two-layer cache

--- .github/shared/src/memory-cache.js
+++ .github/shared/src/memory-cache.js
@@ -1,4 +1,6 @@
 /**
+ * Caches values in memory with a single key.
+ *
  * @template K, V
  */
 export class MemoryCache {
@@ -11,6 +13,9 @@ export class MemoryCache {
    * @param {K} key
    * @param {() => V} factory
    * @returns {V} cached value
+   *
+   * @example
+   * const text = cache.getOrCreate(path, async () => await readFile(path));
    */
   getOrCreate(key, factory) {
     let value = this.#map.get(key);
@@ -21,5 +26,36 @@ export class MemoryCache {
     }
 
     return value;
+  }
+}
+
+/**
+ * Caches values in memory with an ordered pair of keys.
+ *
+ * @template K1, K2, V
+ */
+export class MemoryCache2 {
+  // Two-layer nested cache
+  /** @type {MemoryCache<K1, MemoryCache<K2, V>>} */
+  #cache1 = new MemoryCache();
+
+  /**
+   * Returns cached value, initializing if necessary.
+   * Keys are ordered, meaning (key1, key2) != (key2, key1).
+   *
+   * @param {K1} key1
+   * @param {K2} key2
+   * @param {() => V} factory
+   * @returns {V} cached value
+   *
+   * @example
+   * const text = cache.getOrCreate(folder, file, async () => await readFile(resolve(folder, file)));
+   */
+  getOrCreate(key1, key2, factory) {
+    // key1 => cache for the next layer
+    const cache2 = this.#cache1.getOrCreate(key1, () => new MemoryCache());
+
+    // key2 => final value
+    return cache2.getOrCreate(key2, factory);
   }
 }
--- .github/shared/test/memory-cache.test.js
+++ .github/shared/test/memory-cache.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { MemoryCache } from "../src/memory-cache.js";
+import { MemoryCache, MemoryCache2 } from "../src/memory-cache.js";
 
 describe("MemoryCache", () => {
   it("createAndGetSync", () => {
@@ -13,9 +13,9 @@ describe("MemoryCache", () => {
         return "bar";
       });
 
-    expect(getOrCreate()).toEqual("bar");
-    expect(getOrCreate()).toEqual("bar");
-    expect(getOrCreate()).toEqual("bar");
+    for (let i = 0; i < 3; i++) {
+      expect(getOrCreate()).toEqual("bar");
+    }
     expect(createdCount).toEqual(1);
   });
 
@@ -31,9 +31,59 @@ describe("MemoryCache", () => {
         return "bar";
       });
 
-    await expect(getOrCreate()).resolves.toEqual("bar");
-    await expect(getOrCreate()).resolves.toEqual("bar");
-    await expect(getOrCreate()).resolves.toEqual("bar");
+    for (let i = 0; i < 3; i++) {
+      await expect(getOrCreate()).resolves.toEqual("bar");
+    }
     expect(createdCount).toEqual(1);
+  });
+});
+
+describe("MemoryCache2", () => {
+  it("createAndGetSync", () => {
+    /** @type {MemoryCache2<string, string, string>} */
+    const cache = new MemoryCache2();
+
+    let createdCount = 0;
+    const getOrCreate = () =>
+      cache.getOrCreate("foo", "bar", () => {
+        createdCount++;
+        return "baz";
+      });
+
+    for (let i = 0; i < 3; i++) {
+      expect(getOrCreate()).toEqual("baz");
+    }
+    expect(createdCount).toEqual(1);
+  });
+
+  it("createAndGetAsync", async () => {
+    /** @type {MemoryCache2<string, string, Promise<string>>} */
+    const cache = new MemoryCache2();
+
+    let createdCount = 0;
+    const getOrCreate = () =>
+      cache.getOrCreate("foo", "bar", async () => {
+        await Promise.resolve();
+        createdCount++;
+        return "baz";
+      });
+
+    for (let i = 0; i < 3; i++) {
+      await expect(getOrCreate()).resolves.toEqual("baz");
+    }
+    expect(createdCount).toEqual(1);
+  });
+
+  it("keys are ordered", () => {
+    /** @type {MemoryCache2<string, string, string>} */
+    const cache = new MemoryCache2();
+
+    const getOrCreateFooBar = () => cache.getOrCreate("foo", "bar", () => "foo-bar");
+    const getOrCreateBarFoo = () => cache.getOrCreate("bar", "foo", () => "bar-foo");
+
+    for (let i = 0; i < 3; i++) {
+      expect(getOrCreateFooBar()).toEqual("foo-bar");
+      expect(getOrCreateBarFoo()).toEqual("bar-foo");
+    }
   });
 });
