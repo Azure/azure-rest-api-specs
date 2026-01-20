310669d86bbe62ff8a4f12dcb7e62c7b3e1ab07b
Mike Harder <mharder@microsoft.com>
2026-01-20 22:16:43 +0000

    revert temp changes to spec-model

--- .github/shared/cmd/spec-model.js
+++ .github/shared/cmd/spec-model.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-import { flatMapAsync } from "../src/array.js";
 import { debugLogger } from "../src/logger.js";
 import { SpecModel } from "../src/spec-model.js";
 
@@ -16,17 +15,17 @@ let args = process.argv.slice(2);
 const debug = args.includes("--debug");
 args = args.filter((a) => a != "--debug");
 
-// const includeOperations = args.includes("--include-operations");
-// args = args.filter((a) => a != "--include-operations");
+const includeOperations = args.includes("--include-operations");
+args = args.filter((a) => a != "--include-operations");
 
-// const includeRefs = args.includes("--include-refs");
-// args = args.filter((a) => a != "--include-refs");
+const includeRefs = args.includes("--include-refs");
+args = args.filter((a) => a != "--include-refs");
 
-// const relativePaths = args.includes("--relative-paths");
-// args = args.filter((a) => a != "--relative-paths");
+const relativePaths = args.includes("--relative-paths");
+args = args.filter((a) => a != "--relative-paths");
 
-// const noEmbedErrors = args.includes("--no-embed-errors");
-// args = args.filter((a) => a != "--no-embed-errors");
+const noEmbedErrors = args.includes("--no-embed-errors");
+args = args.filter((a) => a != "--no-embed-errors");
 
 if (args.length < 1) {
   console.error(USAGE);
@@ -44,85 +43,17 @@ const specPath = args[0];
 // Default to 'undefined' instead of 'defaultLogger', so output is always a valid JSON object (no logging)
 const logger = debug ? debugLogger : undefined;
 
-const specModel = await withTiming(`new SpecModel()`, () => new SpecModel(specPath, { logger }));
+const specModel = new SpecModel(specPath, { logger });
 
-console.log();
-
-const readmes = await withTiming("getReadmes()", async () => await specModel.getReadmes());
-console.log(`readmes: ${readmes.size}\n`);
-
-const tags = await withTiming(
-  "getTags()",
-  async () =>
-    await flatMapAsync([...readmes.values()], async (r) => [...(await r.getTags()).values()]),
+console.log(
+  JSON.stringify(
+    await specModel.toJSONAsync({
+      embedErrors: !noEmbedErrors,
+      includeOperations,
+      includeRefs,
+      relativePaths,
+    }),
+    null,
+    2,
+  ),
 );
-console.log(`tags: ${tags.length}\n`);
-
-const swaggers = await withTiming("getSwaggers()", () =>
-  tags.flatMap((t) => [...t.inputFiles.values()]),
-);
-console.log(`swaggers: ${swaggers.length}\n`);
-
-const swagger = swaggers[0];
-console.log(`swagger: ${swagger.path}\n`);
-
-// const refs = await withTiming("getRefs()", async () => await swagger.getRefs());
-// console.log(`swaggers: ${refs.size}\n`);
-
-// const operations = await withTiming("getOperations()", async () => await swagger.getOperations());
-// console.log(`operations: ${operations.size}\n`);
-
-// const affectedReadmeTags = await withTiming(
-//   "getAffectedReadmeTags()",
-//   async () => await specModel.getAffectedReadmeTags(swagger.path),
-// );
-// console.log(`affectedReadmeTags: ${inspect(affectedReadmeTags)}\n`);
-
-// const affectedReadmeTags2 = await withTiming(
-//   "getAffectedReadmeTags()",
-//   async () => await specModel.getAffectedReadmeTags(swaggers[1].path),
-// );
-// console.log(`affectedReadmeTags2: ${affectedReadmeTags2.size}\n`);
-
-for (let i = 0; i < 10; i++) {
-  const affectedSwaggers = await withTiming(
-    "getAffectedSwaggers()",
-    async () => await specModel.getAffectedSwaggers(swaggers[i].path),
-  );
-  console.log(`affectedSwaggers: ${affectedSwaggers.size}\n`);
-}
-
-// console.log(
-//   JSON.stringify(
-//     await specModel.toJSONAsync({
-//       embedErrors: !noEmbedErrors,
-//       includeOperations,
-//       includeRefs,
-//       relativePaths,
-//     }),
-//     null,
-//     2,
-//   ),
-// );
-
-/**
- * Runs a function (sync or async), logs before and after,
- * and includes elapsed time in the final log.
- *
- * @template T
- * @param {string} label - Label to include in log messages.
- * @param {() => T | Promise<T>} fn - Function to execute.
- * @returns {Promise<T>} Resolves with the function's return value.
- */
-async function withTiming(label, fn) {
-  const start = performance.now();
-  console.error(`[start] ${label}`);
-
-  try {
-    // Works for both sync and async functions
-    return await Promise.resolve(fn());
-  } finally {
-    const elapsedMs = performance.now() - start;
-    console.error(`[done] ${label} (${elapsedMs.toFixed(3)} ms)`);
-  }
-}
