dae26cec44309b64156b2b5fefa4b678062c386d
Mike Harder <mharder@microsoft.com>
2026-01-20 22:52:23 +0000

    remove outdated comment

--- .github/shared/src/swagger.js
+++ .github/shared/src/swagger.js
@@ -279,7 +279,6 @@ export class Swagger {
       // But, they are safe to cache in this instance.
       this.#allRefs = new Map(
         refPaths.map((p) => {
-          // TODO: Can we replace `new Swagger()` with "lookup existing swagger from Tag backpointer"?
           const swagger = new Swagger(p, {
             logger: this.#logger,
             tag: this.#tag,
