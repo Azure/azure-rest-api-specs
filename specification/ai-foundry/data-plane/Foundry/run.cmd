46e89400dae161324648fe869b1f73e48e8650bc
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2026-02-11 17:41:22 -0800

    Did not intend for this to be commited

--- specification/ai-foundry/data-plane/Foundry/run.cmd
+++
@@ -1,7 +0,0 @@
-call npx tsp compile .
-call npx tsp compile client.csharp.tsp
-call npx tsp compile client.java.tsp
-call npx tsp compile client.tsp
-call npx tsp compile src\sdk-agents\client.tsp
-call npx tsp compile src\sdk-projects-openai\client.tsp
-call npx tsp compile src\sdk-service-agents-contracts\client.tsp
