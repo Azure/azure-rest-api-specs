9df906e26c50cc22dc6c53adb2a430100d0496f7
Mike Harder <mharder@microsoft.com>
2026-02-20 01:47:33 +0000

    simplify, improve typings

--- .github/shared/test/console.test.js
+++ .github/shared/test/console.test.js
@@ -3,10 +3,11 @@ import { log } from "../src/console.js";
 
 describe("console", () => {
   describe("log", () => {
-    /** @type {import("vitest").MockInstance} */ let writeSpy;
+    /** @type {import("vitest").MockInstance<typeof process.stdout.write>} */
+    let writeSpy;
 
     beforeEach(() => {
-      writeSpy = vi.spyOn(process.stdout, "write");
+      writeSpy = vi.spyOn(process.stdout, "write").mockReturnValue(true);
     });
 
     afterEach(() => {
@@ -14,47 +15,27 @@ describe("console", () => {
     });
 
     it("writes a formatted line to stdout", async () => {
-      writeSpy.mockImplementation(() => true);
-
       await log("hello %s", "world");
-
       expect(writeSpy).toBeCalledWith("hello world\n");
     });
 
     it("formats multiple arguments like console.log", async () => {
-      writeSpy.mockImplementation(() => true);
-
       await log("a", "b", "c");
-
       expect(writeSpy).toBeCalledWith("a b c\n");
     });
 
     it("works with no arguments", async () => {
-      writeSpy.mockImplementation(() => true);
-
       await log();
-
       expect(writeSpy).toBeCalledWith("\n");
     });
 
     it("awaits drain when stdout has backpressure", async () => {
-      let drained = false;
-
-      // Simulate backpressure: first write returns false
-      writeSpy.mockImplementation(() => false);
+      writeSpy.mockReturnValueOnce(false);
 
       const promise = log("backpressure test");
-
-      // The promise should not have resolved yet (waiting for drain)
-      expect(drained).toBe(false);
-
-      // Emit drain to release backpressure
       process.stdout.emit("drain");
-      drained = true;
-
       await promise;
 
-      expect(drained).toBe(true);
       expect(writeSpy).toBeCalledWith("backpressure test\n");
     });
   });
