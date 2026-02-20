b65cf4ce0b4d6b5f6e71202f37979a8dbbb8a841
Mike Harder <mharder@microsoft.com>
2026-02-20 01:40:06 +0000

    make arg types exactly match

--- .github/shared/src/console.js
+++ .github/shared/src/console.js
@@ -3,10 +3,8 @@ import { format } from "node:util";
 
 /**
  * Async, backpressure-aware console.log replacement.
- * Same arg shape/formatting as console.log(...args), but returns Promise<void>.
  *
- * @param {...unknown} args
- * @returns {Promise<void>}
+ * @type {(...args: Parameters<typeof console.log>) => Promise<void>}
  */
 export async function log(...args) {
   const line = format(...args) + "\n";
