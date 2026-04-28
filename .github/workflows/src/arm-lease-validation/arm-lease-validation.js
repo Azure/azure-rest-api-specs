5de4c2782217861168ce51411de7fb2206003a3e
Tejaswi Salaigari <tsalaigari@microsoft.com>
2026-04-28 16:54:05 -0500

    Prettier check

--- .github/workflows/src/arm-lease-validation/arm-lease-validation.js
+++ .github/workflows/src/arm-lease-validation/arm-lease-validation.js
@@ -11,7 +11,8 @@ import { CoreLogger } from "../core-logger.js";
 // Configuration
 // ============================================
 
-export const LEASE_FILE_PATTERN = /^\.github\/arm-leases\/[a-zA-Z0-9-]+\/[a-zA-Z0-9.]+\/lease\.yaml$/;
+export const LEASE_FILE_PATTERN =
+  /^\.github\/arm-leases\/[a-zA-Z0-9-]+\/[a-zA-Z0-9.]+\/lease\.yaml$/;
 export const LEASE_FILE_WITH_GROUP_PATTERN =
   /^\.github\/arm-leases\/[a-zA-Z0-9-]+\/[a-zA-Z0-9.]+\/(?!stable|preview)([^/]+)\/lease\.yaml$/;
 
